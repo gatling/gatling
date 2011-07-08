939a185d12719948dc5213650fa1655ce6deb856
Romain SERTELON <bluepyth@gmail.com>
2011-07-08 10:56:01 +0200

    Modifies Example to create request graph Closes #20

--- gatling-examples/src/main/scala/com/excilys/ebi/gatling/examples/statistics/StatisticsExample.scala
+++ gatling-examples/src/main/scala/com/excilys/ebi/gatling/examples/statistics/StatisticsExample.scala
@@ -1,11 +1,15 @@
 package com.excilys.ebi.gatling.examples.statistics
 
 import com.excilys.ebi.gatling.statistics.ActiveSessionsDataPresenter
+import com.excilys.ebi.gatling.statistics.RequestsDataPresenter
 
 object StatisticsExample {
 
   def run(runOn: String) = {
-    val presenter = new ActiveSessionsDataPresenter
-    presenter.generateGraphFor(runOn)
+    val activeSessionsPresenter = new ActiveSessionsDataPresenter
+    activeSessionsPresenter.generateGraphFor(runOn)
+
+    val requestsPresenter = new RequestsDataPresenter
+    requestsPresenter.generateGraphFor(runOn)
   }
 }
