1790efeec11d3ad79822ecd4df2bfdd1f7e2d4b6
Romain SERTELON <bluepyth@gmail.com>
2011-08-03 11:40:39 +0200

    Fixes a bug in RegExpAssertionProvider. This bug was making regexp assertions false, even if it would succeed

--- gatling-core/src/main/scala/com/excilys/ebi/gatling/core/provider/assertion/RegExpAssertionProvider.scala
+++ gatling-core/src/main/scala/com/excilys/ebi/gatling/core/provider/assertion/RegExpAssertionProvider.scala
@@ -5,6 +5,6 @@ import com.excilys.ebi.gatling.core.provider.capture.RegExpCaptureProvider
 class RegExpAssertionProvider extends AbstractAssertionProvider {
   def assert(expected: String, target: Any, from: Any) = {
     val result = (new RegExpCaptureProvider).capture(target, from)
-    (result == expected, result)
+    (result.getOrElse(None) == expected, result)
   }
 }
