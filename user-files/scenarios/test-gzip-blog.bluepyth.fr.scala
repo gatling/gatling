6d9fa657001ca3f4cb234f52c165f2478d2f4f38
Romain SERTELON <bluepyth@gmail.com>
2011-08-10 11:06:32 +0200

    Adds a test scenario for gzipped responses. Closes #92

---
+++ user-files/scenarios/test-gzip-blog.bluepyth.fr.scala
@@ -0,0 +1,5 @@
+val sc = scenario("Test Gzip").doHttpRequest("Blog Main Page", get("http://blog.bluepyth.fr"), assertRegexp("<title>(.*)</title>","Blog de BluePyth"))
+
+val scConfiguration = configureScenario(sc) withUsersNumber 1
+
+val execution = runSimulations(scConfiguration)
--- user-files/scenarios/test-redirect-mail.bluepyth.fr.scala
+++ user-files/scenarios/test-redirect-mail.bluepyth.fr.scala
@@ -1,10 +1,6 @@
-val concurrentUsers = 1
 
-val url = "http://mail.bluepyth.fr"
-val pause1 = 3
+val lambdaUser = scenario("Standard User").doHttpRequest("Redirect TEST", get("http://mail.bluepyth.fr") followsRedirect true, assertRegexp("""<div class="boxtitle">(.*)</div>""", "Bienvenue sur BluePyth Webmail"))
 
-val lambdaUser = scenario("Standard User").doHttpRequest("Redirect TEST", get(url) followsRedirect true, assertRegexp("""<div class="boxtitle">(.*)</div>""", "Bienvenue sur BluePyth Webmail") build).pause(pause1)
-
-val lambdaUserConfig = configureScenario(lambdaUser) withUsersNumber concurrentUsers withRamp (10000, TimeUnit.MILLISECONDS)
+val lambdaUserConfig = configureScenario(lambdaUser) withUsersNumber 1
 
 val execution = runSimulations(lambdaUserConfig)
