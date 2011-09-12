52e788a8402b497ee2f0f288bc27f0663b7f310e
Romain SERTELON <bluepyth@gmail.com>
2011-09-12 16:40:10 +0200

    Refactoring
    Use of constants in the class

--- gatling-core/src/main/scala/com/excilys/ebi/gatling/core/config/GatlingConfig.scala
+++ gatling-core/src/main/scala/com/excilys/ebi/gatling/core/config/GatlingConfig.scala
@@ -1,17 +1,22 @@
 package com.excilys.ebi.gatling.core.config
 
 import com.excilys.ebi.gatling.core.log.Logging
+import org.apache.commons.lang3.StringUtils
 
 object GatlingConfig extends Logging {
+
+  val GATLING_HOME = System.getenv("GATLING_HOME")
+  val GATLING_SYSTEM_PROPERTY = StringUtils.trimToEmpty(System.getProperty("gatling.config"))
+
   val loadConfig: GatlingConfiguration =
     try {
       val configFile =
-        if (System.getProperty("gatling.config", "") != "") {
-          logger.info("Loading custom configuration file: conf/{}", System.getProperty("gatling.config"))
-          System.getenv("GATLING_HOME") + "/conf/" + System.getProperty("gatling.config")
+        if (GATLING_SYSTEM_PROPERTY != StringUtils.EMPTY) {
+          logger.info("Loading custom configuration file: conf/{}", GATLING_SYSTEM_PROPERTY)
+          GATLING_HOME + "/conf/" + GATLING_SYSTEM_PROPERTY
         } else {
           logger.info("Loading default configuration file")
-          System.getenv("GATLING_HOME") + "/conf/gatling.conf"
+          GATLING_HOME + "/conf/gatling.conf"
         }
 
       GatlingConfiguration.fromFile(configFile)
