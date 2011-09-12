a8d13b35e85f47aaeda041be6240cf1b2be3ba7c
Romain SERTELON <bluepyth@gmail.com>
2011-09-12 16:07:17 +0200

    Refactoring
    Removes unused imports

--- gatling-core/src/main/scala/com/excilys/ebi/gatling/core/action/builder/AbstractActionBuilder.scala
+++ gatling-core/src/main/scala/com/excilys/ebi/gatling/core/action/builder/AbstractActionBuilder.scala
@@ -3,9 +3,6 @@ package com.excilys.ebi.gatling.core.action.builder
 import com.excilys.ebi.gatling.core.action.Action
 import com.excilys.ebi.gatling.core.log.Logging
 
-abstract class TRUE
-abstract class FALSE
-
 trait AbstractActionBuilder extends Logging {
   def build(scenarioId: Int): Action
   def withNext(next: Action): AbstractActionBuilder
