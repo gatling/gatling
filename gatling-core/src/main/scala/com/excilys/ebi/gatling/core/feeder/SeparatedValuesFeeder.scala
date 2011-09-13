4a529887cd3ca55b0f9461cdbf1f548e6a31a2b3
Romain SERTELON <bluepyth@gmail.com>
2011-09-13 17:55:15 +0200

    Adds Configuration Option for feeder encoding. Closes #104
    Using key gatling.feeders.encoding allows the user to set the encoding of
    his/her seeds

--- gatling-core/src/main/scala/com/excilys/ebi/gatling/core/feeder/SeparatedValuesFeeder.scala
+++ gatling-core/src/main/scala/com/excilys/ebi/gatling/core/feeder/SeparatedValuesFeeder.scala
@@ -3,13 +3,17 @@ package com.excilys.ebi.gatling.core.feeder
 import scala.io.Source
 import scala.collection.immutable.HashMap
 import scala.collection.mutable.Queue
+import com.excilys.ebi.gatling.core.config.GatlingConfig._
 
 class SeparatedValuesFeeder(fileName: String, mappings: List[String], val separator: String, val extension: String) extends Feeder(fileName, mappings) {
 
+  val encoding = config.getString("gatling.feeders.encoding", "utf-8")
+
   var seeds: Queue[Map[String, String]] = Queue()
 
-  // FIXME: utf-8 should be configurable
-  for (line <- Source.fromFile("user-files/seeds/" + fileName + extension, "utf-8").getLines) {
+  logger.debug("Feeder Encoding : {}", encoding)
+
+  for (line <- Source.fromFile("user-files/seeds/" + fileName + extension, encoding).getLines) {
     var lineMap = new HashMap[String, String]
 
     for (mapping <- mappings zip line.split(separator).toList)
