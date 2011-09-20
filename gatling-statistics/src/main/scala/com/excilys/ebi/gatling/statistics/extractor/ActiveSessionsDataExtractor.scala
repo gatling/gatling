0e7d9c0b9aa3ba9fe24900049cb601ab3e82f450
Stephane Landelle <slandelle+github@excilys.com>
2011-09-20 22:05:29 +0200

    optimize active sessions chart generation

--- gatling-statistics/src/main/scala/com/excilys/ebi/gatling/statistics/extractor/ActiveSessionsDataExtractor.scala
+++ gatling-statistics/src/main/scala/com/excilys/ebi/gatling/statistics/extractor/ActiveSessionsDataExtractor.scala
@@ -1,24 +1,28 @@
 package com.excilys.ebi.gatling.statistics.extractor
 
-import scala.collection.SortedMap
+import scala.math._
+import scala.collection.mutable.Map
+import scala.collection.mutable.LinkedHashMap
+import scala.collection.mutable.ListBuffer
+import scala.io.Source
+import org.joda.time.format.DateTimeFormat
+import org.joda.time.DateTime
+import org.joda.time.Duration
 import com.excilys.ebi.gatling.core.log.Logging
 
-import scala.io.Source
-import scala.collection.immutable.{ TreeMap, TreeSet }
-import scala.collection.mutable.{ HashMap, MultiMap, Set => MSet }
-
-import java.lang.String
-
 class ActiveSessionsDataExtractor(val runOn: String) extends Logging {
-  val formattedRunOn = (new StringBuilder(runOn)).insert(12, ":").insert(10, ":").insert(8, " ").insert(6, "-").insert(4, "-").toString
 
-  def getResults: List[(String, List[(String, Double)])] = {
+  val maxResolution = 100
 
-    var executionStartDates: TreeSet[String] = TreeSet.empty
-    // String: scenarioName, first MultiMap: active sessions, second MultiMap: dead sessions
-    var scenariosToSessions: Map[String, (MultiMap[String, String], MultiMap[String, String])] = Map.empty
+  val dateTimeFormat = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")
 
-    logger.info("[Stats] reading from file: " + runOn + "/simulation.log")
+  def getResults: Map[String, ListBuffer[(String, Double)]] = {
+
+    val executionWindowByScenarioAndUser = new LinkedHashMap[String, LinkedHashMap[String, (String, String)]]
+    var minDate: String = null
+    var maxDate: String = null
+
+    logger.info("[Stats] reading from file: {}/simulation.log", runOn)
 
     // Going through the specified log file
     for (line <- Source.fromFile("results/" + runOn + "/simulation.log", "utf-8").getLines) {
@@ -27,79 +31,98 @@ class ActiveSessionsDataExtractor(val runOn: String) extends Logging {
         // If we have a well formated result
         case Array(runOn, scenarioName, userId, actionName, executionStartDate, executionDuration, resultStatus, resultMessage) => {
 
-          val sessions = scenariosToSessions.get(scenarioName).getOrElse {
-            val s = (new HashMap[String, MSet[String]] with MultiMap[String, String], new HashMap[String, MSet[String]] with MultiMap[String, String])
-            scenariosToSessions += (scenarioName -> s)
-            s
+          val executionWindowByUser = executionWindowByScenarioAndUser.get(scenarioName).getOrElse {
+            val scenarioWindows = new LinkedHashMap[String, (String, String)]
+            executionWindowByScenarioAndUser += scenarioName -> scenarioWindows
+            scenarioWindows
           }
 
-          // Insert a value to keep the value in the set even if there's no active session
-          scenariosToSessions.map { scenarioAndSessions =>
-            scenarioAndSessions._2._1.addBinding(executionStartDate, "0")
+          val executionWindow = executionWindowByUser.get(userId).getOrElse {
+
+            // this is a start date
+            val userWindow = (executionStartDate, "0");
+            executionWindowByUser += (userId -> userWindow)
+
+            if (minDate == null || minDate > executionStartDate) {
+              minDate = executionStartDate
+            }
+
+            userWindow
           }
 
           // Depending on the type of action, we add the session to the good map
           if (actionName == "End of scenario") {
-            sessions._2.addBinding(executionStartDate, userId)
-          } else {
-            sessions._1.addBinding(executionStartDate, userId)
-          }
 
-          // Storing date in a sorted set (to be used later for global active sessions)
-          executionStartDates = executionStartDates + executionStartDate
+            // this is an end date
+            executionWindowByUser += (userId -> (executionWindow._1, executionStartDate))
+
+            if (maxDate == null || maxDate < executionStartDate) {
+              maxDate = executionStartDate
+            }
+          }
         }
         // Else, if the resulting data is not well formated print an error message
         case _ => sys.error("Input file not well formatted")
       }
     }
 
-    // Adding the missing active/dead sessions to the different executionStartDate
-    var lastDate: String = formattedRunOn
+    // decompose execution window into 500 points
 
-    scenariosToSessions.foreach { scenarioAndSessions =>
-      val (scName, (activeSessions, deadSessions)) = scenarioAndSessions
-      executionStartDates.map { startDate =>
-        if (startDate != formattedRunOn) {
-          activeSessions.get(lastDate).map {
-            set =>
-              set.map {
-                userId =>
-                  activeSessions.addBinding(startDate, userId)
-              }
-              deadSessions.get(startDate).map { set =>
-                set.map {
-                  userId =>
-                    activeSessions.removeBinding(startDate, userId)
-                }
-              }
+    val countsByScenarioAndTime = new LinkedHashMap[String, ListBuffer[(String, Double)]]()
+    var globalCountByTime = new ListBuffer[(String, Double)]()
+    countsByScenarioAndTime += "All scenarios" -> globalCountByTime
+
+    getTimes(minDate, maxDate).foreach { time =>
+
+      // iterate on scenarios
+      var activeUsers = 0
+      executionWindowByScenarioAndUser.foreach { scenarioAndWindowsByUser =>
+        val (scenarioName, windowsByUser) = scenarioAndWindowsByUser
+
+        var activeUsersByScenario = 0
+        // iterate on users
+        windowsByUser.foreach { windowByUser =>
+          val (userId, (windowStart, windowEnd)) = windowByUser
+
+          if (windowStart <= time && time < windowEnd) {
+            activeUsers = activeUsers + 1
+            activeUsersByScenario = activeUsersByScenario + 1
           }
         }
-        lastDate = startDate
+
+        val countsByTime = countsByScenarioAndTime.get(scenarioName).getOrElse {
+          val counts = new ListBuffer[(String, Double)]()
+          countsByScenarioAndTime += (scenarioName -> counts)
+          counts
+        }
+
+        countsByTime += ((time, activeUsersByScenario))
       }
+
+      globalCountByTime += ((time, activeUsers))
     }
 
-    // String: scenarioName, String: Date, Double: Number Of ActiveSessions
-    var results: List[(String, List[(String, Double)])] = Nil
+    countsByScenarioAndTime
+  }
 
-    // Creating results for each scenario
-    scenariosToSessions.map { scenarioAndSessions =>
-      var scenarioResults: List[(String, Double)] = Nil
-      var lastSize = 0D
-      executionStartDates.map { startDate =>
-        val sizeAsDouble: Double = scenariosToSessions.get(scenarioAndSessions._1).get._1.get(startDate).map(set => (set.size - 1).toDouble).getOrElse(lastSize)
-        scenarioResults = (startDate, sizeAsDouble) :: scenarioResults
-      }
-      results = (scenarioAndSessions._1, scenarioResults) :: results
+  private def getTimes(minDate: String, maxDate: String) = {
+    val start = DateTime.parse(minDate, dateTimeFormat);
+    val end: DateTime = DateTime.parse(maxDate, dateTimeFormat);
+
+    val stepMillis = getStepMillis(start, end)
+    var current = start
+    val times = new ListBuffer[String]()
+
+    while (current.compareTo(end) < 0) {
+      times += dateTimeFormat.print(current);
+      current = current.plus(stepMillis)
     }
 
-    var globalResults: Map[String, Double] = Map.empty
-    results.foreach { scenarioAndResults =>
-      scenarioAndResults._2.map { result =>
-        globalResults = globalResults + (result._1 -> (globalResults.get(result._1).getOrElse(0D).asInstanceOf[Double] + result._2))
-      }
-    }
+    times
+  }
 
-    results = ("All Scenarios", globalResults.toList) :: results
-    results.reverse
+  private def getStepMillis(start: DateTime, end: DateTime) = {
+    val between = new Duration(start, end);
+    max(between.getMillis() / maxResolution, 1000);
   }
 }
