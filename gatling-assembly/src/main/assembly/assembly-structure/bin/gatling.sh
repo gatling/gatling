f4f140f113f7505b92d2418d025268f9d5118d97
Romain SERTELON <bluepyth@gmail.com>
2011-09-21 15:24:20 +0200

    Gatling can be launched with ./gatling.sh. Closes #140
    Added a condition in gatling.sh to set GATLING_HOME to the right directory
    when launched from bin folder.

--- gatling-assembly/src/main/assembly/assembly-structure/bin/gatling.sh
+++ gatling-assembly/src/main/assembly/assembly-structure/bin/gatling.sh
@@ -3,9 +3,14 @@
 if [ -n "${GATLING_HOME+x}" ]; then
   echo "GATLING_HOME already set to: $GATLING_HOME"
 else
-  export GATLING_HOME=$(cd $(dirname $(dirname $0)); pwd -P)
+  if [ $(basename `pwd -P`) = "bin" ]; then
+    export GATLING_HOME=$(dirname $(dirname $(dirname `pwd`/$0)))
+  else
+    export GATLING_HOME=$(cd $(dirname $(dirname $0)); pwd -P)
+  fi
   echo "GATLING_HOME not set, using default location ($GATLING_HOME)"
 fi
+
 
 JAVA_OPTS="-XX:+UseThreadPriorities -XX:ThreadPriorityPolicy=42 -Xms2048M -Xmx2048M -Xmn100M -Xss512k -XX:+HeapDumpOnOutOfMemoryError -XX:+AggressiveOpts -XX:+OptimizeStringConcat -XX:+UseFastAccessorMethods -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=1 -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly"
 
