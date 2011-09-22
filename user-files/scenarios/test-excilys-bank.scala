b75a6541e7d7907d8ac7287e41964d66142bbf24
Stephane Landelle <slandelle+github@excilys.com>
2011-09-22 18:35:24 +0200

    Add excilys-bank scenarios

---
+++ user-files/scenarios/test-excilys-bank.scala
@@ -0,0 +1,16 @@
+/* URLs */
+val urlBase = "http://localhost:8080/excilys-bank-web"
+val urlLogin = urlBase + "/login"
+val urlAccueil = urlBase + "/private/bank/accounts.html"
+
+/* Feeder */
+val usersInfos = new TSVFeeder("bank2", List("username", "password", "acc1", "acc2", "acc3", "acc4"))
+
+/* Scenarios */
+include("connection-feeder")
+
+/* Configuration */
+val scnConf = configureScenario(scn) withFeeder usersInfos withUsersNumber 100 withRampOf 100
+
+/* Simulation */
+runSimulations(scnConf)
