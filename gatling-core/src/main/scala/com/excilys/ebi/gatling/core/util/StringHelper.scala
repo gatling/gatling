9dd6a44324af31efcea429655471bdb9f7bb90c3
Stephane Landelle <slandelle+github@excilys.com>
2011-09-22 18:34:27 +0200

    StringHelper clean up

--- gatling-core/src/main/scala/com/excilys/ebi/gatling/core/util/StringHelper.scala
+++ gatling-core/src/main/scala/com/excilys/ebi/gatling/core/util/StringHelper.scala
@@ -1,22 +1,19 @@
 package com.excilys.ebi.gatling.core.util
-
-import com.excilys.ebi.gatling.core.context.Context
-
 import org.slf4j.helpers.MessageFormatter
+import com.excilys.ebi.gatling.core.context.Context
+import com.excilys.ebi.gatling.core.log.Logging
 
-object StringHelper {
+object StringHelper extends Logging {
   def interpolate(context: Context, urlToFormat: String, interpolations: Seq[String]) = {
-    val urlInterpolationsNumber = interpolations.size
-    if (urlInterpolationsNumber == 0)
-      urlToFormat
-    else if (urlInterpolationsNumber == 1)
-      MessageFormatter.format(urlToFormat, context.getAttribute(interpolations.head)).getMessage
-    else if (urlInterpolationsNumber == 2)
-      MessageFormatter.format(urlToFormat, context.getAttribute(interpolations(0)), context.getAttribute(interpolations(1))).getMessage
-    else {
-      val interpolationsFromContext: Seq[String] = for (interpolation <- interpolations) yield context.getAttribute(interpolation)
 
-      MessageFormatter.arrayFormat(urlToFormat, interpolationsFromContext.toArray).getMessage
+    interpolations.size match {
+      case 0 => urlToFormat
+      case 1 => MessageFormatter.format(urlToFormat, context.getAttribute(interpolations(0))).getMessage
+      case 2 => MessageFormatter.format(urlToFormat, context.getAttribute(interpolations(0)), context.getAttribute(interpolations(1))).getMessage
+      case _ => {
+        val interpolationsFromContext: Seq[String] = for (interpolation <- interpolations) yield context.getAttribute(interpolation)
+        MessageFormatter.arrayFormat(urlToFormat, interpolationsFromContext.toArray).getMessage
+      }
     }
   }
 }
