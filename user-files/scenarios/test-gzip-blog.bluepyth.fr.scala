35fe8ed0363a2dcafd6b90a52705b58f87d382cf
Romain SERTELON <bluepyth@gmail.com>
2011-09-23 15:21:18 +0200

    Removes user-files from root
    All development user-files are moved in gatling-assembly.
    It will avoid duplicate scripts/files.

--- user-files/scenarios/test-gzip-blog.bluepyth.fr.scala
+++
@@ -1,5 +0,0 @@
-val sc = scenario("Test Gzip").doHttpRequest("Blog Main Page", get("http://blog.bluepyth.fr"), checkRegexp("<title>(.*)</title>","Blog de BluePyth"))
-
-val scConfiguration = configureScenario(sc) withUsersNumber 1
-
-runSimulations(scConfiguration)
