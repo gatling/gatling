8c5293375dda68f7fa74a44c7ed316667cbf2dab
Romain SERTELON <bluepyth@gmail.com>
2011-10-14 11:28:50 +0200

    Adds Comments

--- gatling-core/src/main/scala/com/excilys/ebi/gatling/core/resource/ResourceRegistry.scala
+++ gatling-core/src/main/scala/com/excilys/ebi/gatling/core/resource/ResourceRegistry.scala
@@ -1,19 +1,47 @@
+/*
+ * Copyright 2011 eBusiness Information, Groupe Excilys (www.excilys.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.excilys.ebi.gatling.core.resource
 
 import com.excilys.ebi.gatling.core.log.Logging
 
+/**
+ * The ResourceRegistry is responsible for storing a reference
+ * to all the resources that has to be closed. It is the responsibility
+ * of the developer not to forgive to register its resources.
+ */
 object ResourceRegistry extends Logging {
 
   private var resources: Set[Resource] = Set.empty
 
+  /**
+   * Registers the resource
+   */
   def register(resource: Resource) = {
-    logger.warn("Registering {}", resource)
+    logger.debug("Registering {}", resource)
     resources += resource
   }
 
+  /**
+   * This method tries to close all the resources properly
+   * If one fails the others are still closed and a warning is sent
+   * to the logs
+   */
   def closeAll = {
     for (resource <- resources) {
-      logger.warn("Closing {}", resource)
+      logger.debug("Closing {}", resource)
       try {
         resource.close
       } catch {
--- gatling-http/src/main/scala/com/excilys/ebi/gatling/http/resource/HttpClientResource.scala
+++ gatling-http/src/main/scala/com/excilys/ebi/gatling/http/resource/HttpClientResource.scala
@@ -1,8 +1,28 @@
+/*
+ * Copyright 2011 eBusiness Information, Groupe Excilys (www.excilys.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.excilys.ebi.gatling.http.resource
 
 import com.excilys.ebi.gatling.core.resource.Resource
 import com.ning.http.client.AsyncHttpClient
 
+/**
+ * This resource contains a reference to an AsyncHttpClient
+ *
+ * @param client the AsyncHttpClient references by this resource
+ */
 class HttpClientResource(client: AsyncHttpClient) extends Resource {
   def close = client.close
 }
