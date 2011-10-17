cd653a13bd2fb075d0e437384eadb86c90182375
Romain SERTELON <bluepyth@gmail.com>
2011-10-17 12:30:00 +0200

    Removes unused scenarios

--- gatling-assembly/src/main/assembly/assembly-structure/user-files/scenarios/test-gzip-blog.bluepyth.fr.scala
+++
@@ -1,5 +0,0 @@
-val sc = scenario("Test Gzip").doHttpRequest("Blog Main Page", get("http://blog.bluepyth.fr"), checkRegexpEquals("<title>(.*)</title>","Blog de BluePyth"))
-
-val scConfiguration = configureScenario(sc) withUsersNumber 1
-
-runSimulations(scConfiguration)
--- gatling-assembly/src/main/assembly/assembly-structure/user-files/scenarios/test-redirect-mail.bluepyth.fr.scala
+++
@@ -1,6 +0,0 @@
-
-val lambdaUser = scenario("Standard User").doHttpRequest("Redirect TEST", get("http://mail.bluepyth.fr") followsRedirect true, checkRegexpExists("""<div class="boxtitle">(.*)</div>""", "Bienvenue sur BluePyth Webmail"))
-
-val lambdaUserConfig = configureScenario(lambdaUser) withUsersNumber 1
-
-runSimulations(lambdaUserConfig)
