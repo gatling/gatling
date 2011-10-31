96237de155e836de9cf65680b9aadec46a3c81aa
Romain SERTELON <bluepyth@gmail.com>
2011-10-31 09:42:09 +0100

    Renames global request chart. Closes #210

--- gatling-statistics/src/main/scala/com/excilys/ebi/gatling/statistics/template/GlobalRequestsTemplate.scala
+++ gatling-statistics/src/main/scala/com/excilys/ebi/gatling/statistics/template/GlobalRequestsTemplate.scala
@@ -20,8 +20,8 @@ import com.excilys.ebi.gatling.core.util.StringHelper._
 class GlobalRequestsTemplate(val runOn: String, val series: List[TimeSeries]) {
 
 	def getOutput: String = {
-		val highstocks = new HighstocksTimeTemplate(series, "Number of Requests", "Number of requests per second", "{} requests").getOutput
-		new LayoutTemplate("Requests", runOn, EMPTY, highstocks).getOutput
+		val highstocks = new HighstocksTimeTemplate(series, "Number of requests per second", "Number of requests per second", "{} requests").getOutput
+		new LayoutTemplate("Requests per second", runOn, EMPTY, highstocks).getOutput
 	}
 
 }
