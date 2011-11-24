d6b47b99b8b34220976ced74919063b9a9f00951
slandelle <slandelle+github@excilys.com>
2011-11-24 09:40:40 +0100

    BodyPartReceived phase is no longer used

--- gatling-http/src/main/scala/com/excilys/ebi/gatling/http/request/HttpPhase.scala
+++ gatling-http/src/main/scala/com/excilys/ebi/gatling/http/request/HttpPhase.scala
@@ -20,5 +20,5 @@ package com.excilys.ebi.gatling.http.request
  */
 object HttpPhase extends Enumeration {
 	type HttpPhase = Value
-	val StatusReceived, HeadersReceived, BodyPartReceived, CompletePageReceived = Value
+	val StatusReceived, HeadersReceived, CompletePageReceived = Value
 }
