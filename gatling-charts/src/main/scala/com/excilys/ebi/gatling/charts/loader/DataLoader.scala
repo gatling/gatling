8c241a61e5f08be0fd0ce78ef5cb176d329b1667
slandelle <slandelle+github@excilys.com>
2011-12-06 09:09:00 +0100

    Optimize DataLoader memory usage with more caching

--- gatling-charts/src/main/scala/com/excilys/ebi/gatling/charts/loader/DataLoader.scala
+++ gatling-charts/src/main/scala/com/excilys/ebi/gatling/charts/loader/DataLoader.scala
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 package com.excilys.ebi.gatling.charts.loader
-import java.util.{Comparator => JComparator}
-import java.util.{HashMap => JHashMap, Collections => JCollections, ArrayList => JArrayList}
+import java.util.{ Comparator => JComparator }
+import java.util.{ HashMap => JHashMap, Collections => JCollections, ArrayList => JArrayList }
 
 import scala.collection.JavaConversions.asScalaBuffer
 import scala.collection.immutable.SortedMap
@@ -30,62 +30,74 @@ import com.excilys.ebi.gatling.core.config.GatlingConfig.CONFIG_ENCODING
 import com.excilys.ebi.gatling.core.config.GatlingFiles.simulationLogFile
 import com.excilys.ebi.gatling.core.log.Logging
 import com.excilys.ebi.gatling.core.result.message.ResultStatus
-import com.excilys.ebi.gatling.core.result.writer.FileDataWriter.{GROUPS_SUFFIX, GROUPS_SEPARATOR, GROUPS_PREFIX}
+import com.excilys.ebi.gatling.core.result.writer.FileDataWriter.{ GROUPS_SUFFIX, GROUPS_SEPARATOR, GROUPS_PREFIX }
 import com.excilys.ebi.gatling.core.util.DateHelper.parseResultDate
 import com.excilys.ebi.gatling.core.util.FileHelper.TABULATION_SEPARATOR
-import scala.collection.mutable.{Seq => MSeq}
+import scala.collection.mutable.{ Seq => MSeq }
 
 class DataLoader(runOn: String) extends Logging {
 
 	private val data: MSeq[ResultLine] = {
 
-		val tmpData = new JArrayList[ResultLine]
+		val data = new JArrayList[ResultLine]
 
 		// use caches in order to reuse String instances instead of holding multiple references of equal Strings
-		val stringCache= new JHashMap[String, String]
+		val stringCache = new JHashMap[String, String]
 		val intCache = new JHashMap[String, Int]
+		val dateTimeCache = new JHashMap[String, DateTime]
+		val groupsCache = new JHashMap[String, List[String]]
 
 		def cachedString(string: String) = {
 			if (!stringCache.containsKey(string)) {
 				val newString = new String(string)
 				stringCache.put(newString, newString)
-			}
-			stringCache.get(string)
+			} else
+				stringCache.get(string)
 		}
 
 		def cachedInt(string: String) = {
 			if (!intCache.containsKey(string)) {
 				val newString = new String(string)
 				intCache.put(newString, newString.toInt)
-			}
-			intCache.get(string)
+			} else
+				intCache.get(string)
 		}
+
+		def cachedDateTime(string: String) = {
+			if (!dateTimeCache.containsKey(string)) {
+				val newString = new String(string)
+				dateTimeCache.put(newString, parseResultDate(newString))
+			} else
+				dateTimeCache.get(string)
+		}
+		
+		def cachedGroups(string: String) = {
+			if (!groupsCache.containsKey(string)) {
+				val newString = new String(string)
+				groupsCache.put(newString, parseGroups(newString))
+			} else
+				groupsCache.get(string)
+		}
+
+		def parseGroups(string: String) = string.stripPrefix(GROUPS_PREFIX).stripSuffix(GROUPS_SUFFIX).split(GROUPS_SEPARATOR).toList
 
 		for (line <- Source.fromFile(simulationLogFile(runOn).jfile, CONFIG_ENCODING).getLines) {
 			line.split(TABULATION_SEPARATOR) match {
 				// If we have a well formated result
 				case Array(runOn, scenarioName, userId, actionName, executionStartDate, executionDuration, resultStatus, resultMessage, groups) =>
 
-					val cachedRunOn = cachedString(runOn)
-					val cachedScenarioName = cachedString(scenarioName)
-					val cachedUserId = cachedInt(userId)
-					val cachedActionName = cachedString(actionName)
-					val cachedGroups = cachedString(groups)
-					val cachedResultMessage = cachedString(resultMessage)
-
-					val groupsList = cachedGroups.stripPrefix(GROUPS_PREFIX).stripSuffix(GROUPS_SUFFIX).split(GROUPS_SEPARATOR).toList
-					tmpData.add(ResultLine(cachedRunOn, cachedScenarioName, cachedUserId, cachedActionName, parseResultDate(executionStartDate), executionDuration.toInt, ResultStatus.withName(resultStatus), cachedResultMessage, groupsList))
+					data.add(ResultLine(cachedString(runOn), cachedString(scenarioName), cachedInt(userId), cachedString(actionName), cachedDateTime(executionStartDate), cachedInt(executionDuration), ResultStatus.withName(resultStatus), cachedString(resultMessage), cachedGroups(groups)))
 				// Else, if the resulting data is not well formated print an error message
 				case _ => logger.warn("simulation.log had bad end of file, statistics will be generated but may not be accurate")
 			}
 		}
-		
-		JCollections.sort(tmpData, new JComparator[ResultLine] {
-			def compare(o1 : ResultLine, o2 : ResultLine) = {
+
+		JCollections.sort(data, new JComparator[ResultLine] {
+			def compare(o1: ResultLine, o2: ResultLine) = {
 				o1.executionStartDate.getMillis.compare(o2.executionStartDate.getMillis)
 			}
 		})
-		tmpData
+		data
 	}
 
 	val dataIndexedByDateInSeconds: SortedMap[DateTime, MSeq[ResultLine]] = SortedMap(data.groupBy(_.executionStartDate.withMillisOfSecond(0)).toSeq: _*)
