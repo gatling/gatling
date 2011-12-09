e0ee25dfa5ebda62b6de1e07e933f585fa50de1d
Romain SERTELON <bluepyth@gmail.com>
2011-12-09 15:19:31 +0100

    Finishes maven archetype. Closes #241

--- gatling-maven-archetype/src/main/resources/archetype-resources/src/main/scala/Recorder.scala
+++ gatling-maven-archetype/src/main/resources/archetype-resources/src/main/scala/Recorder.scala
@@ -1,3 +1,14 @@
+import scala.tools.nsc.io.File
+
+import com.excilys.ebi.gatling.recorder.ui.GatlingHttpProxyUI
+import com.excilys.ebi.gatling.core.util.PathHelper.path2string
+
 object Recorder extends App {
 
+	val url = getClass.getClassLoader.getResource("gatling.conf").getPath
+	val projectDir = File(url).parents(2)
+
+	val outputFolder = projectDir / "src/main/scala" / "${package}".replace(".", "/")
+
+	GatlingHttpProxyUI.main(Array("-scala", "-of", outputFolder, "-run", "-eclipse", "${package}"))
 }
