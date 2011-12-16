7796bd86bfa72d98754b3253280a5b60b3a9c985
slandelle <slandelle+github@excilys.com>
2011-12-16 15:49:16 +0100

    Update license header

--- gatling-bundle/src/main/assembly/assembly-structure/bin/gatling.bat
+++ gatling-bundle/src/main/assembly/assembly-structure/bin/gatling.bat
@@ -1,11 +1,11 @@
 @REM
-@REM Copyright 2011 eBusiness Information, Groupe Excilys (www.excilys.com)
+@REM Copyright 2011-2012 eBusiness Information, Groupe Excilys (www.excilys.com)
 @REM
 @REM Licensed under the Apache License, Version 2.0 (the "License");
 @REM you may not use this file except in compliance with the License.
 @REM You may obtain a copy of the License at
 @REM
-@REM     http://www.apache.org/licenses/LICENSE-2.0
+@REM 		http://www.apache.org/licenses/LICENSE-2.0
 @REM
 @REM Unless required by applicable law or agreed to in writing, software
 @REM distributed under the License is distributed on an "AS IS" BASIS,
