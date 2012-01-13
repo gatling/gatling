2c1d503e8299df54d78e4491f1aeba506cc9bf7a
Stephane Landelle <slandelle+github@excilys.com>
2012-01-13 15:25:13 +0100

    Don't make recorder un uber jar. Close #351

---
+++ gatling-bundle/src/main/assembly/assembly-structure/bin/recorder.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Copyright 2011-2012 eBusiness Information, Groupe Excilys (www.excilys.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# 		http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if [ -n "${GATLING_HOME+x}" ]; then
+	echo "GATLING_HOME already set to: $GATLING_HOME"
+else
+	if [ $(basename `pwd -P`) = "bin" ]; then
+		# If was started with 'sh filename'
+		if [ $0 = "gatling.sh" ]; then
+			export GATLING_HOME=$(dirname $(dirname `pwd -P`/$0))
+		else
+			export GATLING_HOME=$(dirname $(dirname $(dirname `pwd -P`/$0)))
+		fi
+	else
+		export GATLING_HOME=$(cd $(dirname $(dirname $0)); pwd -P)
+	fi
+	echo "GATLING_HOME not set, using default location ($GATLING_HOME)"
+fi
+
+JAVA_OPTS="-server -XX:+UseThreadPriorities -XX:ThreadPriorityPolicy=42 -Xms512M -Xmx512M -Xmn100M -Xss1024k -XX:+HeapDumpOnOutOfMemoryError -XX:+AggressiveOpts -XX:+OptimizeStringConcat -XX:+UseFastAccessorMethods -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=1 -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly"
+
+CLASSPATH="$GATLING_HOME/lib/*:$GATLING_HOME/conf"
+
+java $JAVA_OPTS -cp $CLASSPATH com.excilys.ebi.gatling.recorder.ui.GatlingHttpProxyUI $@
