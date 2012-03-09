0889eac5d41e8cd12084b914ee956c3f2a4e94a6
Stephane Landelle <slandelle+github@excilys.com>
2012-03-09 17:38:27 +0100

    Extract test data into a file

--- gatling-core/src/test/scala/com/excilys/ebi/gatling/core/check/extractor/jsonpath/JsonExtractorSpec.scala
+++ gatling-core/src/test/scala/com/excilys/ebi/gatling/core/check/extractor/jsonpath/JsonExtractorSpec.scala
@@ -17,55 +17,18 @@ package com.excilys.ebi.gatling.core.check.extractor.jsonpath
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
-
 import com.excilys.ebi.gatling.core.check.extractor.jsonpath.JsonExtractorSpec.extractor
+import scala.io.Source
 
 object JsonExtractorSpec {
-	// @formatter:off
-	val document =
-		"{ \"store\": {\n" +
-			"    \"book\": [ \n" +
-			"      { \"category\": \"reference\",\n" +
-			"        \"author\": \"Nigel Rees\",\n" +
-			"        \"title\": \"Sayings of the Century\",\n" +
-			"        \"display-price\": 8.95\n" +
-			"      },\n" +
-			"      { \"category\": \"fiction\",\n" +
-			"        \"author\": \"Evelyn Waugh\",\n" +
-			"        \"title\": \"Sword of Honour\",\n" +
-			"        \"display-price\": 12.99\n" +
-			"      },\n" +
-			"      { \"category\": \"fiction\",\n" +
-			"        \"author\": \"Herman Melville\",\n" +
-			"        \"title\": \"Moby Dick\",\n" +
-			"        \"isbn\": \"0-553-21311-3\",\n" +
-			"        \"display-price\": 8.99\n" +
-			"      },\n" +
-			"      { \"category\": \"fiction\",\n" +
-			"        \"author\": \"J. R. R. Tolkien\",\n" +
-			"        \"title\": \"The Lord of the Rings\",\n" +
-			"        \"isbn\": \"0-395-19395-8\",\n" +
-			"        \"display-price\": 22.99\n" +
-			"      }\n" +
-			"    ],\n" +
-			"    \"bicycle\": {\n" +
-			"      \"color\": \"red\",\n" +
-			"      \"display-price\": 19.95,\n" +
-			"      \"foo:bar\": \"fooBar\",\n" +
-			"      \"dot.notation\": \"new\",\n" +
-			"      \"dash-notation\": \"dashes\"\n" +
-			"    }\n" +
-			"  },\n" +
-			"  \"foo\": \"bar\"\n" +
-			"}"
-	// @formatter:on
+	val document = Source.fromInputStream(getClass.getResourceAsStream("/test.json")).mkString
 
 	val extractor = new JsonPathExtractor(document)
 }
 
 @RunWith(classOf[JUnitRunner])
 class JsonExtractorSpec extends Specification {
-
+	
 	"//author" should {
 
 		"have count return 4" in {
