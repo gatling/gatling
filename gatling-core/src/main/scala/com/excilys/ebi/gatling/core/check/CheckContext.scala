f56271163a0bfe4d80aeb2bf15cdaa48c4d292b3
Stephane Landelle <slandelle+github@excilys.com>
2012-03-28 11:22:54 +0200

    Clean up CheckContext

--- gatling-core/src/main/scala/com/excilys/ebi/gatling/core/check/CheckContext.scala
+++ gatling-core/src/main/scala/com/excilys/ebi/gatling/core/check/CheckContext.scala
@@ -16,14 +16,26 @@
 package com.excilys.ebi.gatling.core.check
 import scala.collection.mutable
 
+/**
+ * A context for performing check that might store resources so that they can be reused for multiple checks.
+ * Resources are stored in a ThreadLocal.
+ *
+ */
 object CheckContext {
 
 	private val contextHolder = new ThreadLocal[mutable.Map[String, Any]]
 
+	/**
+	 * First, set a fresh context
+	 * Then, execute the block of code
+	 * Last, ensure context clean up
+	 *
+	 * @param block code to be executed in the context of performing checks
+	 * @return the result of the block of code
+	 */
 	def useCheckContext[T](block: => T) = {
 		try {
-			if (contextHolder.get == null)
-				contextHolder.set(new mutable.HashMap[String, Any])
+			contextHolder.set(new mutable.HashMap[String, Any])
 
 			block
 		} finally {
@@ -31,10 +43,16 @@ object CheckContext {
 		}
 	}
 
-	def context = Option(contextHolder.get).getOrElse(throw new UnsupportedOperationException("Context not set. You're probably trying to access the CheckContext outside of the useCheckContext scope"))
+	/**
+	 * @return the context in which to store resources
+	 */
+	private def context = Option(contextHolder.get).getOrElse(throw new UnsupportedOperationException("Context not set. You're probably trying to access the CheckContext outside of the useCheckContext scope"))
 
-	def getCheckContextAttribute[T](key: String): Option[T] = context.get(key).asInstanceOf[Option[T]]
-
+	/**
+	 * @param key the key under which a resource might have been previously stored
+	 * @param value a resource to be stored if none was previously stored
+	 * @return the stored resource (old or new)
+	 */
 	def getOrUpdateCheckContextAttribute[T](key: String, value: => T): T = {
 		context.get(key).asInstanceOf[Option[T]].getOrElse {
 			val resolvedValue = value
