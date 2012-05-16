gatling/gatling

commit b950667109d62e35ea81a2bb3fdbfd81f5f07fff

Minor clean up

gatling-recorder/src/main/scala/com/excilys/ebi/gatling/recorder/config/Configuration.scala
@@ -15,26 +15,27 @@
  */
 package com.excilys.ebi.gatling.recorder.config
 
-import java.io.FileWriter
-import java.io.IOException
+import java.io.{ IOException, FileWriter }
+
+import scala.reflect.BeanProperty
 import scala.tools.nsc.io.Path.string2path
 import scala.tools.nsc.io.File
-import org.apache.commons.io.IOUtils.closeQuietly
-import com.excilys.ebi.gatling.core.config.GatlingFiles
+
+import com.excilys.ebi.gatling.core.config.{ GatlingFiles, GatlingConfiguration }
+import com.excilys.ebi.gatling.core.util.IOHelper.use
 import com.excilys.ebi.gatling.recorder.ui.Commons.GATLING_RECORDER_FILE_NAME
 import com.excilys.ebi.gatling.recorder.ui.enumeration.FilterStrategy.NONE
 import com.thoughtworks.xstream.io.xml.DomDriver
 import com.thoughtworks.xstream.XStream
+
 import grizzled.slf4j.Logging
-import com.excilys.ebi.gatling.core.config.GatlingConfiguration
-import scala.reflect.BeanProperty
 
 object Configuration extends Logging {
 
 	GatlingConfiguration.setUp(None, None, None, None, None)
-  
+
 	val configuration = new Configuration
-	lazy val DEFAULT_CLASS_NAME = "Simulation"
+	val DEFAULT_CLASS_NAME = "Simulation"
 
 	private val XSTREAM = new XStream(new DomDriver)
 	private val CONFIGURATION_FILE = File(System.getProperty("user.home") / GATLING_RECORDER_FILE_NAME)
@@ -54,7 +55,7 @@ object Configuration extends Logging {
 		configuration.proxy = c.proxy
 		configuration.filterStrategy = c.filterStrategy
 		configuration.patterns = c.patterns
-		configuration.outputFolder = c.outputFolder 
+		configuration.outputFolder = c.outputFolder
 		configuration.simulationClassName = c.simulationClassName
 		configuration.simulationPackage = c.simulationPackage
 		configuration.followRedirect = c.followRedirect
@@ -63,17 +64,15 @@ object Configuration extends Logging {
 	}
 
 	def saveToDisk {
-		var fw: FileWriter = null
-		try {
-			fw = new FileWriter(CONFIGURATION_FILE.jfile)
-			XSTREAM.toXML(configuration, fw)
-		} catch {
-			case e: IOException => error(e.getMessage)
-		} finally {
-			closeQuietly(fw)
+		use(new FileWriter(CONFIGURATION_FILE.jfile)) { fw =>
+			try {
+				XSTREAM.toXML(configuration, fw)
+			} catch {
+				case e: IOException => error(e.getMessage)
+			}
 		}
 	}
-	
+
 	private def initFromDisk {
 		if (CONFIGURATION_FILE.exists) {
 			try {
@@ -88,7 +87,7 @@ object Configuration extends Logging {
 	private def initFromCli(o: Options) {
 		o.localPort.map(configuration.port = _)
 		o.localPortSsl.map(configuration.sslPort = _)
-		if(o.proxyHost.isDefined && o.proxyPort.isDefined)
+		if (o.proxyHost.isDefined && o.proxyPort.isDefined)
 			configuration.proxy = new ProxyConfig(o.proxyHost, o.proxyPort, o.proxyPortSsl, None, None)
 		o.outputFolder.map(configuration.outputFolder = _)
 		o.simulationClassName.map(configuration.simulationClassName = _)
@@ -99,7 +98,7 @@ object Configuration extends Logging {
 	}
 }
 
-class Configuration{
+class Configuration {
 
 	@BeanProperty var port = 8000
 	@BeanProperty var sslPort = 8001
@@ -112,7 +111,7 @@ class Configuration{
 	@transient var requestBodiesFolder: String = GatlingFiles.requestBodiesFolder.toString
 	@BeanProperty var simulationPackage: Option[String] = None
 	@BeanProperty var simulationClassName: String = Configuration.DEFAULT_CLASS_NAME
-	@BeanProperty var followRedirect: Boolean = false 
+	@BeanProperty var followRedirect: Boolean = false
 
 	override def toString =
 		new StringBuilder("Configuration [")

gatling-recorder/src/main/scala/com/excilys/ebi/gatling/recorder/ui/frame/ValidationHelper.scala
@@ -14,34 +14,30 @@
  * limitations under the License.
  */
 package com.excilys.ebi.gatling.recorder.ui.frame
-import java.awt.event.KeyEvent
-import java.awt.event.KeyListener
-import java.awt.Color
-import java.awt.EventQueue
+
+import java.awt.event.{ KeyListener, KeyEvent }
+import java.awt.{ EventQueue, Color }
 import java.util.Date
 
-import com.excilys.ebi.gatling.core.util.StringHelper.EMPTY
-
 import grizzled.slf4j.Logging
-import javax.swing.BorderFactory
-import javax.swing.JTextField
+import javax.swing.{ JTextField, BorderFactory }
 
 object ValidationHelper extends Logging {
-	
+
 	val standardBorder = BorderFactory.createMatteBorder(1, 1, 1, 1, Color.darkGray)
 	val errorBorder = BorderFactory.createMatteBorder(2, 2, 2, 2, Color.red)
 	val disabledBorder = BorderFactory.createMatteBorder(1, 1, 1, 1, new Color(184, 207, 229))
-	
-	var validationStatus = Map.empty[String,Boolean]
-	
+
+	var validationStatus = Map.empty[String, Boolean]
+
 	// TODO: move table validation here
-	
+
 	def intValidator(cFrame: ConfigurationFrame, id: String) = new KeyListener {
-		def keyReleased(e: KeyEvent){
+		def keyReleased(e: KeyEvent) {
 			val txtField = e.getComponent.asInstanceOf[JTextField]
 			try {
 				txtField.getText.toInt
-				if(txtField.isEnabled)
+				if (txtField.isEnabled)
 					txtField.setBorder(standardBorder)
 				else
 					txtField.setBorder(disabledBorder)
@@ -53,16 +49,16 @@ object ValidationHelper extends Logging {
 				}
 			}
 		}
-		
+
 		def keyPressed(e: KeyEvent) {}
-		
+
 		def keyTyped(e: KeyEvent) {}
 	}
-	
+
 	def nonEmptyValidator(cFrame: ConfigurationFrame, id: String) = new KeyListener {
 		def keyReleased(e: KeyEvent) {
 			val txtField = e.getComponent.asInstanceOf[JTextField]
-			if(!txtField.getText.trim.isEmpty){
+			if (!txtField.getText.trim.isEmpty) {
 				txtField.setBorder(standardBorder)
 				updateValidationStatus(id, true, cFrame)
 			} else {
@@ -70,23 +66,23 @@ object ValidationHelper extends Logging {
 				updateValidationStatus(id, false, cFrame)
 			}
 		}
-		
+
 		def keyPressed(e: KeyEvent) {}
-		
+
 		def keyTyped(e: KeyEvent) {}
 	}
-	
+
 	def proxyHostValidator(cFrame: ConfigurationFrame) = new KeyListener {
 		def keyReleased(e: KeyEvent) {
 			val txtField = e.getComponent.asInstanceOf[JTextField]
-			if(!txtField.getText.trim.isEmpty){
+			if (!txtField.getText.trim.isEmpty) {
 				cFrame.txtProxyPort.setEnabled(true)
 				cFrame.txtProxySslPort.setEnabled(true)
 			} else {
 				cFrame.txtProxyPort.setEnabled(false)
 				cFrame.txtProxyPort.setText("0")
-				cFrame.txtProxyPort.getKeyListeners.foreach{
-					case kl: KeyListener => 
+				cFrame.txtProxyPort.getKeyListeners.foreach {
+					case kl: KeyListener =>
 						EventQueue.invokeLater(new Runnable() {
 							def run {
 								kl.keyReleased(new KeyEvent(cFrame.txtProxyPort, 0, new Date().getTime, 0, 0))
@@ -97,19 +93,19 @@ object ValidationHelper extends Logging {
 				cFrame.txtProxySslPort.setText("0")
 			}
 		}
-		
+
 		def keyPressed(e: KeyEvent) {}
-		
+
 		def keyTyped(e: KeyEvent) {}
 	}
-	
-	def updateValidationStatus(id: String, status: Boolean, cfgFrame: ConfigurationFrame){
+
+	def updateValidationStatus(id: String, status: Boolean, cfgFrame: ConfigurationFrame) {
 		validationStatus += (id -> status)
 		updateStartButtonStatus(cfgFrame)
 	}
-	
-	def updateStartButtonStatus(cfgFrame: ConfigurationFrame){
-		val newStatus = validationStatus.values.foldLeft(true)((b1,b2) => b1 && b2)
+
+	def updateStartButtonStatus(cfgFrame: ConfigurationFrame) {
+		val newStatus = validationStatus.values.foldLeft(true)((b1, b2) => b1 && b2)
 		cfgFrame.btnStart.setEnabled(newStatus)
 	}
 }

gatling-recorder/src/main/scala/com/excilys/ebi/gatling/recorder/config/Pattern.scala
@@ -15,8 +15,9 @@
  */
 package com.excilys.ebi.gatling.recorder.config
 
-import com.excilys.ebi.gatling.recorder.ui.enumeration.PatternType._
 import scala.reflect.BeanProperty
+
+import com.excilys.ebi.gatling.recorder.ui.enumeration.PatternType.PatternType
 
 class Pattern(@BeanProperty val patternType: PatternType, @BeanProperty val pattern: String) {
 	override def toString = patternType + " | " + pattern
@@ -24,17 +25,16 @@ class Pattern(@BeanProperty val patternType: PatternType, @BeanProperty val patt
 	override def hashCode: Int = {
 		val prime = 31
 		var result = 1
-		result = prime * result + (if(pattern == null) 0 else pattern.hashCode)
-		result = prime * result + (if(patternType == null) 0 else patternType.hashCode)
+		result = prime * result + (if (pattern == null) 0 else pattern.hashCode)
+		result = prime * result + (if (patternType == null) 0 else patternType.hashCode)
 		result
 	}
 
 	override def equals(obj: Any): Boolean = {
 		obj match {
-      case p: Pattern =>
-        p.pattern == pattern && p.patternType == patternType
-      case _ => false
-    }
+			case p: Pattern =>
+				p.pattern == pattern && p.patternType == patternType
+			case _ => false
+		}
 	}
-
 }

gatling-recorder/src/main/scala/com/excilys/ebi/gatling/recorder/controller/RecorderController.scala
@@ -25,26 +25,15 @@ import scala.tools.nsc.io.Directory
 
 import org.codehaus.plexus.util.SelectorUtils
 import org.jboss.netty.handler.codec.http.HttpHeaders.Names.PROXY_AUTHORIZATION
-import org.jboss.netty.handler.codec.http.HttpMethod
-import org.jboss.netty.handler.codec.http.HttpRequest
-import org.jboss.netty.handler.codec.http.HttpResponse
+import org.jboss.netty.handler.codec.http.{ HttpResponse, HttpRequest, HttpMethod }
 
 import com.excilys.ebi.gatling.recorder.config.Configuration.configuration
-import com.excilys.ebi.gatling.recorder.config.Configuration
-import com.excilys.ebi.gatling.recorder.config.Options
+import com.excilys.ebi.gatling.recorder.config.{ Options, Configuration }
 import com.excilys.ebi.gatling.recorder.http.GatlingHttpProxy
-import com.excilys.ebi.gatling.recorder.scenario.PauseElement
-import com.excilys.ebi.gatling.recorder.scenario.PauseUnit
-import com.excilys.ebi.gatling.recorder.scenario.RequestElement
-import com.excilys.ebi.gatling.recorder.scenario.ScenarioElement
-import com.excilys.ebi.gatling.recorder.scenario.ScenarioExporter
-import com.excilys.ebi.gatling.recorder.ui.enumeration.FilterStrategy
-import com.excilys.ebi.gatling.recorder.ui.enumeration.PatternType
-import com.excilys.ebi.gatling.recorder.ui.frame.ConfigurationFrame
-import com.excilys.ebi.gatling.recorder.ui.frame.RunningFrame
-import com.excilys.ebi.gatling.recorder.ui.info.PauseInfo
-import com.excilys.ebi.gatling.recorder.ui.info.RequestInfo
-import com.excilys.ebi.gatling.recorder.ui.info.SSLInfo
+import com.excilys.ebi.gatling.recorder.scenario.{ ScenarioExporter, ScenarioElement, RequestElement, PauseUnit, PauseElement }
+import com.excilys.ebi.gatling.recorder.ui.enumeration.{ PatternType, FilterStrategy }
+import com.excilys.ebi.gatling.recorder.ui.frame.{ RunningFrame, ConfigurationFrame }
+import com.excilys.ebi.gatling.recorder.ui.info.{ SSLInfo, RequestInfo, PauseInfo }
 import com.ning.http.util.Base64
 
 import grizzled.slf4j.Logging
@@ -57,7 +46,7 @@ object RecorderController extends Logging {
 	private var lastRequestDate: Date = null
 
 	private var scenarioElements = List[ScenarioElement]()
-	
+
 	def apply(options: Options) {
 		Configuration(options)
 		configurationFrame.populateItemsFromConfiguration(configuration)
@@ -76,14 +65,14 @@ object RecorderController extends Logging {
 		GatlingHttpProxy.shutdown
 
 		// Save Scenario to Disk only if there are recorded elements
-		if(!scenarioElements.isEmpty)
+		if (!scenarioElements.isEmpty)
 			ScenarioExporter.saveScenario(startDate, scenarioElements.reverse)
 
 		clearRecorderState
 
 		showConfigurationFrame
 	}
-	
+
 	def receiveRequest(request: HttpRequest) {
 		// If Outgoing Proxy set, we record the credentials to use them when sending the request
 		Option(request.getHeader(PROXY_AUTHORIZATION)).map { header =>
@@ -97,19 +86,19 @@ object RecorderController extends Logging {
 	def receiveResponse(request: HttpRequest, response: HttpResponse) {
 		if (isRequestToBeAdded(request)) {
 			processRequest(request, response)
-			
+
 			// Pause calculation
 			if (lastRequestDate != null) {
 				val newRequestDate = new Date
 				val diff = newRequestDate.getTime - lastRequestDate.getTime
-				if(diff > 10){
-					
-					val pauseValueAndUnit = 
-						if(diff > 1000) 
+				if (diff > 10) {
+
+					val pauseValueAndUnit =
+						if (diff > 1000)
 							(round(diff / 1000).toLong, PauseUnit.SECONDS)
 						else
 							(diff, PauseUnit.MILLISECONDS)
-					
+
 					lastRequestDate = newRequestDate
 					EventQueue.invokeLater(new Runnable() {
 						def run {
@@ -127,7 +116,7 @@ object RecorderController extends Logging {
 	def secureConnection(securedHostURI: URI) {
 		EventQueue.invokeLater(new Runnable() {
 			def run {
-				runningFrame.receiveEventInfo(new SSLInfo(securedHostURI.toString))
+				runningFrame.receiveEventInfo(SSLInfo(securedHostURI.toString))
 			}
 		})
 	}
@@ -152,7 +141,7 @@ object RecorderController extends Logging {
 
 	private def isRequestToBeAdded(request: HttpRequest): Boolean = {
 		val uri = new URI(request.getUri)
-		if(Array(HttpMethod.POST, HttpMethod.GET, HttpMethod.PUT, HttpMethod.DELETE).contains(request.getMethod)) {
+		if (Vector(HttpMethod.POST, HttpMethod.GET, HttpMethod.PUT, HttpMethod.DELETE).contains(request.getMethod)) {
 			if (configuration.filterStrategy != FilterStrategy.NONE) {
 
 				val uriMatched = (for (configPattern <- configuration.patterns) yield {
@@ -160,11 +149,11 @@ object RecorderController extends Logging {
 						case PatternType.ANT => SelectorUtils.ANT_HANDLER_PREFIX
 						case PatternType.JAVA => SelectorUtils.REGEX_HANDLER_PREFIX
 					}
-					
+
 					SelectorUtils.matchPath(pattern + configPattern.pattern + SelectorUtils.PATTERN_HANDLER_SUFFIX, uri.getPath)
 				}).foldLeft(false)(_ || _)
-				
-				if(configuration.filterStrategy == FilterStrategy.ONLY)
+
+				if (configuration.filterStrategy == FilterStrategy.ONLY)
 					uriMatched
 				else
 					!uriMatched
@@ -191,6 +180,4 @@ object RecorderController extends Logging {
 			}
 		})
 	}
-
-	
 }

gatling-recorder/src/main/scala/com/excilys/ebi/gatling/recorder/http/GatlingHttpProxy.scala
@@ -23,18 +23,18 @@ import org.jboss.netty.channel.Channel
 import com.excilys.ebi.gatling.recorder.config.ProxyConfig
 import com.excilys.ebi.gatling.recorder.http.channel.BootstrapFactory.bootstrapFactory
 
-object GatlingHttpProxy{
-	
+object GatlingHttpProxy {
+
 	private var instance: GatlingHttpProxy = null
-	
-	def apply(port: Int, sslPort: Int, proxyConfig: ProxyConfig){
+
+	def apply(port: Int, sslPort: Int, proxyConfig: ProxyConfig) {
 		instance = new GatlingHttpProxy(port, sslPort, proxyConfig)
 		instance.start
 	}
-	
+
 	def shutdown = instance.shutdown
-	
-	def receiveMessage(channel: Channel){
+
+	def receiveMessage(channel: Channel) {
 		instance.onMessageReceived(channel)
 	}
 }

gatling-recorder/src/main/scala/com/excilys/ebi/gatling/recorder/http/channel/BootstrapFactory.scala
@@ -17,29 +17,16 @@ package com.excilys.ebi.gatling.recorder.http.channel;
 
 import java.util.concurrent.Executors
 
-import org.jboss.netty.bootstrap.ClientBootstrap
-import org.jboss.netty.bootstrap.ServerBootstrap
+import org.jboss.netty.bootstrap.{ ServerBootstrap, ClientBootstrap }
 import org.jboss.netty.channel.Channels.pipeline
-import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory
-import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory
-import org.jboss.netty.channel.ChannelHandlerContext
-import org.jboss.netty.channel.ChannelPipeline
-import org.jboss.netty.channel.ChannelPipelineFactory
-import org.jboss.netty.handler.codec.http.HttpChunkAggregator
-import org.jboss.netty.handler.codec.http.HttpClientCodec
-import org.jboss.netty.handler.codec.http.HttpContentCompressor
-import org.jboss.netty.handler.codec.http.HttpContentDecompressor
-import org.jboss.netty.handler.codec.http.HttpRequest
-import org.jboss.netty.handler.codec.http.HttpRequestDecoder
-import org.jboss.netty.handler.codec.http.HttpResponseEncoder
+import org.jboss.netty.channel.socket.nio.{ NioServerSocketChannelFactory, NioClientSocketChannelFactory }
+import org.jboss.netty.channel.{ ChannelPipelineFactory, ChannelPipeline, ChannelHandlerContext }
+import org.jboss.netty.handler.codec.http.{ HttpResponseEncoder, HttpRequestDecoder, HttpRequest, HttpContentDecompressor, HttpContentCompressor, HttpClientCodec, HttpChunkAggregator }
 import org.jboss.netty.handler.ssl.SslHandler
 
 import com.excilys.ebi.gatling.recorder.config.ProxyConfig
-import com.excilys.ebi.gatling.recorder.http.handler.BrowserHttpRequestHandler
-import com.excilys.ebi.gatling.recorder.http.handler.BrowserHttpsRequestHandler
-import com.excilys.ebi.gatling.recorder.http.handler.ServerHttpResponseHandler
-import com.excilys.ebi.gatling.recorder.http.ssl.FirstEventIsUnsecuredConnectSslHandler
-import com.excilys.ebi.gatling.recorder.http.ssl.SSLEngineFactory
+import com.excilys.ebi.gatling.recorder.http.handler.{ ServerHttpResponseHandler, BrowserHttpsRequestHandler, BrowserHttpRequestHandler }
+import com.excilys.ebi.gatling.recorder.http.ssl.{ SSLEngineFactory, FirstEventIsUnsecuredConnectSslHandler }
 
 object BootstrapFactory {
 	val bootstrapFactory = new BootstrapFactory
@@ -47,7 +34,7 @@ object BootstrapFactory {
 
 class BootstrapFactory {
 
-	private val CHUNK_MAX_SIZE = 100 * 1024 * 1024; // 1Mo
+	private val CHUNK_MAX_SIZE = 100 * 1024 * 1024; // 100Mo
 
 	private val threadPool = Executors.newCachedThreadPool
 

gatling-recorder/src/main/scala/com/excilys/ebi/gatling/recorder/http/handler/AbstractBrowserRequestHandler.scala
@@ -15,26 +15,20 @@
  */
 package com.excilys.ebi.gatling.recorder.http.handler;
 
-import org.jboss.netty.channel.ChannelFuture
-import org.jboss.netty.channel.ChannelFutureListener
-import org.jboss.netty.channel.ChannelHandlerContext
-import org.jboss.netty.channel.ExceptionEvent
-import org.jboss.netty.channel.MessageEvent
-import org.jboss.netty.channel.SimpleChannelHandler
-import org.jboss.netty.handler.codec.http.HttpRequest
-import org.jboss.netty.handler.codec.http.DefaultHttpRequest
+import java.net.URI
+
+import scala.collection.JavaConversions.asScalaBuffer
+
+import org.jboss.netty.channel.{ SimpleChannelHandler, MessageEvent, ExceptionEvent, ChannelHandlerContext, ChannelFutureListener, ChannelFuture }
+import org.jboss.netty.handler.codec.http.{ HttpRequest, DefaultHttpRequest }
 
 import com.excilys.ebi.gatling.recorder.controller.RecorderController
 import com.excilys.ebi.gatling.recorder.http.GatlingHttpProxy
 
-import scala.collection.JavaConversions._
-
 import grizzled.slf4j.Logging
 
-import java.net.URI
-
-abstract class AbstractBrowserRequestHandler(val outgoingProxyHost: Option[String], val outgoingProxyPort: Option[Int]) 
-	extends SimpleChannelHandler with Logging {
+abstract class AbstractBrowserRequestHandler(val outgoingProxyHost: Option[String], val outgoingProxyPort: Option[Int])
+		extends SimpleChannelHandler with Logging {
 
 	override def messageReceived(ctx: ChannelHandlerContext, event: MessageEvent) {
 
@@ -43,7 +37,7 @@ abstract class AbstractBrowserRequestHandler(val outgoingProxyHost: Option[Strin
 		val request = event.getMessage.asInstanceOf[HttpRequest]
 
 		// remove Proxy-Connection header if it's not significant
-		if(outgoingProxyHost.isEmpty){
+		if (outgoingProxyHost.isEmpty) {
 			request.removeHeader("Proxy-Connection")
 		}
 
@@ -73,7 +67,7 @@ abstract class AbstractBrowserRequestHandler(val outgoingProxyHost: Option[Strin
 				def operationComplete(future: ChannelFuture) = future.getChannel.write(buildRequestWithRelativeURI(request))
 			})
 	}
-	
+
 	private def buildRequestWithRelativeURI(request: HttpRequest) = {
 		val uri = new URI(request.getUri)
 		val newUri = new URI(null, null, null, -1, uri.getPath, uri.getQuery, uri.getFragment).toString

gatling-recorder/src/main/scala/com/excilys/ebi/gatling/recorder/http/handler/BrowserHttpRequestHandler.scala
@@ -15,11 +15,9 @@
  */
 package com.excilys.ebi.gatling.recorder.http.handler;
 
-import java.net.InetSocketAddress
-import java.net.URI
+import java.net.{ URI, InetSocketAddress }
 
-import org.jboss.netty.channel.ChannelFuture
-import org.jboss.netty.channel.ChannelHandlerContext
+import org.jboss.netty.channel.{ ChannelHandlerContext, ChannelFuture }
 import org.jboss.netty.handler.codec.http.HttpRequest
 
 import com.excilys.ebi.gatling.recorder.config.ProxyConfig
@@ -31,12 +29,16 @@ class BrowserHttpRequestHandler(proxyConfig: ProxyConfig) extends AbstractBrowse
 
 		val bootstrap = bootstrapFactory.newClientBootstrap(ctx, request, false)
 
-		if(outgoingProxyHost.isDefined && outgoingProxyPort.isDefined) {
-			bootstrap.connect(new InetSocketAddress(outgoingProxyHost.get, outgoingProxyPort.get))
-		} else {
-			val uri = new URI(request.getUri)
-			val port = if(uri.getPort == -1) 80 else uri.getPort
-			bootstrap.connect(new InetSocketAddress(uri.getHost, port))
-		}
+		val (proxyHost, proxyPort) = (for {
+			host <- outgoingProxyHost
+			port <- outgoingProxyPort
+		} yield (host, port))
+			.getOrElse {
+				val uri = new URI(request.getUri)
+				val port = if (uri.getPort == -1) 80 else uri.getPort
+				(uri.getHost, port)
+			}
+
+		bootstrap.connect(new InetSocketAddress(proxyHost, proxyPort))
 	}
 }

commit 6a94635b679ad050adce7ca55c78ef9bf5425dca

Fix regression on Simulation class name introduced in b950667109d62e35ea81a2bb3fdbfd81f5f07fff

gatling-recorder/src/main/scala/com/excilys/ebi/gatling/recorder/config/Configuration.scala
@@ -33,16 +33,18 @@ import grizzled.slf4j.Logging
 object Configuration extends Logging {
 
 	GatlingConfiguration.setUp(None, None, None, None, None)
-
-	val configuration = new Configuration
 	val DEFAULT_CLASS_NAME = "Simulation"
 
-	private val XSTREAM = new XStream(new DomDriver)
+	private val XSTREAM = {
+		val xstream = new XStream(new DomDriver)
+		xstream.alias("configuration", classOf[Configuration])
+		xstream.alias("pattern", classOf[Pattern])
+		xstream.alias("proxy", classOf[ProxyConfig])
+		xstream
+	}
 	private val CONFIGURATION_FILE = File(System.getProperty("user.home") / GATLING_RECORDER_FILE_NAME)
 
-	XSTREAM.alias("configuration", classOf[Configuration])
-	XSTREAM.alias("pattern", classOf[Pattern])
-	XSTREAM.alias("proxy", classOf[ProxyConfig])
+	val configuration = new Configuration
 
 	def apply(options: Options) {
 		initFromDisk

gatling-recorder/src/main/scala/com/excilys/ebi/gatling/recorder/ui/frame/ValidationHelper.scala
@@ -19,6 +19,8 @@ import java.awt.event.{ KeyListener, KeyEvent }
 import java.awt.Color
 import java.util.Date
 
+import scala.collection.mutable
+
 import com.excilys.ebi.gatling.recorder.ui.util.UIHelper.useUIThread
 
 import grizzled.slf4j.Logging
@@ -26,11 +28,11 @@ import javax.swing.{ JTextField, BorderFactory }
 
 object ValidationHelper extends Logging {
 
-	val standardBorder = BorderFactory.createMatteBorder(1, 1, 1, 1, Color.darkGray)
-	val errorBorder = BorderFactory.createMatteBorder(2, 2, 2, 2, Color.red)
-	val disabledBorder = BorderFactory.createMatteBorder(1, 1, 1, 1, new Color(184, 207, 229))
+	private val standardBorder = BorderFactory.createMatteBorder(1, 1, 1, 1, Color.darkGray)
+	private val errorBorder = BorderFactory.createMatteBorder(2, 2, 2, 2, Color.red)
+	private val disabledBorder = BorderFactory.createMatteBorder(1, 1, 1, 1, new Color(184, 207, 229))
 
-	var validationStatus = Map.empty[String, Boolean]
+	private val validationStatus = mutable.Map.empty[String, Boolean]
 
 	// TODO: move table validation here
 
@@ -99,12 +101,12 @@ object ValidationHelper extends Logging {
 		def keyTyped(e: KeyEvent) {}
 	}
 
-	def updateValidationStatus(id: String, status: Boolean, cfgFrame: ConfigurationFrame) {
+	private def updateValidationStatus(id: String, status: Boolean, cfgFrame: ConfigurationFrame) {
 		validationStatus += (id -> status)
 		updateStartButtonStatus(cfgFrame)
 	}
 
-	def updateStartButtonStatus(cfgFrame: ConfigurationFrame) {
+	private def updateStartButtonStatus(cfgFrame: ConfigurationFrame) {
 		val newStatus = validationStatus.values.foldLeft(true)((b1, b2) => b1 && b2)
 		cfgFrame.btnStart.setEnabled(newStatus)
 	}