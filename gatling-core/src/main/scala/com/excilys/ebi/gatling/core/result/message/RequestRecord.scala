79f97f0c952458eb13558bdf43d3379a744513f5
Stephen Kuenzli <skuenzli@qualimente.com>
2012-06-09 09:54:37 -0700

    Modify RequestRecord to accept extra information about the request, optionally.

--- gatling-core/src/main/scala/com/excilys/ebi/gatling/core/result/message/RequestRecord.scala
+++ gatling-core/src/main/scala/com/excilys/ebi/gatling/core/result/message/RequestRecord.scala
@@ -30,5 +30,11 @@ import com.excilys.ebi.gatling.core.result.message.RecordType.ACTION
  * @param responseReceivingStartDate the date on which the response was started being received
  * @param requestStatus the status of the action
  * @param requestMessage the message of the action on completion
+ * @param extraRequestInfo information about this request extracted via user-defined functions
  */
-case class RequestRecord(scenarioName: String, userId: Int, requestName: String, executionStartDate: Long, executionEndDate: Long, requestSendingEndDate: Long, responseReceivingStartDate: Long, requestStatus: RequestStatus.RequestStatus, requestMessage: String) extends Record(ACTION)
+case class RequestRecord(scenarioName: String, userId: Int, requestName: String,
+                         executionStartDate: Long, executionEndDate: Long,
+                         requestSendingEndDate: Long, responseReceivingStartDate: Long,
+                         requestStatus: RequestStatus.RequestStatus, requestMessage: String,
+                         extraRequestInfo: List[String] = List())
+  extends Record(ACTION)
---
+++ gatling-core/src/test/scala/com/excilys/ebi/gatling/core/result/message/RequestRecordTest.scala
@@ -0,0 +1,19 @@
+package com.excilys.ebi.gatling.core.result.message
+
+import org.specs2.mutable.Specification
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import com.excilys.ebi.gatling.core.result.message.RequestStatus._
+
+@RunWith(classOf[JUnitRunner])
+class RequestRecordTest extends Specification {
+
+  "constructor" should {
+    "have sensible defaults for optional parameters" in {
+      val record: RequestRecord = RequestRecord("scenarioName", 1, "requestName", 0L, 0L, 0L, 0L, OK, "requestMessage")
+
+      record.extraRequestInfo should beEmpty
+    }
+
+  }
+}
