f92f4603f8caa54b61d09b6480816932642061b2
Gabe Hicks <gabe.hicks@gmail.com>
2012-08-17 08:38:06 -0700

    Reworked upload to use a list of attachments instead of a single.

--- gatling-http/src/main/scala/com/excilys/ebi/gatling/http/request/builder/HttpRequestBaseBuilder.scala
+++ gatling-http/src/main/scala/com/excilys/ebi/gatling/http/request/builder/HttpRequestBaseBuilder.scala
@@ -76,14 +76,14 @@ class HttpRequestBaseBuilder(val requestName: String) {
 	 * @param url the url on which this request will be made
 	 * @param interpolations session keys for interpolation
 	 */
-	def post(url: String) = new PostHttpRequestBuilder(requestName, parseEvaluatable(url), Nil, Nil, Map.empty, None, None, None, Nil)
+	def post(url: String) = new PostHttpRequestBuilder(requestName, parseEvaluatable(url), Nil, Nil, Map.empty, None, Nil, None, Nil)
 
 	/**
 	 * Starts the definition of an HTTP request with word POST
 	 *
 	 * @param f the function returning the url of this request
 	 */
-	def post(f: EvaluatableString) = new PostHttpRequestBuilder(requestName, f, Nil, Nil, Map.empty, None, None, None, Nil)
+	def post(f: EvaluatableString) = new PostHttpRequestBuilder(requestName, f, Nil, Nil, Map.empty, None, Nil, None, Nil)
 
 	/**
 	 * Starts the definition of an HTTP request with word PUT
