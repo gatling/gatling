0435f3208d049246665d24d71ee2aaf4ec83cf29
Stephane Landelle <slandelle+github@excilys.com>
2012-09-18 16:37:06 +0200

    Make baseURLs take a varag and not a Seq, compliat with doc

--- gatling-http/src/main/scala/com/excilys/ebi/gatling/http/config/HttpProtocolConfigurationBuilder.scala
+++ gatling-http/src/main/scala/com/excilys/ebi/gatling/http/config/HttpProtocolConfigurationBuilder.scala
@@ -17,7 +17,7 @@ package com.excilys.ebi.gatling.http.config
 
 import com.excilys.ebi.gatling.http.Headers
 import com.excilys.ebi.gatling.http.action.HttpRequestAction.HTTP_CLIENT
-import com.ning.http.client.{ Response, RequestBuilder, Request, ProxyServer }
+import com.ning.http.client.{ ProxyServer, Request, RequestBuilder, Response }
 
 import grizzled.slf4j.Logging
 
@@ -25,13 +25,25 @@ import grizzled.slf4j.Logging
  * HttpProtocolConfigurationBuilder class companion
  */
 object HttpProtocolConfigurationBuilder {
-	
-	private[gatling] val BASE_HTTP_PROTOCOL_CONFIGURATION_BUILDER = new HttpProtocolConfigurationBuilder(None, None, None, true, true, true, true, Map.empty, None, None, None)
-	
+
+	private[gatling] val BASE_HTTP_PROTOCOL_CONFIGURATION_BUILDER = new HttpProtocolConfigurationBuilder(Attributes(None, None, None, true, true, true, true, Map.empty, None, None, None))
+
 	def httpConfig = BASE_HTTP_PROTOCOL_CONFIGURATION_BUILDER.warmUp("http://gatling-tool.org")
 
 	implicit def toHttpProtocolConfiguration(builder: HttpProtocolConfigurationBuilder) = builder.build
 }
+
+private case class Attributes(baseUrls: Option[List[String]],
+	proxy: Option[ProxyServer],
+	securedProxy: Option[ProxyServer],
+	followRedirectEnabled: Boolean,
+	automaticRefererEnabled: Boolean,
+	cachingEnabled: Boolean,
+	responseChunksDiscardingEnabled: Boolean,
+	baseHeaders: Map[String, String],
+	warmUpUrl: Option[String],
+	extraRequestInfoExtractor: Option[(Request => List[String])],
+	extraResponseInfoExtractor: Option[(Response => List[String])])
 
 /**
  * Builder for HttpProtocolConfiguration used in DSL
@@ -39,54 +51,44 @@ object HttpProtocolConfigurationBuilder {
  * @param baseUrl the radix of all the URLs that will be used (eg: http://mywebsite.tld)
  * @param proxy a proxy through which all the requests must pass to succeed
  */
-class HttpProtocolConfigurationBuilder(baseUrls: Option[Seq[String]],
-		proxy: Option[ProxyServer],
-		securedProxy: Option[ProxyServer],
-		followRedirectEnabled: Boolean,
-		automaticRefererEnabled: Boolean,
-		cachingEnabled: Boolean,
-		responseChunksDiscardingEnabled: Boolean,
-		baseHeaders: Map[String, String],
-		warmUpUrl: Option[String],
-		extraRequestInfoExtractor: Option[(Request => List[String])],
-		extraResponseInfoExtractor: Option[(Response => List[String])]) extends Logging {
+class HttpProtocolConfigurationBuilder(attributes: Attributes) extends Logging {
 
 	/**
 	 * Sets the baseURL of the future HttpProtocolConfiguration
 	 *
 	 * @param baseUrl the base url that will be set
 	 */
-	def baseURL(baseUrl: String) = new HttpProtocolConfigurationBuilder(Some(List(baseUrl)), proxy, securedProxy, followRedirectEnabled, automaticRefererEnabled, cachingEnabled, responseChunksDiscardingEnabled, baseHeaders, warmUpUrl, extraRequestInfoExtractor, extraResponseInfoExtractor)
+	def baseURL(baseUrl: String) = new HttpProtocolConfigurationBuilder(attributes.copy(baseUrls = Some(List(baseUrl))))
 
-	def baseURLs(baseUrls: Seq[String]) = new HttpProtocolConfigurationBuilder(Some(baseUrls), proxy, securedProxy, followRedirectEnabled, automaticRefererEnabled, cachingEnabled, responseChunksDiscardingEnabled, baseHeaders, warmUpUrl, extraRequestInfoExtractor, extraResponseInfoExtractor)
+	def baseURLs(baseUrl1: String, baseUrl2: String, baseUrls: String*) = new HttpProtocolConfigurationBuilder(attributes.copy(baseUrls = Some(baseUrl1 :: baseUrl2 :: baseUrls.toList)))
 
-	def disableFollowRedirect = new HttpProtocolConfigurationBuilder(baseUrls, proxy, securedProxy, false, automaticRefererEnabled, cachingEnabled, responseChunksDiscardingEnabled, baseHeaders, warmUpUrl, extraRequestInfoExtractor, extraResponseInfoExtractor)
+	def disableFollowRedirect = new HttpProtocolConfigurationBuilder(attributes.copy(followRedirectEnabled = false))
 
-	def disableAutomaticReferer = new HttpProtocolConfigurationBuilder(baseUrls, proxy, securedProxy, followRedirectEnabled, false, cachingEnabled, responseChunksDiscardingEnabled, baseHeaders, warmUpUrl, extraRequestInfoExtractor, extraResponseInfoExtractor)
+	def disableAutomaticReferer = new HttpProtocolConfigurationBuilder(attributes.copy(automaticRefererEnabled = false))
 
-	def disableCaching = new HttpProtocolConfigurationBuilder(baseUrls, proxy, securedProxy, followRedirectEnabled, automaticRefererEnabled, false, responseChunksDiscardingEnabled, baseHeaders, warmUpUrl, extraRequestInfoExtractor, extraResponseInfoExtractor)
+	def disableCaching = new HttpProtocolConfigurationBuilder(attributes.copy(cachingEnabled = false))
 
-	def disableResponseChunksDiscarding = new HttpProtocolConfigurationBuilder(baseUrls, proxy, securedProxy, followRedirectEnabled, automaticRefererEnabled, cachingEnabled, false, baseHeaders, warmUpUrl, extraRequestInfoExtractor, extraResponseInfoExtractor)
+	def disableResponseChunksDiscarding = new HttpProtocolConfigurationBuilder(attributes.copy(responseChunksDiscardingEnabled = false))
 
-	def acceptHeader(value: String) = new HttpProtocolConfigurationBuilder(baseUrls, proxy, securedProxy, followRedirectEnabled, automaticRefererEnabled, cachingEnabled, responseChunksDiscardingEnabled, baseHeaders + (Headers.Names.ACCEPT -> value), warmUpUrl, extraRequestInfoExtractor, extraResponseInfoExtractor)
+	def acceptHeader(value: String) = new HttpProtocolConfigurationBuilder(attributes.copy(baseHeaders = attributes.baseHeaders + (Headers.Names.ACCEPT -> value)))
 
-	def acceptCharsetHeader(value: String) = new HttpProtocolConfigurationBuilder(baseUrls, proxy, securedProxy, followRedirectEnabled, automaticRefererEnabled, cachingEnabled, responseChunksDiscardingEnabled, baseHeaders + (Headers.Names.ACCEPT_CHARSET -> value), warmUpUrl, extraRequestInfoExtractor, extraResponseInfoExtractor)
+	def acceptCharsetHeader(value: String) = new HttpProtocolConfigurationBuilder(attributes.copy(baseHeaders = attributes.baseHeaders + (Headers.Names.ACCEPT_CHARSET -> value)))
 
-	def acceptEncodingHeader(value: String) = new HttpProtocolConfigurationBuilder(baseUrls, proxy, securedProxy, followRedirectEnabled, automaticRefererEnabled, cachingEnabled, responseChunksDiscardingEnabled, baseHeaders + (Headers.Names.ACCEPT_ENCODING -> value), warmUpUrl, extraRequestInfoExtractor, extraResponseInfoExtractor)
+	def acceptEncodingHeader(value: String) = new HttpProtocolConfigurationBuilder(attributes.copy(baseHeaders = attributes.baseHeaders + (Headers.Names.ACCEPT_ENCODING -> value)))
 
-	def acceptLanguageHeader(value: String) = new HttpProtocolConfigurationBuilder(baseUrls, proxy, securedProxy, followRedirectEnabled, automaticRefererEnabled, cachingEnabled, responseChunksDiscardingEnabled, baseHeaders + (Headers.Names.ACCEPT_LANGUAGE -> value), warmUpUrl, extraRequestInfoExtractor, extraResponseInfoExtractor)
+	def acceptLanguageHeader(value: String) = new HttpProtocolConfigurationBuilder(attributes.copy(baseHeaders = attributes.baseHeaders + (Headers.Names.ACCEPT_LANGUAGE -> value)))
 
-	def hostHeader(value: String) = new HttpProtocolConfigurationBuilder(baseUrls, proxy, securedProxy, followRedirectEnabled, automaticRefererEnabled, cachingEnabled, responseChunksDiscardingEnabled, baseHeaders + (Headers.Names.HOST -> value), warmUpUrl, extraRequestInfoExtractor, extraResponseInfoExtractor)
+	def hostHeader(value: String) = new HttpProtocolConfigurationBuilder(attributes.copy(baseHeaders = attributes.baseHeaders + (Headers.Names.HOST -> value)))
 
-	def userAgentHeader(value: String) = new HttpProtocolConfigurationBuilder(baseUrls, proxy, securedProxy, followRedirectEnabled, automaticRefererEnabled, cachingEnabled, responseChunksDiscardingEnabled, baseHeaders + (Headers.Names.USER_AGENT -> value), warmUpUrl, extraRequestInfoExtractor, extraResponseInfoExtractor)
+	def userAgentHeader(value: String) = new HttpProtocolConfigurationBuilder(attributes.copy(baseHeaders = attributes.baseHeaders + (Headers.Names.USER_AGENT -> value)))
 
-	def warmUp(warmUpUrl: String) = new HttpProtocolConfigurationBuilder(baseUrls, proxy, securedProxy, followRedirectEnabled, automaticRefererEnabled, cachingEnabled, responseChunksDiscardingEnabled, baseHeaders, Some(warmUpUrl), extraRequestInfoExtractor, extraResponseInfoExtractor)
+	def warmUp(warmUpUrl: String) = new HttpProtocolConfigurationBuilder(attributes.copy(warmUpUrl = Some(warmUpUrl)))
 
-	def disableWarmUp = new HttpProtocolConfigurationBuilder(baseUrls, proxy, securedProxy, followRedirectEnabled, automaticRefererEnabled, cachingEnabled, responseChunksDiscardingEnabled, baseHeaders, None, extraRequestInfoExtractor, extraResponseInfoExtractor)
+	def disableWarmUp = new HttpProtocolConfigurationBuilder(attributes.copy(warmUpUrl = None))
 
-	def requestInfoExtractor(value: (Request => List[String])) = new HttpProtocolConfigurationBuilder(baseUrls, proxy, securedProxy, followRedirectEnabled, automaticRefererEnabled, cachingEnabled, responseChunksDiscardingEnabled, baseHeaders, warmUpUrl, Some(value), extraResponseInfoExtractor)
+	def requestInfoExtractor(value: (Request => List[String])) = new HttpProtocolConfigurationBuilder(attributes.copy(extraRequestInfoExtractor = Some(value)))
 
-	def responseInfoExtractor(value: (Response) => List[String]) = new HttpProtocolConfigurationBuilder(baseUrls, proxy, securedProxy, followRedirectEnabled, automaticRefererEnabled, cachingEnabled, responseChunksDiscardingEnabled, baseHeaders, warmUpUrl, extraRequestInfoExtractor, Some(value))
+	def responseInfoExtractor(value: (Response) => List[String]) = new HttpProtocolConfigurationBuilder(attributes.copy(extraResponseInfoExtractor = Some(value)))
 
 	/**
 	 * Sets the proxy of the future HttpProtocolConfiguration
@@ -96,14 +98,14 @@ class HttpProtocolConfigurationBuilder(baseUrls: Option[Seq[String]],
 	 */
 	def proxy(host: String, port: Int) = new HttpProxyBuilder(this, host, port)
 
-	private[http] def addProxies(httpProxy: ProxyServer, httpsProxy: Option[ProxyServer]) = new HttpProtocolConfigurationBuilder(baseUrls, Some(httpProxy), httpsProxy, followRedirectEnabled, automaticRefererEnabled, cachingEnabled, responseChunksDiscardingEnabled, baseHeaders, warmUpUrl, extraRequestInfoExtractor, extraResponseInfoExtractor)
+	private[http] def addProxies(httpProxy: ProxyServer, httpsProxy: Option[ProxyServer]) = new HttpProtocolConfigurationBuilder(attributes.copy(proxy = Some(httpProxy), securedProxy = httpsProxy))
 
 	private[http] def build = {
-		warmUpUrl.map { url =>
+		attributes.warmUpUrl.map { url =>
 			val requestBuilder = new RequestBuilder().setUrl(url)
 
-			proxy.map { proxy => if (url.startsWith("http://")) requestBuilder.setProxyServer(proxy) }
-			securedProxy.map { proxy => if (url.startsWith("https://")) requestBuilder.setProxyServer(proxy) }
+			attributes.proxy.map { proxy => if (url.startsWith("http://")) requestBuilder.setProxyServer(proxy) }
+			attributes.securedProxy.map { proxy => if (url.startsWith("https://")) requestBuilder.setProxyServer(proxy) }
 
 			try {
 				HTTP_CLIENT.executeRequest(requestBuilder.build).get
@@ -112,6 +114,6 @@ class HttpProtocolConfigurationBuilder(baseUrls: Option[Seq[String]],
 			}
 		}
 
-		HttpProtocolConfiguration(baseUrls, proxy, securedProxy, followRedirectEnabled, automaticRefererEnabled, cachingEnabled, responseChunksDiscardingEnabled, baseHeaders, extraRequestInfoExtractor, extraResponseInfoExtractor)
+		HttpProtocolConfiguration(attributes.baseUrls, attributes.proxy, attributes.securedProxy, attributes.followRedirectEnabled, attributes.automaticRefererEnabled, attributes.cachingEnabled, attributes.responseChunksDiscardingEnabled, attributes.baseHeaders, attributes.extraRequestInfoExtractor, attributes.extraResponseInfoExtractor)
 	}
 }
