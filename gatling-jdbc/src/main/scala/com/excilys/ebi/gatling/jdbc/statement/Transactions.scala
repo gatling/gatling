57bca9b18c286e9ef7e2a994050813c330542595
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2012-12-01 21:39:54 +0100

    placeholder for transactions

---
+++ gatling-jdbc/src/main/scala/com/excilys/ebi/gatling/jdbc/statement/Transactions.scala
@@ -0,0 +1,8 @@
+package com.excilys.ebi.gatling.jdbc.statement
+
+import com.excilys.ebi.gatling.core.structure.ChainBuilder
+
+object Transactions {
+
+	def transaction(chain : ChainBuilder) = throw new UnsupportedOperationException("Not implemented yet")
+}
