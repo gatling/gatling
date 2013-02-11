8aca68d6c16a0eebf1ae6b3f860d324c058f44f6
Stephane Landelle <slandelle+github@excilys.com>
2013-02-11 12:53:27 +0100

    Remove random tests that of course sometimes fail, no need to test non Gatling code

--- gatling-core/src/test/scala/com/excilys/ebi/gatling/core/util/NumberHelperSpec.scala
+++ gatling-core/src/test/scala/com/excilys/ebi/gatling/core/util/NumberHelperSpec.scala
@@ -17,11 +17,7 @@ package com.excilys.ebi.gatling.core.util
 
 import scala.math.abs
 
-import org.apache.commons.math3.distribution.{ UniformIntegerDistribution, ExponentialDistribution }
 import org.apache.commons.math3.exception.NotStrictlyPositiveException
-import org.apache.commons.math3.random.EmpiricalDistribution
-import org.apache.commons.math3.stat.descriptive.StatisticalSummary
-import org.apache.commons.math3.stat.StatUtils
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
@@ -34,49 +30,6 @@ class NumberHelperSpec extends Specification {
 		"be zero when min and max are zero" in {
 			NumberHelper.createUniformRandomLongGenerator(0, 0)() must beEqualTo(0)
 		}
-
-		"produce uniformly-distributed random numbers within the specified range" in {
-			val min: Int = 0
-			val max: Int = 100
-			val expectedAverage: Double = (max + min) / 2
-			val numSamples: Int = 1000
-			val numTests = 100
-			var timesMatchingDistribution = 0
-
-			val uniformDistribution: UniformIntegerDistribution = new UniformIntegerDistribution(min, max)
-
-			for (n <- 0 until numTests) {
-				var samples = List[Double]()
-
-				val uniformRandomLongGenerator = NumberHelper.createUniformRandomLongGenerator(min, max)
-
-				for (i <- 0 until numSamples) {
-					val uniformSample: Long = uniformRandomLongGenerator()
-					samples ::= uniformRandomLongGenerator()
-
-					uniformSample must beGreaterThanOrEqualTo(min.toLong)
-					uniformSample must beLessThanOrEqualTo(max.toLong)
-				}
-
-				val samplesArray: Array[Double] = samples.toArray
-				val empiricalDistribution = new EmpiricalDistribution
-				empiricalDistribution.load(samplesArray)
-				val sampleStats: StatisticalSummary = empiricalDistribution.getSampleStats
-				val twentyFifthPercentileVal: Long = StatUtils.percentile(samplesArray, 25.0).round
-				val seventyFifthPercentileVal: Long = StatUtils.percentile(samplesArray, 75.0).round
-
-				if (sampleStats.getN == numSamples
-					&& isCloseTo(sampleStats.getMean, expectedAverage, 2)
-					&& isCloseTo(uniformDistribution.cumulativeProbability(twentyFifthPercentileVal.toInt), 0.25, 0.15)
-					&& isCloseTo(uniformDistribution.cumulativeProbability(seventyFifthPercentileVal.toInt), 0.75, 0.15)) {
-					timesMatchingDistribution += 1
-				}
-
-			}
-
-			val percentTestsMatchingDistribution: Double = timesMatchingDistribution.toDouble / numTests.toDouble
-			percentTestsMatchingDistribution must beGreaterThanOrEqualTo(0.90)
-		}
 	}
 
 	"getRandomDoubleFromExponential" should {
@@ -84,82 +37,12 @@ class NumberHelperSpec extends Specification {
 		"throw exception when expected average is zero" in {
 			NumberHelper.createExpRandomDoubleGenerator(0) should throwA[NotStrictlyPositiveException]
 		}
-
-		"produce exponentially-distributed random doubles around the specified average" in {
-			val expectedAverage: Long = 10
-			val numSamples: Int = 1000
-			val numTests = 100
-			var timesMatchingDistribution = 0
-
-			val exponentialDistribution: ExponentialDistribution = new ExponentialDistribution(expectedAverage)
-
-			for (n <- 0 until numTests) {
-				var samples = List[Double]()
-
-				val expRandomDoubleGenerator = NumberHelper.createExpRandomDoubleGenerator(expectedAverage)
-				for (i <- 0 until numSamples) {
-					samples ::= expRandomDoubleGenerator()
-				}
-
-				val samplesArray: Array[Double] = samples.toArray
-				val empiricalDistribution = new EmpiricalDistribution
-				empiricalDistribution.load(samplesArray)
-				val sampleStats: StatisticalSummary = empiricalDistribution.getSampleStats
-				val twentyFifthPercentileVal: Double = StatUtils.percentile(samplesArray, 25.0)
-				val seventyFifthPercentileVal: Double = StatUtils.percentile(samplesArray, 75.0)
-
-				if (sampleStats.getN == numSamples
-					&& isCloseTo(sampleStats.getMean, expectedAverage, 1)
-					&& isCloseTo(exponentialDistribution.cumulativeProbability(twentyFifthPercentileVal), 0.25, 0.05)
-					&& isCloseTo(exponentialDistribution.cumulativeProbability(seventyFifthPercentileVal), 0.75, 0.05)) {
-					timesMatchingDistribution += 1
-				}
-			}
-
-			val percentTestsMatchingDistribution: Double = timesMatchingDistribution.toDouble / numTests.toDouble
-			percentTestsMatchingDistribution must beGreaterThanOrEqualTo(0.90)
-		}
 	}
 
 	"getRandomLongFromExponential" should {
 
 		"throw exception when expected average is zero" in {
 			NumberHelper.createExpRandomLongGenerator(0) should throwA[NotStrictlyPositiveException]
-		}
-
-		"produce exponentially-distributed random longs around the specified average" in {
-			val expectedAverage: Long = 10
-			val numSamples: Int = 1000
-			val numTests = 100
-			var timesMatchingDistribution = 0
-
-			val exponentialDistribution: ExponentialDistribution = new ExponentialDistribution(expectedAverage)
-
-			for (n <- 0 until numTests) {
-				var samples = List[Double]()
-
-				val expRandomLongGenerator = NumberHelper.createExpRandomLongGenerator(expectedAverage)
-				for (i <- 0 until numSamples) {
-					samples ::= expRandomLongGenerator()
-				}
-
-				val samplesArray: Array[Double] = samples.toArray
-				val empiricalDistribution = new EmpiricalDistribution
-				empiricalDistribution.load(samplesArray)
-				val sampleStats: StatisticalSummary = empiricalDistribution.getSampleStats
-				val twentyFifthPercentileVal: Double = StatUtils.percentile(samplesArray, 25.0)
-				val seventyFifthPercentileVal: Double = StatUtils.percentile(samplesArray, 75.0)
-
-				if (sampleStats.getN == numSamples
-					&& isCloseTo(sampleStats.getMean, expectedAverage, 1)
-					&& isCloseTo(exponentialDistribution.cumulativeProbability(twentyFifthPercentileVal), 0.25, 0.05)
-					&& isCloseTo(exponentialDistribution.cumulativeProbability(seventyFifthPercentileVal), 0.75, 0.05)) {
-					timesMatchingDistribution += 1
-				}
-			}
-
-			val percentTestsMatchingDistribution: Double = timesMatchingDistribution.toDouble / numTests.toDouble
-			percentTestsMatchingDistribution must beGreaterThanOrEqualTo(0.90)
 		}
 	}
 
