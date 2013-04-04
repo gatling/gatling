gatling/gatling

commit 57ba3ee57d0b1490a1fcb9b2272da53c616e5532

Lazy load DataReader

gatling-app/src/main/scala/io/gatling/app/Gatling.scala
@@ -101,10 +101,10 @@ class Gatling {
 			(runId, Some(simulation))
 		}
 
-		val dataReader = DataReader.newInstance(outputDirectoryName)
+		lazy val dataReader = DataReader.newInstance(outputDirectoryName)
 
 		val result = simulation match {
-			case Some(simulation) => if (applyAssertions(simulation, dataReader)) Gatling.SUCCESS else Gatling.SIMULATION_ASSERTIONS_FAILED
+			case Some(simulation) if !simulation.assertions.isEmpty => if (applyAssertions(simulation, dataReader)) Gatling.SUCCESS else Gatling.SIMULATION_ASSERTIONS_FAILED
 			case None => Gatling.SUCCESS
 		}
 
@@ -169,7 +169,7 @@ class Gatling {
 	 *
 	 * @param outputDirectoryName The directory from which the simulation.log will be parsed
 	 */
-	private def generateReports(outputDirectoryName: String, dataReader: DataReader) {
+	private def generateReports(outputDirectoryName: String, dataReader: => DataReader) {
 		println("Generating reports...")
 		val start = currentTimeMillis
 		val indexFile = ReportsGenerator.generateFor(outputDirectoryName, dataReader)

gatling-charts/src/main/scala/io/gatling/charts/result/reader/FileDataReader.scala
@@ -44,6 +44,8 @@ class FileDataReader(runUuid: String) extends DataReader(runUuid) with Logging {
 
 	private def multipleFileIterator(streams: Seq[InputStream]): Iterator[String] = streams.map(Source.fromInputStream(_, configuration.simulation.encoding).getLines).reduce((first, second) => first ++ second)
 
+	println("Parsing log file(s)...")
+	
 	val inputFiles = simulationLogDirectory(runUuid, create = false).files
 		.collect { case file if (file.name.matches(FileDataReader.simulationFilesNamePattern)) => file.jfile }
 		.toList
@@ -122,6 +124,8 @@ class FileDataReader(runUuid: String) extends DataReader(runUuid) with Logging {
 	}
 
 	val resultsHolder = doWithInputFiles(process(bucketFunction))
+	
+	println("Parsing log file(s) done")
 
 	def groupsAndRequests: List[(Option[Group], Option[String])] =
 		resultsHolder.groupAndRequestsNameBuffer.map.toList.map {

commit 7fd21201d74393278826d56523f9a655d6a5748f

missing case in pattern matching, regression introduced by 57ba3ee57d0b1490a1fcb9b2272da53c616e5532

gatling-app/src/main/scala/io/gatling/app/Gatling.scala
@@ -105,7 +105,7 @@ class Gatling {
 
 		val result = simulation match {
 			case Some(simulation) if !simulation.assertions.isEmpty => if (applyAssertions(simulation, dataReader)) Gatling.SUCCESS else Gatling.SIMULATION_ASSERTIONS_FAILED
-			case None => Gatling.SUCCESS
+			case _ => Gatling.SUCCESS
 		}
 
 		if (!configuration.charting.noReports) generateReports(outputDirectoryName, dataReader)