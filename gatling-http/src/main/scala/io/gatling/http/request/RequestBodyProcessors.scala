b5479c255be1d1138bd4dfe9e5881ae7615dd151
Stephane Landelle <slandelle+github@excilys.com>
2013-04-06 14:44:36 +0200

    Minor clean up

--- gatling-http/src/main/scala/io/gatling/http/request/RequestBodyProcessors.scala
+++ gatling-http/src/main/scala/io/gatling/http/request/RequestBodyProcessors.scala
@@ -25,18 +25,12 @@ import io.gatling.http.util.GZIPHelper
 object RequestBodyProcessors {
 
 	val gzipRequestBody = (body: HttpRequestBody) => {
+
 		val gzippedBytes = body match {
-
 			case StringBody(string) => (session: Session) => string(session).map(GZIPHelper.gzip)
-
 			case ByteArrayBody(byteArray) => (session: Session) => byteArray(session).map(GZIPHelper.gzip)
-
-			case RawFileBody(file) => (session: Session) => file(session).map { file =>
-				withCloseable(new FileInputStream(file))(GZIPHelper.gzip)
-			}
-			case InputStreamBody(inputStream) => (session: Session) => inputStream(session).map { is =>
-				withCloseable(is)(GZIPHelper.gzip)
-			}
+			case RawFileBody(file) => (session: Session) => file(session).map { f => withCloseable(new FileInputStream(f))(GZIPHelper.gzip) }
+			case InputStreamBody(inputStream) => (session: Session) => inputStream(session).map { withCloseable(_)(GZIPHelper.gzip) }
 			case body => throw new IllegalArgumentException(s"Body $body is not supported by requestCompressor")
 		}
 
@@ -44,18 +38,15 @@ object RequestBodyProcessors {
 	}
 
 	val streamRequestBody = (body: HttpRequestBody) => {
+
 		val stream = body match {
-
 			case StringBody(string) => (session: Session) => string(session).map(s => new BufferedInputStream(new ByteArrayInputStream(s.getBytes(configuration.simulation.encoding))))
-
 			case ByteArrayBody(byteArray) => (session: Session) => byteArray(session).map(b => new BufferedInputStream(new ByteArrayInputStream(b)))
-
 			case RawFileBody(file) => (session: Session) => file(session).map(f => new BufferedInputStream(new FileInputStream(f)))
-
 			case InputStreamBody(inputStream) => inputStream
-
 			case body => throw new IllegalArgumentException(s"Body $body is not supported by streamRequestBody")
 		}
+
 		InputStreamBody(stream)
 	}
 }
