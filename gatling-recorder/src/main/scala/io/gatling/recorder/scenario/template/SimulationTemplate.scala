059c8893f3f5c84fd48e652a105aae96680f072a
Stephane Landelle <slandelle+github@excilys.com>
2013-04-08 17:24:16 +0200

    Expose predefined HTPP header values

--- gatling-recorder/src/main/scala/io/gatling/recorder/scenario/template/SimulationTemplate.scala
+++ gatling-recorder/src/main/scala/io/gatling/recorder/scenario/template/SimulationTemplate.scala
@@ -91,6 +91,7 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.jdbc.Predef._
 import io.gatling.http.Headers.Names._
+import io.gatling.http.Headers.Values._
 import io.gatling.http.request.RequestBodyProcessors._
 import scala.concurrent.duration._
 import bootstrap._
