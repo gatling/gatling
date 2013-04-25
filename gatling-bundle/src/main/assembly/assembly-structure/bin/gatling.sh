f9b5f1fc447640429cb49588634ae48ccdd320ac
Stephane Landelle <slandelle+github@excilys.com>
2013-04-25 23:51:02 +0200

    Update Copyright

--- gatling-bundle/src/main/assembly/assembly-structure/bin/gatling.sh
+++ gatling-bundle/src/main/assembly/assembly-structure/bin/gatling.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #
-# Copyright 2011-2012 eBusiness Information, Groupe Excilys (www.excilys.com)
+# Copyright 2011-2013 eBusiness Information, Groupe Excilys (www.ebusinessinformation.fr)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#   	http://www.apache.org/licenses/LICENSE-2.0
+# 		http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
