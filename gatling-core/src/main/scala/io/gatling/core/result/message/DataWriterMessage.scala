85110fd1c4d289a7e54ddd31ccaf15b17bb28c0f
Stephane Landelle <slandelle+github@excilys.com>
2013-04-30 22:18:16 +0200

    Don't recompute runId

--- gatling-core/src/main/scala/io/gatling/core/result/message/DataWriterMessage.scala
+++ gatling-core/src/main/scala/io/gatling/core/result/message/DataWriterMessage.scala
@@ -46,7 +46,7 @@ case class RequestMessage(
 case class RunMessage(runDate: DateTime, simulationId: String, runDescription: String) extends DataWriterMessage {
 	val recordType = RunMessageType
 	val timestamp = runDate.toTimestamp
-	def runId = simulationId + "-" + runDate.toTimestamp
+	lazy val runId = simulationId + "-" + timestamp
 }
 
 case class ScenarioMessage(scenarioName: String, userId: Int, event: MessageEvent, startDate: Long, endDate: Long) extends DataWriterMessage {
