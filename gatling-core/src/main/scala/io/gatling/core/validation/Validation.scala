63d651dfcb8fa8956e13b622ce4a98239e1c27a5
Stephane Landelle <slandelle+github@excilys.com>
2013-07-04 16:18:57 +0200

    Implement Validation.filter, close #1264

--- gatling-core/src/main/scala/io/gatling/core/validation/Validation.scala
+++ gatling-core/src/main/scala/io/gatling/core/validation/Validation.scala
@@ -20,16 +20,19 @@ sealed trait Validation[+T] {
 	def flatMap[A](f: T => Validation[A]): Validation[A]
 	def mapError(f: String => String): Validation[T]
 	def foreach[A](f: T => A) { map(f) }
+	def filter(p: T => Boolean): Validation[T]
 }
 
 case class Success[+T](value: T) extends Validation[T] {
 	def map[A](f: T => A): Validation[A] = Success(f(value))
 	def flatMap[A](f: T => Validation[A]): Validation[A] = f(value)
 	def mapError(f: String => String): Validation[T] = this
+	def filter(p: T => Boolean): Validation[T] = if (p(value)) this else Failure("Filtered")
 }
 
 case class Failure(message: String) extends Validation[Nothing] {
 	def map[A](f: Nothing => A): Validation[A] = this
 	def flatMap[A](f: Nothing => Validation[A]): Validation[A] = this
 	def mapError(f: String => String): Validation[Nothing] = Failure(f(message))
+	def filter(p: Nothing => Boolean) = this
 }
