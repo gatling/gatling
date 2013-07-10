5aa9ed40600874a55e5e2074e9d9bfffb5834ccf
Nicolas Rémond <nremond+github@gmail.com>
2013-07-10 19:07:38 +0200

    Sample app now hosted on CloudBees

--- gatling-bundle/src/main/assembly/assembly-structure/user-files/simulations/advanced/AdvancedExampleSimulation.scala
+++ gatling-bundle/src/main/assembly/assembly-structure/user-files/simulations/advanced/AdvancedExampleSimulation.scala
@@ -8,7 +8,7 @@ import scala.concurrent.duration._
 
 class AdvancedExampleSimulation extends Simulation {
 
-	val httpProtocol = http.baseURL("http://excilys-bank-web.cloudfoundry.com").disableFollowRedirect
+	val httpProtocol = http.baseURL("http://excilysbank.gatling.cloudbees.net").disableFollowRedirect
 
 	setUp(SomeScenario.scn.inject(ramp(10 users) over (10 seconds)),
 		SomeOtherScenario.otherScn.inject(nothingFor(30 seconds), ramp(5 users) over (20 seconds)))
--- gatling-bundle/src/main/assembly/assembly-structure/user-files/simulations/basic/BasicExampleSimulation.scala
+++ gatling-bundle/src/main/assembly/assembly-structure/user-files/simulations/basic/BasicExampleSimulation.scala
@@ -11,7 +11,7 @@ import assertions._
 class BasicExampleSimulation extends Simulation {
 
 	val httpProtocol = http
-		.baseURL("http://excilys-bank-web.cloudfoundry.com")
+		.baseURL("http://excilysbank.gatling.cloudbees.net")
 		.acceptCharsetHeader("ISO-8859-1,utf-8;q=0.7,*;q=0.7")
 		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
 		.acceptEncodingHeader("gzip, deflate")
