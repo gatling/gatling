c94e8583aa55d09ca20d4118b56e42382cf0a464
Nicolas Rémond <nremond+github@gmail.com>
2013-07-17 23:16:30 +0200

    support more possible values of Cache-Control header

--- gatling-http/src/test/scala/io/gatling/http/cache/CacheHandlingSpec.scala
+++ gatling-http/src/test/scala/io/gatling/http/cache/CacheHandlingSpec.scala
@@ -16,13 +16,53 @@
 package io.gatling.http.cache
 
 import org.junit.runner.RunWith
+import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
+import com.ning.http.client.{ RequestBuilder, Response => AHCResponse }
+import io.gatling.core.config.GatlingConfiguration
+import io.gatling.core.session.Session
+import io.gatling.http.config.HttpProtocol
+import io.gatling.http.Headers
+import io.gatling.http.response.HttpResponse
+import io.gatling.core.config.ProtocolRegistry
 
 @RunWith(classOf[JUnitRunner])
-class CacheHandlingSpec extends Specification {
+class CacheHandlingSpec extends Specification with Mockito {
 
-	"convertExpireField" should {
+	// Default config
+	GatlingConfiguration.setUp()
+
+	"isResponseCacheable()" should {
+
+		val http = HttpProtocol.default.copy(cache = true)
+		val request = new RequestBuilder().setUrl("http://localhost").build
+
+		def isCacheable(headers: Seq[(String, String)]) = {
+			val ahcResponse = mock[AHCResponse].smart
+			headers.foreach { case (name, value) => ahcResponse.getHeader(name) returns value }
+			val response = HttpResponse(request, Some(ahcResponse), Map.empty, -1, -1, -1, -1, Array.empty)
+
+			CacheHandling.isResponseCacheable(http, response)
+		}
+
+		"correctly support Expires header" in {
+			isCacheable(List(Headers.Names.EXPIRES -> "3600")) must beTrue
+		}
+
+		"correctly support Pragma header" in {
+			isCacheable(List(Headers.Names.EXPIRES -> "3600", Headers.Names.PRAGMA -> Headers.Values.NO_CACHE)) must beFalse
+		}
+
+		"correctly support Cache-Control header" in {
+			isCacheable(List(Headers.Names.CACHE_CONTROL -> "private,max-age=3600")) must beTrue
+			isCacheable(List(Headers.Names.EXPIRES -> "3600", Headers.Names.CACHE_CONTROL -> "public,no-cache")) must beFalse
+			isCacheable(List(Headers.Names.EXPIRES -> "3600", Headers.Names.CACHE_CONTROL -> "max-age=0")) must beFalse
+			isCacheable(List(Headers.Names.EXPIRES -> "3600", Headers.Names.CACHE_CONTROL -> "no-store")) must beFalse
+		}
+	}
+
+	"convertExpireField()" should {
 
 		"supports Expires field format" in {
 			CacheHandling.isFutureExpire("Thu, 01 Dec 1994 16:00:00 GMT") must beFalse
