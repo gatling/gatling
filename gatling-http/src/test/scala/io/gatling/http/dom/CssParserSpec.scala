c5b562d68f850a37499d697ff92fffe58b6a705f
Stephane Landelle <slandelle@excilys.com>
2013-10-11 08:06:10 +0200

    Fix test

--- gatling-http/src/test/scala/io/gatling/http/dom/CssParserSpec.scala
+++ gatling-http/src/test/scala/io/gatling/http/dom/CssParserSpec.scala
@@ -24,63 +24,65 @@ import org.specs2.runner.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class CssParserSpec extends Specification {
 
-	val rootURI = new URI("http://akka.io")
+	val rootURI = new URI("http://akka.io/")
 
 	"parsing CSS" should {
 
+		def rulesUri(css: String) = CssParser.extractRules(rootURI, css).styleRules.map(r => r.selector -> r.url)
+
 		"work with an empty CSS" in {
-			CssParser.extractRules(rootURI, "") must beEqualTo(CssContent(Nil, Nil))
+			rulesUri("") must beEqualTo(Nil)
 		}
 
 		"work with a simple CSS" in {
 			val css = """body{background-image: url('backgrounds/blizzard.png');}"""
-			CssParser.extractRules(rootURI, css).styleRules must beEqualTo(Seq("body" -> "backgrounds/blizzard.png"))
+			rulesUri(css) must beEqualTo(Seq("body" -> "http://akka.io/backgrounds/blizzard.png"))
 
 			val css2 = """body { 
 										background-image: url('backgrounds/blizzard.png'); 
 								 }"""
-			CssParser.extractRules(rootURI, css2).styleRules must beEqualTo(Seq("body" -> "backgrounds/blizzard.png"))
+			rulesUri(css2) must beEqualTo(Seq("body" -> "http://akka.io/backgrounds/blizzard.png"))
 		}
 
 		"work with a all kind of selectors CSS" in {
 			val css = """.foo { background-image: url('backgrounds/blizzard.png');}"""
-			CssParser.extractRules(rootURI, css).styleRules must beEqualTo(Seq(".foo" -> "backgrounds/blizzard.png"))
+			rulesUri(css) must beEqualTo(Seq(".foo" -> "http://akka.io/backgrounds/blizzard.png"))
 
 			val css2 = """#foo { background-image: url('backgrounds/blizzard.png');}"""
-			CssParser.extractRules(rootURI, css2).styleRules must beEqualTo(Seq("#foo" -> "backgrounds/blizzard.png"))
+			rulesUri(css2) must beEqualTo(Seq("#foo" -> "http://akka.io/backgrounds/blizzard.png"))
 
 			val css3 = """.foo .bar { background-image: url('backgrounds/blizzard.png');}"""
-			CssParser.extractRules(rootURI, css3).styleRules must beEqualTo(Seq(".foo .bar" -> "backgrounds/blizzard.png"))
+			rulesUri(css3) must beEqualTo(Seq(".foo .bar" -> "http://akka.io/backgrounds/blizzard.png"))
 		}
 
 		"work with a complex CSS background directive" in {
 			val css = """body {background:#ffffff url('img_tree.png') no-repeat right top;}"""
-			CssParser.extractRules(rootURI, css).styleRules must beEqualTo(Seq("body" -> "img_tree.png"))
+			rulesUri(css) must beEqualTo(Seq("body" -> "http://akka.io/img_tree.png"))
 		}
 
 		"work with a multiple urls in the CSS" in {
 			val css = """body { background-image: url('backgrounds/blizzard.png'); }
 				    			.foo { background: url('backgrounds/landscapes/sky.jpg'); }"""
-			CssParser.extractRules(rootURI, css).styleRules must beEqualTo(Seq("body" -> "backgrounds/blizzard.png", ".foo" -> "backgrounds/landscapes/sky.jpg"))
+			rulesUri(css) must beEqualTo(Seq("body" -> "http://akka.io/backgrounds/blizzard.png", ".foo" -> "http://akka.io/backgrounds/landscapes/sky.jpg"))
 		}
 
 		// See http://www.w3.org/TR/CSS2/syndata.html#value-def-uri for details
 		"work with all URI format" in {
 			// With double quotes
 			val css = """body { background: url("http://www.example.com/pinkish.png") }"""
-			CssParser.extractRules(rootURI, css).styleRules must beEqualTo(Seq("body" -> "http://www.example.com/pinkish.png"))
+			rulesUri(css) must beEqualTo(Seq("body" -> "http://www.example.com/pinkish.png"))
 
 			// With simple quotes
 			val css2 = """li { list-style: url(http://www.example.com/redball.png) disc }"""
-			CssParser.extractRules(rootURI, css2).styleRules must beEqualTo(Seq("li" -> "http://www.example.com/redball.png"))
+			rulesUri(css2) must beEqualTo(Seq("li" -> "http://www.example.com/redball.png"))
 
 			// With nothing
 			val css3 = """body { background: url(http://www.example.com/pinkish.png) }"""
-			CssParser.extractRules(rootURI, css3).styleRules must beEqualTo(Seq("body" -> "http://www.example.com/pinkish.png"))
+			rulesUri(css3) must beEqualTo(Seq("body" -> "http://www.example.com/pinkish.png"))
 
 			// With spaces
 			val css4 = """body { background: url( http://www.example.com/pinkish.png ) }"""
-			CssParser.extractRules(rootURI, css4).styleRules must beEqualTo(Seq("body" -> "http://www.example.com/pinkish.png"))
+			rulesUri(css4) must beEqualTo(Seq("body" -> "http://www.example.com/pinkish.png"))
 		}
 	}
 }
