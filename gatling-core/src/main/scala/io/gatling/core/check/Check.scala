cd2b3f6662f017e204b2579e9547bdaee086327a
Stephane Landelle <slandelle@excilys.com>
2013-11-05 11:46:11 +0100

    Handle empty check list

--- gatling-core/src/main/scala/io/gatling/core/check/Check.scala
+++ gatling-core/src/main/scala/io/gatling/core/check/Check.scala
@@ -18,16 +18,21 @@ package io.gatling.core.check
 import scala.collection.mutable
 
 import io.gatling.core.session.Session
-import io.gatling.core.validation.{ Validation, ValidationList }
+import io.gatling.core.validation.{ SuccessWrapper, Validation, ValidationList }
 import io.gatling.core.check.extractor.Extractor
 
 object Checks {
+	
+	val noopUpdate = (identity[Session] _).success
 
 	def check[R](response: R, session: Session, checks: List[Check[R]]): Validation[Session => Session] = {
 
 		implicit val cache = mutable.Map.empty[Any, Any]
-
-		checks.map(_.check(response, session)).sequence.map(_.reduce(_ andThen _))
+		
+		checks match {
+			case Nil => noopUpdate
+			case checks => checks.map(_.check(response, session)).sequence.map(_.reduce(_ andThen _))
+		}
 	}
 }
 
