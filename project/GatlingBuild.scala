f41daa981141157bdd6365b9ebf3db82ec269ece
Stephane Landelle <slandelle@excilys.com>
2013-11-20 18:24:03 +0100

    Revert "Make gatling-bundle anemic, close #1472"
    This reverts commit 24989822ece96941b4a20be4c3b6d7a3e32153c4.

--- project/GatlingBuild.scala
+++ project/GatlingBuild.scala
@@ -60,7 +60,7 @@ object GatlingBuild extends Build {
 		.settings(libraryDependencies ++= recorderDeps)
 
 	lazy val bundle = gatlingModule("gatling-bundle")
-		.dependsOn(Seq(app, core, charts, http, jdbc, redis, recorder).map(_ % "provided->provided"): _*)
+		.dependsOn(app, core, charts, http, jdbc, redis, recorder)
 		.settings(bundleSettings: _*)
 		.settings(noCodeToPublish: _*)
 		.settings(exportJars := false) // Don't export gatling-bundle's jar 
