b9e5a5caa7a3cbaf7252d1d37e9536c87ec47d5b
Stephane Landelle <slandelle@excilys.com>
2013-12-06 14:54:52 +0100

    Remove jdbc predef

--- gatling-recorder/src/main/scala/io/gatling/recorder/scenario/template/SimulationTemplate.scala
+++ gatling-recorder/src/main/scala/io/gatling/recorder/scenario/template/SimulationTemplate.scala
@@ -91,7 +91,6 @@ import scala.concurrent.duration._
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-import io.gatling.jdbc.Predef._
 
 class $simulationClassName extends Simulation {
 
