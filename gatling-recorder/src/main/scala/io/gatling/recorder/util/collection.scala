3b5acff7d819bd56216ddedbbd6dbd9b17073df3
Stephane Landelle <slandelle@excilys.com>
2014-01-19 18:48:27 +0100

    spaces

--- gatling-recorder/src/main/scala/io/gatling/recorder/util/collection.scala
+++ gatling-recorder/src/main/scala/io/gatling/recorder/util/collection.scala
@@ -3,7 +3,7 @@ package io.gatling.recorder.util
 object collection {
 
 	implicit class RichSeq[T](val elts: Seq[T]) extends AnyVal {
-		
+
 		// See ScenarioSpec for example
 		def groupAsLongAs(p: T => Boolean): List[List[T]] =
 			elts.foldRight(List[List[T]]()) {
