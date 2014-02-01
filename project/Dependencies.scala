a636a6748337863c69fefba5b71d1e3e4a926b75
Stephane Landelle <slandelle@excilys.com>
2014-02-01 20:13:36 +0100

    Upgrade JsonPath 0.3.3, close #1624

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
 	private val scalalogging      = "com.typesafe"              %% "scalalogging-slf4j" % "1.0.1"
 	private val jackson           = "com.fasterxml.jackson.core" % "jackson-databind"   % "2.3.0"
 	private val boon              = "io.fastjson"                % "boon"               % "0.8"
-	private val jsonpath          = "io.gatling"                %% "jsonpath"           % "0.3.2"
+	private val jsonpath          = "io.gatling"                %% "jsonpath"           % "0.3.3"
 	private val commonsMath       = "org.apache.commons"         % "commons-math3"      % "3.2"
 	private val joddLagarto       = "org.jodd"                   % "jodd-lagarto"       % "3.4.10"
 	private val jzlib             = "com.jcraft"                 % "jzlib"              % "1.1.3"
