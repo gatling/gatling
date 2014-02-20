c3a0d8c110e7baaebd1e425e0cc2358279b238dc
Nicolas Rémond <nremond+github@gmail.com>
2014-02-20 18:32:52 +0100

    Recorder page for the documentation

--- gatling-http/src/test/scala/io/gatling/http/action/WebSocketActorSpec.scala
+++ gatling-http/src/test/scala/io/gatling/http/action/WebSocketActorSpec.scala
@@ -44,173 +44,173 @@ import io.gatling.http.config.HttpProtocol
 @RunWith(classOf[JUnitRunner])
 class WebSocketActorSpec extends Specification with AllExpectations with Mockito {
 
-//	step {
-//		// initialize logging to avoid substitute logger error messages
-//		LoggerFactory.getLogger(classOf[WebSocketActorSpec])
-//		// set up configuration to avoid NPEs constructing actors
-//		GatlingConfiguration.setUp()
-//		GatlingActorSystem.start
-//		success
-//	}
-//
-//	"A WebSocketActor" should {
-//
-//		"record a successful open and advance" in new scope {
-//			open(webSocketClient(_.onOpen(mock[WebSocket].smart)))
-//
-//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(OK), anyLong, anyLong, isEq(null))
-//			next.underlyingActor.session.map(_.contains("testAttributeName")) mustEqual Some(true)
-//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(false)
-//		}
-//
-//		"record a failed open and advance" in new scope {
-//			open(mock[WebSocketClient].open(any[URI], any[WebSocketListener]) throws new IOException("testErrorMessage"))
-//
-//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(KO), anyLong, anyLong, anArgThat(contains("testErrorMessage")))
-//			next.underlyingActor.session.map(_.contains("testAttributeName")) mustEqual Some(false)
-//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(true)
-//		}
-//
-//		"record an incomplete open as failed and advance" in new scope {
-//			open(webSocketClient(_.onError(new IOException("testException"))))
-//
-//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(KO), anyLong, anyLong, anArgThat(contains("testException")))
-//			next.underlyingActor.session.map(_.contains("testAttributeName")) mustEqual Some(false)
-//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(true)
-//		}
-//
-//		"record a close-before-open as failed and advance" in new scope {
-//			open(webSocketClient(_.onClose(mock[WebSocket].smart)))
-//
-//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(KO), anyLong, anyLong, any[Option[String]])
-//			next.underlyingActor.session.map(_.contains("testAttributeName")) mustEqual Some(false)
-//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(true)
-//		}
-//
-//		"record a successful send and advance" in new scope {
-//			val webSocket = mock[WebSocket].smart
-//			openSuccessfully(webSocket)
-//			sendMessage("testMessage")
-//
-//			there was one(webSocket).sendTextMessage(isEq("testMessage"))
-//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(OK), anyLong, anyLong, isEq(null))
-//
-//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(false)
-//		}
-//
-//		"record a send after an error as failed and advance" in new scope {
-//			val webSocket = mock[WebSocket].smart
-//			reportError(webSocket, new IOException("testException"))
-//			sendMessage("testMessage")
-//
-//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(KO), anyLong, anyLong, anArgThat(contains("testException")))
-//			there was no(webSocket).sendTextMessage(anyString)
-//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(true)
-//		}
-//
-//		"record a send after an unexpected close as failed and advance" in new scope {
-//			val webSocket = mock[WebSocket].smart
-//			closeUnexpectedly(webSocket)
-//			sendMessage("testMessage")
-//
-//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(KO), anyLong, anyLong, any[Option[String]])
-//			there was no(webSocket).sendTextMessage(anyString)
-//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(true)
-//		}
-//
-//		"record a successful close and advance" in new scope {
-//			val webSocket = mock[WebSocket].smart
-//			openSuccessfully(webSocket)
-//			close()
-//
-//			there was one(webSocket).close()
-//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(OK), anyLong, anyLong, isEq(null))
-//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(false)
-//		}
-//
-//		"record a close after an error as failed and advance" in new scope {
-//			val webSocket = mock[WebSocket].smart
-//			reportError(webSocket, new IOException("testException"))
-//			close()
-//
-//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(KO), anyLong, anyLong, anArgThat(contains("testException")))
-//			there was no(webSocket).sendTextMessage(anyString)
-//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(true)
-//		}
-//
-//		"record a close after an unexpected close as failed and advance" in new scope {
-//			val webSocket = mock[WebSocket].smart
-//			closeUnexpectedly(webSocket)
-//			close()
-//
-//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(KO), anyLong, anyLong, any[Option[String]])
-//			there was no(webSocket).sendTextMessage(anyString)
-//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(true)
-//		}
-//	}
-//
-//	step {
-//		GatlingActorSystem.shutdown
-//	}
-//
-//	private trait scope extends Scope with MocksCreation {
-//		val requestLogger = mock[RequestLogger]
-//		var next: TestActorRef[DummyAction] = _
-//
-//		def webSocketClient(open: (WebSocketListener) => Unit) = {
-//			mock[WebSocketClient].open(any[URI], any[WebSocketListener]) answers { (params, _) =>
-//				open(params.asInstanceOf[Array[_]](1).asInstanceOf[WebSocketListener])
-//			}
-//		}
-//
-//		def open(webSocketClient: WebSocketClient) {
-//			next = TestActorRef[DummyAction](Props(new DummyAction))(GatlingActorSystem.instance)
-//			val action = websocket("testRequestName")
-//				.open("ws://dummy/", "testAttributeName")(webSocketClient, requestLogger)
-//				.build(next, Protocols() + HttpProtocol.default)
-//
-//			action ! new Session("test", "0")
-//		}
-//
-//		def openSuccessfully(webSocket: WebSocket) {
-//			open(webSocketClient(_.onOpen(webSocket)))
-//			reset(requestLogger)
-//		}
-//
-//		def reportError(webSocket: WebSocket, t: Throwable) {
-//			open(webSocketClient { s => s.onOpen(webSocket); s.onError(t) })
-//			reset(requestLogger)
-//		}
-//
-//		def closeUnexpectedly(webSocket: WebSocket) {
-//			open(webSocketClient { s => s.onOpen(webSocket); s.onClose(webSocket) })
-//			reset(requestLogger)
-//		}
-//
-//		def sendMessage(message: String) {
-//			val session = next.underlyingActor.session.get
-//			next = TestActorRef[DummyAction](Props(new DummyAction))(GatlingActorSystem.instance)
-//			val action = websocket("testRequestName")
-//				.sendMessage(message, "testAttributeName")
-//				.build(next, Protocols() + HttpProtocol.default)
-//
-//			action ! session
-//		}
-//
-//		def close() {
-//			val session = next.underlyingActor.session.get
-//			next = TestActorRef[DummyAction](Props(new DummyAction))(GatlingActorSystem.instance)
-//			val action = websocket("testRequestName")
-//				.close("testAttributeName")
-//				.build(next, Protocols() + HttpProtocol.default)
-//
-//			action ! session
-//		}
-//	}
-//
-//	private case class contains(str: String) extends ArgumentMatcher[Option[String]] {
-//		def matches(argument: Any) = argument.asInstanceOf[Option[String]].exists(_.contains(str))
-//	}
+	//	step {
+	//		// initialize logging to avoid substitute logger error messages
+	//		LoggerFactory.getLogger(classOf[WebSocketActorSpec])
+	//		// set up configuration to avoid NPEs constructing actors
+	//		GatlingConfiguration.setUp()
+	//		GatlingActorSystem.start
+	//		success
+	//	}
+	//
+	//	"A WebSocketActor" should {
+	//
+	//		"record a successful open and advance" in new scope {
+	//			open(webSocketClient(_.onOpen(mock[WebSocket].smart)))
+	//
+	//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(OK), anyLong, anyLong, isEq(null))
+	//			next.underlyingActor.session.map(_.contains("testAttributeName")) mustEqual Some(true)
+	//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(false)
+	//		}
+	//
+	//		"record a failed open and advance" in new scope {
+	//			open(mock[WebSocketClient].open(any[URI], any[WebSocketListener]) throws new IOException("testErrorMessage"))
+	//
+	//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(KO), anyLong, anyLong, anArgThat(contains("testErrorMessage")))
+	//			next.underlyingActor.session.map(_.contains("testAttributeName")) mustEqual Some(false)
+	//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(true)
+	//		}
+	//
+	//		"record an incomplete open as failed and advance" in new scope {
+	//			open(webSocketClient(_.onError(new IOException("testException"))))
+	//
+	//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(KO), anyLong, anyLong, anArgThat(contains("testException")))
+	//			next.underlyingActor.session.map(_.contains("testAttributeName")) mustEqual Some(false)
+	//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(true)
+	//		}
+	//
+	//		"record a close-before-open as failed and advance" in new scope {
+	//			open(webSocketClient(_.onClose(mock[WebSocket].smart)))
+	//
+	//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(KO), anyLong, anyLong, any[Option[String]])
+	//			next.underlyingActor.session.map(_.contains("testAttributeName")) mustEqual Some(false)
+	//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(true)
+	//		}
+	//
+	//		"record a successful send and advance" in new scope {
+	//			val webSocket = mock[WebSocket].smart
+	//			openSuccessfully(webSocket)
+	//			sendMessage("testMessage")
+	//
+	//			there was one(webSocket).sendTextMessage(isEq("testMessage"))
+	//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(OK), anyLong, anyLong, isEq(null))
+	//
+	//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(false)
+	//		}
+	//
+	//		"record a send after an error as failed and advance" in new scope {
+	//			val webSocket = mock[WebSocket].smart
+	//			reportError(webSocket, new IOException("testException"))
+	//			sendMessage("testMessage")
+	//
+	//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(KO), anyLong, anyLong, anArgThat(contains("testException")))
+	//			there was no(webSocket).sendTextMessage(anyString)
+	//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(true)
+	//		}
+	//
+	//		"record a send after an unexpected close as failed and advance" in new scope {
+	//			val webSocket = mock[WebSocket].smart
+	//			closeUnexpectedly(webSocket)
+	//			sendMessage("testMessage")
+	//
+	//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(KO), anyLong, anyLong, any[Option[String]])
+	//			there was no(webSocket).sendTextMessage(anyString)
+	//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(true)
+	//		}
+	//
+	//		"record a successful close and advance" in new scope {
+	//			val webSocket = mock[WebSocket].smart
+	//			openSuccessfully(webSocket)
+	//			close()
+	//
+	//			there was one(webSocket).close()
+	//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(OK), anyLong, anyLong, isEq(null))
+	//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(false)
+	//		}
+	//
+	//		"record a close after an error as failed and advance" in new scope {
+	//			val webSocket = mock[WebSocket].smart
+	//			reportError(webSocket, new IOException("testException"))
+	//			close()
+	//
+	//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(KO), anyLong, anyLong, anArgThat(contains("testException")))
+	//			there was no(webSocket).sendTextMessage(anyString)
+	//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(true)
+	//		}
+	//
+	//		"record a close after an unexpected close as failed and advance" in new scope {
+	//			val webSocket = mock[WebSocket].smart
+	//			closeUnexpectedly(webSocket)
+	//			close()
+	//
+	//			there was one(requestLogger).logRequest(any[Session], anyString, isEq(KO), anyLong, anyLong, any[Option[String]])
+	//			there was no(webSocket).sendTextMessage(anyString)
+	//			next.underlyingActor.session.map(_.isFailed) mustEqual Some(true)
+	//		}
+	//	}
+	//
+	//	step {
+	//		GatlingActorSystem.shutdown
+	//	}
+	//
+	//	private trait scope extends Scope with MocksCreation {
+	//		val requestLogger = mock[RequestLogger]
+	//		var next: TestActorRef[DummyAction] = _
+	//
+	//		def webSocketClient(open: (WebSocketListener) => Unit) = {
+	//			mock[WebSocketClient].open(any[URI], any[WebSocketListener]) answers { (params, _) =>
+	//				open(params.asInstanceOf[Array[_]](1).asInstanceOf[WebSocketListener])
+	//			}
+	//		}
+	//
+	//		def open(webSocketClient: WebSocketClient) {
+	//			next = TestActorRef[DummyAction](Props(new DummyAction))(GatlingActorSystem.instance)
+	//			val action = websocket("testRequestName")
+	//				.open("ws://dummy/", "testAttributeName")(webSocketClient, requestLogger)
+	//				.build(next, Protocols() + HttpProtocol.default)
+	//
+	//			action ! new Session("test", "0")
+	//		}
+	//
+	//		def openSuccessfully(webSocket: WebSocket) {
+	//			open(webSocketClient(_.onOpen(webSocket)))
+	//			reset(requestLogger)
+	//		}
+	//
+	//		def reportError(webSocket: WebSocket, t: Throwable) {
+	//			open(webSocketClient { s => s.onOpen(webSocket); s.onError(t) })
+	//			reset(requestLogger)
+	//		}
+	//
+	//		def closeUnexpectedly(webSocket: WebSocket) {
+	//			open(webSocketClient { s => s.onOpen(webSocket); s.onClose(webSocket) })
+	//			reset(requestLogger)
+	//		}
+	//
+	//		def sendMessage(message: String) {
+	//			val session = next.underlyingActor.session.get
+	//			next = TestActorRef[DummyAction](Props(new DummyAction))(GatlingActorSystem.instance)
+	//			val action = websocket("testRequestName")
+	//				.sendMessage(message, "testAttributeName")
+	//				.build(next, Protocols() + HttpProtocol.default)
+	//
+	//			action ! session
+	//		}
+	//
+	//		def close() {
+	//			val session = next.underlyingActor.session.get
+	//			next = TestActorRef[DummyAction](Props(new DummyAction))(GatlingActorSystem.instance)
+	//			val action = websocket("testRequestName")
+	//				.close("testAttributeName")
+	//				.build(next, Protocols() + HttpProtocol.default)
+	//
+	//			action ! session
+	//		}
+	//	}
+	//
+	//	private case class contains(str: String) extends ArgumentMatcher[Option[String]] {
+	//		def matches(argument: Any) = argument.asInstanceOf[Option[String]].exists(_.contains(str))
+	//	}
 }
 
 //class DummyAction extends Actor {
--- src/sphinx/index.rst
+++ src/sphinx/index.rst
@@ -14,6 +14,7 @@ User's guide
    :maxdepth: 2
 
    Quickstart
+   Recorder
    Operations
    FAQ
    gatling2
