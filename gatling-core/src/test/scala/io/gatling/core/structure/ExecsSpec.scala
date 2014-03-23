5f5587d1734a8df1765a4451fc4ce4a0a1b85509
James Pickering <james_pic@hotmail.com>
2014-03-23 12:33:17 +0000

    Added before and after actions to ExecsSpec

--- gatling-core/src/test/scala/io/gatling/core/structure/ExecsSpec.scala
+++ gatling-core/src/test/scala/io/gatling/core/structure/ExecsSpec.scala
@@ -6,21 +6,38 @@ import io.gatling.core.Predef._
 import io.gatling.core.config.Protocols
 import io.gatling.core.session.Session
 
-/**
- * Created by hisg085 on 22/03/2014.
- */
 class ExecsSpec extends Specification {
 	"Execs" should {
 		"wrap Scenarios in chains, using exec" in new ActorSupport {
 			val testScenario = scenario("Test Scenario").exec { session =>
-				self ! "Success"
+				self ! "Message 2"
 				session
 			}
-			val chainBuilder = exec(testScenario)
+
+			val chainBuilder = exec { session =>
+				self ! "Message 1"
+				session
+			}
+				.exec(testScenario)
+				.exec { session =>
+					self ! "Message 3"
+					session
+				}
+
 			val chain = chainBuilder.build(self, Protocols())
 			val session = Session("TestScenario", "testUser")
 			chain ! session
-			expectMsgAllOf("Success", session)
+			/*
+			 * We're cheating slightly by assuming messages will be delivered
+			 * in order (technically, Akka doesn't guarantee transitive
+			 * ordering, although within the same JVM ordering is generally
+			 * transitive) as it gives us more informative error messages.
+			 */
+			expectMsg("Message 1")
+			expectMsg("Message 2")
+			expectMsg("Message 3")
+			expectMsg(session)
+			expectNoMsg
 		}
 	}
 }
