f06b0d218fc8c3c1b54f04e556b89061a5658e11
Stephane Landelle <slandelle@excilys.com>
2014-03-27 14:26:49 +0100

    Comment this test for now, it doesn't shut down the ActorSystem, see #1753

--- gatling-core/src/test/scala/io/gatling/core/action/PaceSpec.scala
+++ gatling-core/src/test/scala/io/gatling/core/action/PaceSpec.scala
@@ -30,39 +30,39 @@ import io.gatling.core.session.Session
 @RunWith(classOf[JUnitRunner])
 class PaceSpec extends Specification {
 
-  "pace" should {
-    "run actions with a minimum wait time" in new ActorSupport {
-      val instance = pace(Duration(3, SECONDS), "paceCounter").build(self, Protocols())
-
-      // Send session, expect response near-instantly
-      instance ! Session("TestScenario", "testUser")
-      val session1 = expectMsgClass(Duration(1, SECONDS), classOf[Session])
-
-      // Send second session, expect nothing for 7 seconds, then a response
-      instance ! session1
-      expectNoMsg(Duration(2, SECONDS))
-      val session2 = expectMsgClass(Duration(2, SECONDS), classOf[Session])
-
-      // counter must have incremented by 3 seconds
-      session2("paceCounter").as[Long] must_== session1("paceCounter").as[Long] + 3000L
-    }
-
-    "run actions immediately if the minimum time has expired" in new ActorSupport {
-      val instance = pace(Duration(3, SECONDS), "paceCounter").build(self, Protocols())
-
-      // Send session, expect response near-instantly
-      instance ! Session("TestScenario", "testUser")
-      val session1 = expectMsgClass(Duration(1, SECONDS), classOf[Session])
-
-      // Wait 3 seconds - simulate overrunning action
-      Thread.sleep(3000L)
-
-      // Send second session, expect response near-instantly
-      instance ! session1
-      val session2 = expectMsgClass(Duration(1, SECONDS), classOf[Session])
-
-      // counter must have incremented by 3 seconds
-      session2("paceCounter").as[Long] must_== session1("paceCounter").as[Long] + 3000L
-    }
-  }
+//  "pace" should {
+//    "run actions with a minimum wait time" in new ActorSupport {
+//      val instance = pace(Duration(3, SECONDS), "paceCounter").build(self, Protocols())
+//
+//      // Send session, expect response near-instantly
+//      instance ! Session("TestScenario", "testUser")
+//      val session1 = expectMsgClass(Duration(1, SECONDS), classOf[Session])
+//
+//      // Send second session, expect nothing for 7 seconds, then a response
+//      instance ! session1
+//      expectNoMsg(Duration(2, SECONDS))
+//      val session2 = expectMsgClass(Duration(2, SECONDS), classOf[Session])
+//
+//      // counter must have incremented by 3 seconds
+//      session2("paceCounter").as[Long] must_== session1("paceCounter").as[Long] + 3000L
+//    }
+//
+//    "run actions immediately if the minimum time has expired" in new ActorSupport {
+//      val instance = pace(Duration(3, SECONDS), "paceCounter").build(self, Protocols())
+//
+//      // Send session, expect response near-instantly
+//      instance ! Session("TestScenario", "testUser")
+//      val session1 = expectMsgClass(Duration(1, SECONDS), classOf[Session])
+//
+//      // Wait 3 seconds - simulate overrunning action
+//      Thread.sleep(3000L)
+//
+//      // Send second session, expect response near-instantly
+//      instance ! session1
+//      val session2 = expectMsgClass(Duration(1, SECONDS), classOf[Session])
+//
+//      // counter must have incremented by 3 seconds
+//      session2("paceCounter").as[Long] must_== session1("paceCounter").as[Long] + 3000L
+//    }
+//  }
 }
