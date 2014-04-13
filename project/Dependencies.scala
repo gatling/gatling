9dcfc67412bf140ca460ad327ae8a664b89af92b
Stephane Landelle <slandelle@excilys.com>
2014-04-13 23:24:31 +0200

    Upgrade Zinc 0.3.2-M3, close #1803

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
 	private val jzlib                          = "com.jcraft"                 % "jzlib"              % "1.1.3"
 	private val commonsIo                      = "commons-io"                 % "commons-io"         % "2.4"
 	private val redisClient                    =  "net.debasishg"             %% "redisclient"        % "2.12"
-	private val zinc                           = "com.typesafe.zinc"          % "zinc"               % "0.3.2-M1"
+	private val zinc                           = "com.typesafe.zinc"          % "zinc"               % "0.3.2-M3"
 	private val openCsv                        = "net.sf.opencsv"             % "opencsv"            % "2.3"
 	private val jmsApi                         = "javax.jms"                  % "jms-api"            % "1.1-rev-1"
 	private val logbackClassic                 = "ch.qos.logback"             % "logback-classic"    % "1.1.2"
