560fe91b88d7855f55cf23bcd14d1772f50c042b
Stephane Landelle <slandelle@excilys.com>
2014-05-13 10:21:50 +0200

    Upgrade Boon 0.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
   private val scopt                          = "com.github.scopt"          %% "scopt"              % "3.2.0"
   private val scalalogging                   = "com.typesafe"              %% "scalalogging-slf4j" % "1.1.0"
   private val jackson                        = "com.fasterxml.jackson.core" % "jackson-databind"   % "2.4.0-rc3"
-  private val boon                           = "io.fastjson"                % "boon"               % "0.14"
+  private val boon                           = "io.fastjson"                % "boon"               % "0.17"
   private val jsonpath                       = "io.gatling"                %% "jsonpath"           % "0.4.1"
   private val commonsMath                    = "org.apache.commons"         % "commons-math3"      % "3.2"
   private val joddLagarto                    = "org.jodd"                   % "jodd-lagarto"       % "3.5"
