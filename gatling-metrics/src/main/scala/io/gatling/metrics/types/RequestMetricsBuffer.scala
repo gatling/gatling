85741f7b6bd701e886844afd66fdf099660a4951
Nicolas Rémond <nremond+github@gmail.com>
2014-05-17 00:21:57 +0200

    Use a fixed buffer of 1s rather than 10% for sizing the metrics histogram

--- gatling-metrics/src/main/scala/io/gatling/metrics/types/RequestMetricsBuffer.scala
+++ gatling-metrics/src/main/scala/io/gatling/metrics/types/RequestMetricsBuffer.scala
@@ -24,10 +24,9 @@ import org.HdrHistogram.HistogramData
 
 class RequestMetricsBuffer(implicit configuration: GatlingConfiguration) {
 
-  // Let's take the max of the possible timeouts and add a 10% buffer 
-  private val maxValue =
-    (configuration.data.graphite.maxMeasuredValue
-      .max(configuration.http.ahc.requestTimeOutInMs) * 1.1d).toInt
+  // Let's take the max of the possible timeouts and add a 1s buffer 
+  private val maxValue = configuration.data.graphite.maxMeasuredValue.max(
+    configuration.http.ahc.requestTimeOutInMs) + 1000
 
   private val precision = 3
   private val percentile1 = configuration.charting.indicators.percentile1
