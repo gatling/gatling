2ac34c80ead402dd29ab0b549ff078cee588aafc
Ivan Mushketyk <ivan.mushketyk@gmail.com>
2014-05-22 09:03:58 +0300

    Added AbstractHttpRequestBuilder.signatureCalculator method that accepts function

--- gatling-http/src/test/scala/io/gatling/http/request/builder/HttpRequestBuilderSpec.scala
+++ gatling-http/src/test/scala/io/gatling/http/request/builder/HttpRequestBuilderSpec.scala
@@ -19,7 +19,7 @@ import org.specs2.mutable.Specification
 import org.junit.runner.RunWith
 import org.specs2.runner.JUnitRunner
 import org.specs2.mock.Mockito
-import com.ning.http.client.SignatureCalculator
+import com.ning.http.client.{ Request, RequestBuilderBase, SignatureCalculator }
 import java.net.URI
 import io.gatling.core.validation.Success
 import io.gatling.http.config.HttpProtocol
@@ -30,13 +30,29 @@ class HttpRequestBuilderSpec extends Specification with Mockito {
   def mockComonAttributes() = CommonAttributes(_ => Success("attributes"), "method", Right(new URI("http://gatling-tool.org")))
 
   "request builder" should {
-    "set signature calculator" in {
+    "set signature calculator object" in {
       var builder = new HttpRequestBuilder(mockComonAttributes(), HttpAttributes())
       val sigCalc = mock[SignatureCalculator]
       builder = builder.signatureCalculator(sigCalc)
 
       val httpRequest = builder.build(HttpProtocol.default, false)
       httpRequest.signatureCalculator.get must_== sigCalc
+    }
+
+    "set signature calculator function" in {
+      var builder = new HttpRequestBuilder(mockComonAttributes(), HttpAttributes())
+      val sigCalcFunc = mock[Function3[String, Request, RequestBuilderBase[_], Unit]]
+      builder = builder.signatureCalculator(sigCalcFunc)
+
+      val httpRequest = builder.build(HttpProtocol.default, false)
+      val sigCalc = httpRequest.signatureCalculator.get
+
+      val mockUrl = "mockUrl"
+      val mockRequest = mock[Request]
+      val mockRequestBuilder = mock[RequestBuilderBase[_]]
+
+      sigCalc.calculateAndAddSignature(mockUrl, mockRequest, mockRequestBuilder)
+      there was one(sigCalcFunc).apply(mockUrl, mockRequest, mockRequestBuilder)
     }
   }
 }
