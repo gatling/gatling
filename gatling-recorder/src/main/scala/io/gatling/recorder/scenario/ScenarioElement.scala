5c4675b07015cc223eb861a8099f1aa0670f4308
Ivan Mushketyk <ivan.mushketyk@gmail.com>
2014-05-24 21:41:37 +0300

    Added configuration key to leave/remove conditional cache headers

--- gatling-recorder/src/main/scala/io/gatling/recorder/scenario/ScenarioElement.scala
+++ gatling-recorder/src/main/scala/io/gatling/recorder/scenario/ScenarioElement.scala
@@ -32,6 +32,7 @@ import com.ning.http.util.Base64
 import io.gatling.http.fetch.{ EmbeddedResource, HtmlParser }
 import io.gatling.http.util.HttpHelper.parseFormBody
 import io.gatling.recorder.util.URIHelper
+import io.gatling.recorder.config.RecorderConfiguration
 
 sealed trait ScenarioElement
 
@@ -81,7 +82,11 @@ object RequestElement {
       else
         RequestBodyBytes(content))
 
-    val filteredRequestHeaders = requestHeaders.filter(header => !CONDITIONAL_CACHE_HEADERS.contains(header._1))
+    val filteredRequestHeaders =
+      if (RecorderConfiguration.configuration.http.removeConditionalCache)
+        requestHeaders.filter(header => !CONDITIONAL_CACHE_HEADERS.contains(header._1))
+      else
+        requestHeaders
 
     RequestElement(new String(request.getUri), request.getMethod.toString, filteredRequestHeaders, requestBody, response.getStatus.getCode, resources)
   }
