915168edbb3236456a30e9b7dd3b1cd67ef9e92e
Stephane Landelle <slandelle@excilys.com>
2014-05-28 10:25:05 +0200

    Fix build

--- gatling-recorder/src/test/scala/io/gatling/recorder/scenario/ScenarioSpec.scala
+++ gatling-recorder/src/test/scala/io/gatling/recorder/scenario/ScenarioSpec.scala
@@ -39,7 +39,12 @@ class ScenarioSpec extends Specification {
       val r3 = RequestElement("http://gatling.io/release-note-1.html", "GET", Map.empty, None, 200, List.empty)
       val r4 = RequestElement("http://gatling.io/details.html", "GET", Map.empty, None, 200, List.empty)
 
-      val scn = ScenarioDefinition(List(1l -> r1, 2l -> r2, 3l -> r3, 4l -> r4), List.empty)
+      val scn = ScenarioDefinition(
+        List(TimedScenarioElement(1l, r1),
+          TimedScenarioElement(2l, r2),
+          TimedScenarioElement(3l, r3),
+          TimedScenarioElement(4l, r4)),
+        List.empty)
       scn.elements should beEqualTo(List(r1, r2.copy(statusCode = 200), r4))
     }
 
@@ -49,13 +54,18 @@ class ScenarioSpec extends Specification {
       val r2 = RequestElement("http://gatling.io/main.css", "GET", Map.empty, None, 200, List.empty)
       val r3 = RequestElement("http://gatling.io/details.html", "GET", Map(CONTENT_TYPE -> "text/html;charset=UTF-8"), None, 200, List.empty)
       val r4 = RequestElement("http://gatling.io/img.jpg", "GET", Map.empty, None, 200, List.empty)
-      val r5 = RequestElement("http://gatling.io", "GET", Map.empty, None, 200,
-        List(CssResource(new URI("http://gatling.io/main.css"))))
+      val r5 = RequestElement("http://gatling.io", "GET", Map.empty, None, 200, List(CssResource(new URI("http://gatling.io/main.css"))))
       val r6 = RequestElement("http://gatling.io/main.css", "GET", Map.empty, None, 200, List.empty)
 
-      val scn = ScenarioDefinition(List(1l -> r1, 2l -> r2, 3l -> r3, 4l -> r4, 5l -> r5, 6l -> r6), List.empty)
+      val scn = ScenarioDefinition(
+        List(TimedScenarioElement(1l, r1),
+          TimedScenarioElement(2l, r2),
+          TimedScenarioElement(3l, r3),
+          TimedScenarioElement(4l, r4),
+          TimedScenarioElement(5l, r5),
+          TimedScenarioElement(6l, r6)),
+        List.empty)
       scn.elements should beEqualTo(List(r1, r3, r5))
     }
   }
-
 }
