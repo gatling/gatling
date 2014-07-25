92d5bf259c4fd555f514ba17ae0e4dd1b7b78253
Stephane Landelle <slandelle@excilys.com>
2014-07-25 12:40:30 +0200

    Upgrade Akka 2.3.4, close #1600

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   /** Compile dependencies **/
   /**************************/
 
-  private val akkaVersion                    = "2.2.4"
+  private val akkaVersion                    = "2.3.4"
 
   private def scalaLibrary(version: String)  = "org.scala-lang"                         % "scala-library"               % version
   private def scalaCompiler(version: String) = "org.scala-lang"                         % "scala-compiler"              % version
