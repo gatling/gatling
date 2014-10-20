f6f41015f7c1cb3f82858437f11b0e7d5279af68
Stephane Landelle <slandelle@excilys.com>
2014-10-20 17:28:21 +0200

    Upgrade Jodd 3.6.2-BETA1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
   private val boon                           = "io.fastjson"                            % "boon"                        % "0.28"
   private val jsonpath                       = "io.gatling"                            %% "jsonpath"                    % "0.6.1"
   private val uncommonsMaths                 = "io.gatling.uncommons.maths"             % "uncommons-maths"             % "1.2.3"
-  private val joddLagarto                    = "org.jodd"                               % "jodd-lagarto"                % "3.6.1"
+  private val joddLagarto                    = "org.jodd"                               % "jodd-lagarto"                % "3.6.2-BETA1"
   private val jzlib                          = "com.jcraft"                             % "jzlib"                       % "1.1.3"
   private val redisClient                    = "net.debasishg"                         %% "redisclient"                 % "2.13"
   private val zinc                           = "com.typesafe.zinc"                      % "zinc"                        % "0.3.5.3" exclude("org.scala-lang", "scala-compiler")
