4ceb4180c32aa2d7bfa896b18e31831ab8e47a62
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2014-11-08 12:44:33 +0100

    Update redisclient 2.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
   private val uncommonsMaths                 = "io.gatling.uncommons.maths"             % "uncommons-maths"             % "1.2.3"
   private val joddLagarto                    = "org.jodd"                               % "jodd-lagarto"                % "3.6.2"
   private val jzlib                          = "com.jcraft"                             % "jzlib"                       % "1.1.3"
-  private val redisClient                    = "net.debasishg"                         %% "redisclient"                 % "2.13"
+  private val redisClient                    = "net.debasishg"                         %% "redisclient"                 % "2.14"
   private val zinc                           = "com.typesafe.zinc"                      % "zinc"                        % "0.3.5.3" exclude("org.scala-lang", "scala-compiler")
   private val openCsv                        = "net.sf.opencsv"                         % "opencsv"                     % "2.3"
   private val jmsApi                         = "org.apache.geronimo.specs"              % "geronimo-jms_1.1_spec"       % "1.1.1"
