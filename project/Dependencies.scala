0732cae1adb49a1035de8de14001795090d0c894
Stephane Landelle <slandelle@excilys.com>
2014-11-25 17:26:16 +0100

    Upgrade Boon 0.30, close #2411

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
   private val scopt                          = "com.github.scopt"                      %% "scopt"                       % "3.2.0"
   private val scalalogging                   = "com.typesafe.scala-logging"            %% "scala-logging"               % "3.1.0"
   private val jackson                        = "com.fasterxml.jackson.core"             % "jackson-databind"            % "2.4.4"
-  private val boon                           = "io.fastjson"                            % "boon"                        % "0.28"
+  private val boon                           = "io.fastjson"                            % "boon"                        % "0.30"
   private val jsonpath                       = "io.gatling"                            %% "jsonpath"                    % "0.6.2"
   private val joddLagarto                    = "org.jodd"                               % "jodd-lagarto"                % "3.6.2"
   private val jzlib                          = "com.jcraft"                             % "jzlib"                       % "1.1.3"
