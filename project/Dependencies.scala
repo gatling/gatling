6b1b3a7450c645d9571a559bcc673391668052bb
Stephane Landelle <slandelle@excilys.com>
2014-11-26 11:06:06 +0100

    format

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
   private val logbackClassic                 = "ch.qos.logback"                         % "logback-classic"             % "1.1.2"
   private val tdigest                        = "com.tdunning"                           % "t-digest"                    % "3.0"
   private val lru                            = "com.googlecode.concurrentlinkedhashmap" % "concurrentlinkedhashmap-lru" % "1.4"
-  private val bouncycastle                   = "org.bouncycastle"                       % "bcpkix-jdk15on"             % "1.51"
+  private val bouncycastle                   = "org.bouncycastle"                       % "bcpkix-jdk15on"              % "1.51"
 
   /***********************/
   /** Test dependencies **/
