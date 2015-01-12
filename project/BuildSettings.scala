5114693c150ba2c032245cd6ad2ad45563448d0b
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2015-01-12 16:26:01 +0100

    Disable parallel execution of tests

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -13,16 +13,17 @@ import scoverage.ScoverageSbtPlugin.ScoverageKeys
 object BuildSettings {
 
   lazy val basicSettings = Seq(
-    homepage              := Some(url("http://gatling.io")),
-    organization          := "io.gatling",
-    organizationHomepage  := Some(url("http://gatling.io")),
-    startYear             := Some(2011),
-    licenses              := Seq("Apache 2" -> url("http://www.apache.org/licenses/LICENSE-2.0.html")),
-    scalaVersion          := "2.11.5",
-    updateOptions         := updateOptions.value.withCachedResolution(true),
-    resolvers             := Seq(Resolver.mavenLocal, Opts.resolver.sonatypeSnapshots),
-    javacOptions          := Seq("-Xlint:-options","-source", "1.7", "-target", "1.7"),
-    scalacOptions         := Seq(
+    homepage                  := Some(url("http://gatling.io")),
+    organization              := "io.gatling",
+    organizationHomepage      := Some(url("http://gatling.io")),
+    startYear                 := Some(2011),
+    licenses                  := Seq("Apache 2" -> url("http://www.apache.org/licenses/LICENSE-2.0.html")),
+    scalaVersion              := "2.11.5",
+    updateOptions             := updateOptions.value.withCachedResolution(true),
+    resolvers                 := Seq(Resolver.mavenLocal, Opts.resolver.sonatypeSnapshots),
+    javacOptions              := Seq("-Xlint:-options","-source", "1.7", "-target", "1.7"),
+    parallelExecution in Test := false,
+    scalacOptions             := Seq(
       "-encoding", "UTF-8",
       "-target:jvm-1.7",
       "-deprecation",
