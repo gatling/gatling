e0db6d023de8a76d8c74496aee87a1a43055e751
Stephane Landelle <slandelle@excilys.com>
2015-01-12 12:33:45 +0100

    Fix users lazy scheduling, close #2502

--- gatling-core/src/main/scala/io/gatling/core/controller/Controller.scala
+++ gatling-core/src/main/scala/io/gatling/core/controller/Controller.scala
@@ -189,8 +189,11 @@ class Controller extends BaseActor {
       }
 
       // schedule next batch
-      if (stream.hasNext)
-        self ! ScheduleNextUserBatch(scenario.name)
+      if (stream.hasNext) {
+        scheduler.scheduleOnce(batchWindow) {
+          self ! ScheduleNextUserBatch(scenario.name)
+        }
+      }
     }
   }
 
