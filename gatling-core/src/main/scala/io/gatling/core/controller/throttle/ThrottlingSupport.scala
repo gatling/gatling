de633b2f40fe1de3343ecfb6907853fb085977dc
Stephane Landelle <slandelle@excilys.com>
2015-01-14 17:05:18 +0100

    Completely throttle at the end of throttling profile, close #2508

--- gatling-core/src/main/scala/io/gatling/core/controller/throttle/ThrottlingSupport.scala
+++ gatling-core/src/main/scala/io/gatling/core/controller/throttle/ThrottlingSupport.scala
@@ -58,10 +58,10 @@ case class Throttling(override val steps: List[ThrottleStep]) extends Throttling
 
   def profile: ThrottlingProfile = {
 
-    val userStream: (Long => Int) = {
+    val limit: (Long => Int) = {
         @tailrec
         def valueAt(steps: List[ThrottleStep], pendingTime: Long, previousLastValue: Int): Int = steps match {
-          case Nil => Int.MaxValue
+          case Nil => 0
           case head :: tail =>
             if (pendingTime < head.durationInSec)
               head.rps(pendingTime, previousLastValue)
@@ -81,6 +81,6 @@ case class Throttling(override val steps: List[ThrottleStep]) extends Throttling
       }
     }
 
-    ThrottlingProfile(userStream, duration)
+    ThrottlingProfile(limit, duration)
   }
 }
