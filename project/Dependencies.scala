fadfcdddbd02d4248d58dc1db7af7238a7c91be9
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2015-01-29 15:34:48 +0100

    Upgrade Scalatest 2.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -40,7 +40,7 @@ object Dependencies {
   /** Test dependencies **/
   /***********************/
 
-  private val scalaTest                      = "org.scalatest"                         %% "scalatest"                   % "2.2.3"       % "test"
+  private val scalaTest                      = "org.scalatest"                         %% "scalatest"                   % "2.2.4"       % "test"
   private val scalaCheck                     = "org.scalacheck"                        %% "scalacheck"                  % "1.12.1"      % "test"
   private val akkaTestKit                    = "com.typesafe.akka"                     %% "akka-testkit"                % akkaVersion   % "test"
   private val mockitoCore                    = "org.mockito"                            % "mockito-core"                % "1.10.19"     % "test"
