bc7ec125f28fd1e5d5d2735c4a14657d8f4a9ebe
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2015-03-31 08:26:39 +0200

    Add UserEndSpec

---
+++ gatling-core/src/test/scala/io/gatling/core/action/UserEndSpec.scala
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2011-2015 eBusiness Information, Groupe Excilys (www.ebusinessinformation.fr)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gatling.core.action
+
+import akka.testkit._
+import io.gatling.AkkaSpec
+
+import io.gatling.core.result.message.End
+import io.gatling.core.result.writer.UserMessage
+import io.gatling.core.session.Session
+
+class UserEndSpec extends AkkaSpec {
+
+  "UserEnd" should "terminate the session and notify the Controller execution has ended" in {
+    val userEnd = TestActorRef(UserEnd.props(self))
+
+    var hasTerminated = false
+
+    val session = Session("scenario", "userId", userEnd = _ => hasTerminated = true)
+
+    userEnd ! session
+
+    hasTerminated shouldBe true
+    val userMessage = expectMsgType[UserMessage]
+    userMessage.session shouldBe session
+    userMessage.event shouldBe End
+
+  }
+}
+
