cdf2bbc30929a64fb0082948ed5129892ab969a0
Stephane Landelle <slandelle@gatling.io>
2015-04-02 23:35:39 +0200

    Can't schedule this long

--- gatling-core/src/main/scala/io/gatling/core/runner/Runner.scala
+++ gatling-core/src/main/scala/io/gatling/core/runner/Runner.scala
@@ -77,7 +77,7 @@ class Runner(selection: Selection)(implicit configuration: GatlingConfiguration)
       System.gc()
       System.gc()
 
-      val timeout = Int.MaxValue seconds
+      val timeout = 1 week
 
       val runResult = controller.ask(Run(simulationDef))(timeout).mapTo[Try[String]]
 
