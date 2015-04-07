308640ea03c37fc2a86f59fdd5a584a2af220fa7
Stephane Landelle <slandelle@gatling.io>
2015-04-07 17:22:55 +0200

    Upgrade tdigest 3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
   private val zinc                          = "com.typesafe.zinc"                      % "zinc"                        % "0.3.7" exclude("org.scala-lang", "scala-compiler")
   private val jmsApi                        = "org.apache.geronimo.specs"              % "geronimo-jms_1.1_spec"       % "1.1.1"
   private val logbackClassic                = "ch.qos.logback"                         % "logback-classic"             % "1.1.3"
-  private val tdigest                       = "com.tdunning"                           % "t-digest"                    % "3.0"
+  private val tdigest                       = "com.tdunning"                           % "t-digest"                    % "3.1"
   private val lru                           = "com.googlecode.concurrentlinkedhashmap" % "concurrentlinkedhashmap-lru" % "1.4.2"
   private val bouncycastle                  = "org.bouncycastle"                       % "bcpkix-jdk15on"              % "1.52"
   private val testInterface                 = "org.scala-sbt"                          % "test-interface"              % "1.0"
