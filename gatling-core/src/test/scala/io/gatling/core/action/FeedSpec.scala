cb8480eb28b6a99a187569fb1a30f870e59bce1b
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2015-04-11 00:12:35 +0200

    Add FeedSpec

---
+++ gatling-core/src/test/scala/io/gatling/core/action/FeedSpec.scala
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2011-2015 eBusiness Information, Groupe Excilys (www.ebusinessinformation.fr)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gatling.core.action
+
+import io.gatling.AkkaSpec
+import io.gatling.core.result.writer.{ DataWriters, GroupMessage }
+import io.gatling.core.session._
+import io.gatling.core.validation._
+
+import akka.testkit._
+
+class FeedSpec extends AkkaSpec {
+
+  "Feed" should "send a FeedMessage to the SingletonFeed actor" in {
+    val dataWriterProbe = TestProbe()
+    val dataWriters = new DataWriters(system, List(dataWriterProbe.ref))
+    val singleton = TestProbe()
+    val controller = TestProbe()
+    val number: Expression[Int] = session => 1.success
+
+    val feed = TestActorRef(Feed.props(singleton.ref, controller.ref, number, dataWriters, self))
+
+    val session = Session("scenario", "userId")
+
+    feed ! session
+
+    val feedMessage = singleton.expectMsgType[FeedMessage]
+    feedMessage.session shouldBe session
+    feedMessage.controller shouldBe controller.ref
+    feedMessage.next shouldBe self
+  }
+}
+
