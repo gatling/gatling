4ea65b56eccf474b1bf88370e70f3b00fd5cc908
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2015-04-16 11:00:52 +0200

    Update license headers

--- gatling-core/src/main/scala/io/gatling/core/util/FastBufferedOutputStream.scala
+++ gatling-core/src/main/scala/io/gatling/core/util/FastBufferedOutputStream.scala
@@ -1,11 +1,11 @@
 /**
- * Copyright 2011-2014 eBusiness Information, Groupe Excilys (www.ebusinessinformation.fr)
+ * Copyright 2011-2015 eBusiness Information, Groupe Excilys (www.ebusinessinformation.fr)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * 		http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -44,4 +44,3 @@ final class FastBufferedOutputStream(os: OutputStream, bufferSize: Int) extends 
     }
   }
 }
-
--- gatling-core/src/main/scala/io/gatling/core/util/cache/Cache.scala
+++ gatling-core/src/main/scala/io/gatling/core/util/cache/Cache.scala
@@ -1,11 +1,11 @@
 /**
- * Copyright 2014 eBusiness Information, Groupe Excilys (www.ebusinessinformation.fr)
+ * Copyright 2011-2015 eBusiness Information, Groupe Excilys (www.ebusinessinformation.fr)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -56,4 +56,3 @@ class Cache[K, V](queue: Queue[K], map: Map[K, V], maxCapacity: Int) {
 
   def get(key: K): Option[V] = map.get(key)
 }
-
