ae0ef140d418844b800d29de71246f02be2c7656
Stephane Landelle <slandelle@gatling.io>
2015-05-08 21:48:16 +0200

    Upgrade Netty 3.10.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   private val scalaXml                      = "org.scala-lang.modules"                %% "scala-xml"                   % "1.0.4"
   private val scalaParserCombinators        = "org.scala-lang.modules"                %% "scala-parser-combinators"    % "1.0.4"
   private val ahc                           = "com.ning"                               % "async-http-client"           % "1.9.21"
-  private val netty                         = "io.netty"                               % "netty"                       % "3.10.1.Final"
+  private val netty                         = "io.netty"                               % "netty"                       % "3.10.3.Final"
   private val dnsJava                       = "dnsjava"                                % "dnsjava"                     % "2.1.7"
   private val akkaActor                     = "com.typesafe.akka"                     %% "akka-actor"                  % "2.3.10"
   private val config                        = "com.typesafe"                           % "config"                      % "1.2.1"
