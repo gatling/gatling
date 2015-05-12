a6501dfe2a8af266da706606f1400dfdd8158afd
Stephane Landelle <slandelle@gatling.io>
2015-05-12 13:02:08 +0200

    Upgrade Akka 2.3.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   /** Compile dependencies **/
   /**************************/
 
-  private val akkaVersion                    = "2.3.10"
+  private val akkaVersion                    = "2.3.11"
 
   private def scalaReflect(version: String) = "org.scala-lang"                         % "scala-reflect"               % version
   private val scalaSwing                    = "org.scala-lang.modules"                %% "scala-swing"                 % "1.0.2"
