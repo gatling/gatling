7e3ab13f6d6de0e0efe793aa8b78c080e440dcde
Stephane Landelle <slandelle@gatling.io>
2015-05-26 14:47:19 +0200

    Upgrade Jodd 3.6.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
   private val jacksonCsv                    = "com.fasterxml.jackson.dataformat"       % "jackson-dataformat-csv"      % jackson.revision
   private val boon                          = "io.fastjson"                            % "boon"                        % "0.33"
   private val jsonpath                      = "io.gatling"                            %% "jsonpath"                    % "0.6.4"
-  private val joddLagarto                   = "org.jodd"                               % "jodd-lagarto"                % "3.6.5"
+  private val joddLagarto                   = "org.jodd"                               % "jodd-lagarto"                % "3.6.6"
   private val jzlib                         = "com.jcraft"                             % "jzlib"                       % "1.1.3"
   private val redisClient                   = "net.debasishg"                         %% "redisclient"                 % "2.15"
   private val zinc                          = "com.typesafe.zinc"                      % "zinc"                        % "0.3.7" exclude("org.scala-lang", "scala-compiler")
