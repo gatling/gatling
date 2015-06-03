d0739ce76ab22be7de65538017a27655233fd6f6
Stephane Landelle <slandelle@gatling.io>
2015-06-03 18:06:34 +0200

    Use CompositePickler

--- gatling-core/src/main/scala/io/gatling/core/assertion/AssertionCodec.scala
+++ gatling-core/src/main/scala/io/gatling/core/assertion/AssertionCodec.scala
@@ -17,260 +17,49 @@ package io.gatling.core.assertion
 
 import boopickle._
 
-trait AssertionCodec extends PathCodec with TargetCodec with ConditionCodec
+trait AssertionCodec {
 
-trait PathCodec {
+  implicit val defaultPathPicklerPair =
+    CompositePickler[Path]
+      .addConcreteType[Global.type]
+      .addConcreteType[ForAll.type]
+      .addConcreteType[Details]
 
-  implicit val defaultPathPickler: Pickler[Path] = new Pickler[Path] with PicklerHelper {
+  implicit val defaultConditionPicklerPair =
+    CompositePickler[Condition]
+      .addConcreteType[LessThan]
+      .addConcreteType[GreaterThan]
+      .addConcreteType[Is]
+      .addConcreteType[Between]
+      .addConcreteType[In]
 
-    def pickle(obj: Path)(implicit state: PickleState): Unit =
-      state.identityRefFor(obj) match {
-        case Some(idx) =>
-          state.enc.writeInt(-idx)
-        case None =>
-          obj match {
+  implicit val defaultTimeSelectionPicklerPair =
+    CompositePickler[TimeSelection]
+      .addConcreteType[Min.type]
+      .addConcreteType[Max.type]
+      .addConcreteType[Mean.type]
+      .addConcreteType[StandardDeviation.type]
+      .addConcreteType[Percentiles]
 
-            case Global => state.enc.writeInt(0)
-            case ForAll => state.enc.writeInt(1)
-            case Details(parts) =>
-              state.enc.writeInt(2)
-              state.pickle(parts)
-          }
+  implicit val defaultCountSelectionPicklerPair =
+    CompositePickler[CountSelection]
+      .addConcreteType[Count.type]
+      .addConcreteType[Percent.type]
+      .addConcreteType[PerMillion.type]
 
-          state.addIdentityRef(obj)
-      }
-  }
+  implicit val defaultTimeMetricPicklerPair =
+    CompositePickler[TimeMetric]
+      .addConcreteType[ResponseTime.type]
 
-  implicit val defaultPathUnpickler: Unpickler[Path] = new Unpickler[Path] with UnpicklerHelper {
+  implicit val defaultCountMetricPicklerPair =
+    CompositePickler[CountMetric]
+      .addConcreteType[AllRequests.type]
+      .addConcreteType[FailedRequests.type]
+      .addConcreteType[SuccessfulRequests.type]
 
-    override def unpickle(implicit state: UnpickleState): Path =
-      state.dec.readIntCode match {
-        case Right(idx) if idx < 0 =>
-          state.identityFor[Path](-idx)
-
-        case Right(0) => Global
-        case Right(1) => ForAll
-        case Right(2) => Details(read[List[String]])
-        case Right(i) => throw new IllegalArgumentException(s"Invalid coding for Path type: $i")
-      }
-  }
-}
-
-trait TargetCodec extends CountMetricCodec with TimeMetricCodec with CountSelectionCodec with TimeSelectionCodec {
-
-  implicit val defaultTargetPickler: Pickler[Target] = new Pickler[Target] with PicklerHelper {
-
-    def pickle(obj: Target)(implicit state: PickleState): Unit =
-      state.identityRefFor(obj) match {
-        case Some(idx) =>
-          state.enc.writeInt(-idx)
-        case None =>
-          obj match {
-            case CountTarget(metric: CountMetric, selection: CountSelection) =>
-              state.enc.writeInt(0)
-              state.pickle(metric).pickle(selection)
-
-            case TimeTarget(metric: TimeMetric, selection: TimeSelection) =>
-              state.enc.writeInt(1)
-              state.pickle(metric).pickle(selection)
-
-            case MeanRequestsPerSecondTarget =>
-              state.enc.writeInt(2)
-          }
-
-          state.addIdentityRef(obj)
-      }
-  }
-
-  implicit val defaultTargetUnpickler = new Unpickler[Target] with UnpicklerHelper {
-
-    override def unpickle(implicit state: UnpickleState): Target =
-      state.dec.readIntCode match {
-        case Right(idx) if idx < 0 =>
-          state.identityFor[Target](-idx)
-
-        case Right(0) => CountTarget(read[CountMetric], read[CountSelection])
-        case Right(1) => TimeTarget(read[TimeMetric], read[TimeSelection])
-        case Right(2) => MeanRequestsPerSecondTarget
-        case Right(i) => throw new IllegalArgumentException(s"Invalid coding for Target type: $i")
-      }
-  }
-
-}
-
-trait CountMetricCodec {
-
-  implicit val defaultCountMetricPickler: Pickler[CountMetric] = new Pickler[CountMetric] with PicklerHelper {
-
-    def pickle(obj: CountMetric)(implicit state: PickleState): Unit =
-      state.identityRefFor(obj) match {
-        case Some(idx) =>
-          state.enc.writeInt(-idx)
-        case None =>
-          obj match {
-            case AllRequests        => state.enc.writeInt(0)
-            case FailedRequests     => state.enc.writeInt(1)
-            case SuccessfulRequests => state.enc.writeInt(2)
-          }
-
-          state.addIdentityRef(obj)
-      }
-  }
-
-  implicit val defaultCountMetricUnpickler: Unpickler[CountMetric] = new Unpickler[CountMetric] with UnpicklerHelper {
-
-    override def unpickle(implicit state: UnpickleState): CountMetric =
-      state.dec.readIntCode match {
-        case Right(idx) if idx < 0 =>
-          state.identityFor[CountMetric](-idx)
-
-        case Right(0) => AllRequests
-        case Right(1) => FailedRequests
-        case Right(2) => SuccessfulRequests
-        case Right(i) => throw new IllegalArgumentException(s"Invalid coding for CountMetric type: $i")
-      }
-  }
-
-}
-
-trait TimeMetricCodec {
-
-  implicit val defaultTimeMetricPickler: Pickler[TimeMetric] = new Pickler[TimeMetric] with PicklerHelper {
-
-    def pickle(obj: TimeMetric)(implicit state: PickleState): Unit =
-      state.identityRefFor(obj) match {
-        case Some(idx) =>
-          state.enc.writeInt(-idx)
-        case None =>
-          obj match {
-            case ResponseTime => state.enc.writeInt(0)
-          }
-
-          state.addIdentityRef(obj)
-      }
-  }
-
-  implicit val defaultTimeMetricUnpickler: Unpickler[TimeMetric] = new Unpickler[TimeMetric] with UnpicklerHelper {
-
-    override def unpickle(implicit state: UnpickleState): TimeMetric =
-      state.dec.readIntCode match {
-        case Right(idx) if idx < 0 =>
-          state.identityFor[TimeMetric](-idx)
-
-        case Right(0) => ResponseTime
-        case Right(i) => throw new IllegalArgumentException(s"Invalid coding for TimeMetric type: $i")
-      }
-  }
-}
-
-trait CountSelectionCodec {
-
-  implicit val defaultCountSelectionPickler: Pickler[CountSelection] = new Pickler[CountSelection] with PicklerHelper {
-
-    def pickle(obj: CountSelection)(implicit state: PickleState): Unit =
-      state.identityRefFor(obj) match {
-        case Some(idx) =>
-          state.enc.writeInt(-idx)
-        case None =>
-          obj match {
-            case Count      => state.enc.writeInt(0)
-            case Percent    => state.enc.writeInt(1)
-            case PerMillion => state.enc.writeInt(2)
-          }
-
-          state.addIdentityRef(obj)
-      }
-  }
-
-  implicit val defaultCountSelectionUnpickler: Unpickler[CountSelection] = new Unpickler[CountSelection] with UnpicklerHelper {
-
-    override def unpickle(implicit state: UnpickleState): CountSelection =
-      state.dec.readIntCode match {
-        case Right(idx) if idx < 0 =>
-          state.identityFor[CountSelection](-idx)
-
-        case Right(0) => Count
-        case Right(1) => Percent
-        case Right(2) => PerMillion
-        case Right(i) => throw new IllegalArgumentException(s"Invalid coding for CountSelection type: $i")
-      }
-  }
-}
-
-trait TimeSelectionCodec {
-
-  implicit val defaultTimeSelectionPickler: Pickler[TimeSelection] = new Pickler[TimeSelection] with PicklerHelper {
-
-    def pickle(obj: TimeSelection)(implicit state: PickleState): Unit =
-      state.identityRefFor(obj) match {
-        case Some(idx) =>
-          state.enc.writeInt(-idx)
-        case None =>
-          obj match {
-            case Min                => state.enc.writeInt(0)
-            case Max                => state.enc.writeInt(1)
-            case Mean               => state.enc.writeInt(2)
-            case StandardDeviation  => state.enc.writeInt(3)
-            case Percentiles(value) => state.enc.writeInt(4).writeDouble(value)
-          }
-
-          state.addIdentityRef(obj)
-      }
-  }
-
-  implicit val defaultTimeSelectionUnpickler: Unpickler[TimeSelection] = new Unpickler[TimeSelection] with UnpicklerHelper {
-
-    override def unpickle(implicit state: UnpickleState): TimeSelection =
-      state.dec.readIntCode match {
-        case Right(idx) if idx < 0 =>
-          state.identityFor[TimeSelection](-idx)
-
-        case Right(0) => Min
-        case Right(1) => Max
-        case Right(2) => Mean
-        case Right(3) => StandardDeviation
-        case Right(4) => Percentiles(read[Double])
-        case Right(i) => throw new IllegalArgumentException(s"Invalid coding for TimeSelection type: $i")
-      }
-  }
-}
-
-trait ConditionCodec {
-
-  implicit val defaultConditionPickler: Pickler[Condition] = new Pickler[Condition] with PicklerHelper {
-
-    def pickle(obj: Condition)(implicit state: PickleState): Unit =
-      state.identityRefFor(obj) match {
-        case Some(idx) =>
-          state.enc.writeInt(-idx)
-        case None =>
-          obj match {
-            case LessThan(value)                 => state.enc.writeInt(0).writeInt(value)
-            case GreaterThan(value)              => state.enc.writeInt(1).writeInt(value)
-            case Is(value)                       => state.enc.writeInt(2).writeInt(value)
-            case Between(lowerBound, upperBound) => state.enc.writeInt(3).writeInt(lowerBound).writeInt(upperBound)
-            case In(elements) =>
-              state.enc.writeInt(4)
-              state.pickle(elements)
-          }
-
-          state.addIdentityRef(obj)
-      }
-  }
-
-  implicit val defaultConditionUnpickler: Unpickler[Condition] = new Unpickler[Condition] with UnpicklerHelper {
-
-    override def unpickle(implicit state: UnpickleState): Condition =
-      state.dec.readIntCode match {
-        case Right(idx) if idx < 0 =>
-          state.identityFor[Condition](-idx)
-
-        case Right(0) => LessThan(read[Int])
-        case Right(1) => GreaterThan(read[Int])
-        case Right(2) => Is(read[Int])
-        case Right(3) => Between(read[Int], read[Int])
-        case Right(4) => In(read[List[Int]])
-        case Right(i) => throw new IllegalArgumentException(s"Invalid coding for Condition type: $i")
-      }
-  }
+  implicit val defaultTargetPicklerPair =
+    CompositePickler[Target]
+      .addConcreteType[CountTarget]
+      .addConcreteType[TimeTarget]
+      .addConcreteType[MeanRequestsPerSecondTarget.type]
 }
