0bdbcf5410436d3f38db576272c96e995f9a52e0
Stephane Landelle <slandelle@gatling.io>
2015-06-09 15:23:36 +0200

    Upgrade Jackson 2.6.0-rc2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   private val fastring                      = "com.dongxiguo"                         %% "fastring"                            % "0.2.4"
   private val scopt                         = "com.github.scopt"                      %% "scopt"                               % "3.3.0"
   private val scalalogging                  = "com.typesafe.scala-logging"            %% "scala-logging"                       % "3.1.0"
-  private val jackson                       = "com.fasterxml.jackson.core"             % "jackson-databind"                    % "2.6.0-rc1"
+  private val jackson                       = "com.fasterxml.jackson.core"             % "jackson-databind"                    % "2.6.0-rc2"
   private val jacksonCsv                    = "com.fasterxml.jackson.dataformat"       % "jackson-dataformat-csv"              % jackson.revision
   private val boon                          = "io.fastjson"                            % "boon"                                % "0.33"
   private val jsonpath                      = "io.gatling"                            %% "jsonpath"                            % "0.6.4"
