8fb060eb2b641f22f2fdbc7f00e3cdcca35febba
Stephane Landelle <slandelle@gatling.io>
2015-06-22 11:52:19 +0200

    Upgrade AHC 2.0.0-alpha6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,36 +6,36 @@ object Dependencies {
   /** Compile dependencies **/
   /**************************/
 
-  private def scalaReflect(version: String) = "org.scala-lang"                         % "scala-reflect"                       % version
-  private val scalaSwing                    = "org.scala-lang.modules"                %% "scala-swing"                         % "1.0.2"
-  private val scalaXml                      = "org.scala-lang.modules"                %% "scala-xml"                           % "1.0.4"
-  private val scalaParserCombinators        = "org.scala-lang.modules"                %% "scala-parser-combinators"            % "1.0.4"
-  private val ahc                           = "org.asynchttpclient"                    % "async-http-client-netty3-provider"   % "2.0.0-alpha5"
-  private val netty                         = "io.netty"                               % "netty"                               % "3.10.3.Final"
-  private val dnsJava                       = "dnsjava"                                % "dnsjava"                             % "2.1.7"
-  private val akkaActor                     = "com.typesafe.akka"                     %% "akka-actor"                          % "2.3.11"
-  private val config                        = "com.typesafe"                           % "config"                              % "1.2.1"
-  private val saxon                         = "net.sf.saxon"                           % "Saxon-HE"                            % "9.6.0-6"
-  private val slf4jApi                      = "org.slf4j"                              % "slf4j-api"                           % "1.7.12"
-  private val fastring                      = "com.dongxiguo"                         %% "fastring"                            % "0.2.4"
-  private val scopt                         = "com.github.scopt"                      %% "scopt"                               % "3.3.0"
-  private val scalalogging                  = "com.typesafe.scala-logging"            %% "scala-logging"                       % "3.1.0"
-  private val jackson                       = "com.fasterxml.jackson.core"             % "jackson-databind"                    % "2.6.0-rc2"
-  private val jacksonCsv                    = "com.fasterxml.jackson.dataformat"       % "jackson-dataformat-csv"              % jackson.revision
-  private val boon                          = "io.fastjson"                            % "boon"                                % "0.33"
-  private val jsonpath                      = "io.gatling"                            %% "jsonpath"                            % "0.6.4"
-  private val joddLagarto                   = "org.jodd"                               % "jodd-lagarto"                        % "3.6.6"
-  private val boopickle                     = "me.chrons"                             %% "boopickle"                           % "1.0.0"
-  private val jzlib                         = "com.jcraft"                             % "jzlib"                               % "1.1.3"
-  private val redisClient                   = "net.debasishg"                         %% "redisclient"                         % "2.15"
-  private val zinc                          = "com.typesafe.zinc"                      % "zinc"                                % "0.3.7" exclude("org.scala-lang", "scala-compiler")
-  private val jmsApi                        = "org.apache.geronimo.specs"              % "geronimo-jms_1.1_spec"               % "1.1.1"
-  private val logbackClassic                = "ch.qos.logback"                         % "logback-classic"                     % "1.1.3"
-  private val tdigest                       = "com.tdunning"                           % "t-digest"                            % "3.1"
-  private val hdrHistogram                  = "org.hdrhistogram"                       % "HdrHistogram"                        % "2.1.4"
-  private val lru                           = "com.googlecode.concurrentlinkedhashmap" % "concurrentlinkedhashmap-lru"         % "1.4.2"
-  private val bouncycastle                  = "org.bouncycastle"                       % "bcpkix-jdk15on"                      % "1.52"
-  private val testInterface                 = "org.scala-sbt"                          % "test-interface"                      % "1.0"
+  private def scalaReflect(version: String) = "org.scala-lang"                         % "scala-reflect"                % version
+  private val scalaSwing                    = "org.scala-lang.modules"                %% "scala-swing"                  % "1.0.2"
+  private val scalaXml                      = "org.scala-lang.modules"                %% "scala-xml"                    % "1.0.4"
+  private val scalaParserCombinators        = "org.scala-lang.modules"                %% "scala-parser-combinators"     % "1.0.4"
+  private val ahc                           = "org.asynchttpclient"                    % "async-http-client-netty3"     % "2.0.0-alpha6"
+  private val netty                         = "io.netty"                               % "netty"                        % "3.10.3.Final"
+  private val dnsJava                       = "dnsjava"                                % "dnsjava"                      % "2.1.7"
+  private val akkaActor                     = "com.typesafe.akka"                     %% "akka-actor"                   % "2.3.11"
+  private val config                        = "com.typesafe"                           % "config"                       % "1.2.1"
+  private val saxon                         = "net.sf.saxon"                           % "Saxon-HE"                     % "9.6.0-6"
+  private val slf4jApi                      = "org.slf4j"                              % "slf4j-api"                    % "1.7.12"
+  private val fastring                      = "com.dongxiguo"                         %% "fastring"                     % "0.2.4"
+  private val scopt                         = "com.github.scopt"                      %% "scopt"                        % "3.3.0"
+  private val scalalogging                  = "com.typesafe.scala-logging"            %% "scala-logging"                % "3.1.0"
+  private val jackson                       = "com.fasterxml.jackson.core"             % "jackson-databind"             % "2.6.0-rc2"
+  private val jacksonCsv                    = "com.fasterxml.jackson.dataformat"       % "jackson-dataformat-csv"       % jackson.revision
+  private val boon                          = "io.fastjson"                            % "boon"                         % "0.33"
+  private val jsonpath                      = "io.gatling"                            %% "jsonpath"                     % "0.6.4"
+  private val joddLagarto                   = "org.jodd"                               % "jodd-lagarto"                 % "3.6.6"
+  private val boopickle                     = "me.chrons"                             %% "boopickle"                    % "1.0.0"
+  private val jzlib                         = "com.jcraft"                             % "jzlib"                        % "1.1.3"
+  private val redisClient                   = "net.debasishg"                         %% "redisclient"                  % "2.15"
+  private val zinc                          = "com.typesafe.zinc"                      % "zinc"                         % "0.3.7" exclude("org.scala-lang", "scala-compiler")
+  private val jmsApi                        = "org.apache.geronimo.specs"              % "geronimo-jms_1.1_spec"        % "1.1.1"
+  private val logbackClassic                = "ch.qos.logback"                         % "logback-classic"              % "1.1.3"
+  private val tdigest                       = "com.tdunning"                           % "t-digest"                     % "3.1"
+  private val hdrHistogram                  = "org.hdrhistogram"                       % "HdrHistogram"                 % "2.1.4"
+  private val lru                           = "com.googlecode.concurrentlinkedhashmap" % "concurrentlinkedhashmap-lru"  % "1.4.2"
+  private val bouncycastle                  = "org.bouncycastle"                       % "bcpkix-jdk15on"               % "1.52"
+  private val testInterface                 = "org.scala-sbt"                          % "test-interface"               % "1.0"
 
   /***********************/
   /** Test dependencies **/
