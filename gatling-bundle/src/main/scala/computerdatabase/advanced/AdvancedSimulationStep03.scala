00446a97aabf18463892676d085cbb50e7a8cdac
Stephane Landelle <slandelle@gatling.io>
2015-07-07 11:41:51 +0200

    css selector, not regex

--- gatling-bundle/src/main/scala/computerdatabase/advanced/AdvancedSimulationStep03.scala
+++ gatling-bundle/src/main/scala/computerdatabase/advanced/AdvancedSimulationStep03.scala
@@ -34,7 +34,7 @@ class AdvancedSimulationStep03 extends Simulation {
       .feed(feeder) // every time a user passes here, a record is popped from the feeder and injected into the user's session
       .exec(http("Search")
         .get("/computers?f=${searchCriterion}") // use session data thanks to Gatling's EL
-        .check(css("a:contains('${searchComputerName}')", "href").saveAs("computerURL"))) // use a regex with an EL, save the result of the capture group
+        .check(css("a:contains('${searchComputerName}')", "href").saveAs("computerURL"))) // use a CSS selector with an EL, save the result of the capture group
       .pause(1)
       .exec(http("Select")
         .get("${computerURL}") // use the link previously saved
