73475ffeb1de350fa5620b863837f2a44272e445
Stephane Landelle <slandelle@gatling.io>
2015-08-05 17:11:07 +0200

    Don't crash deepList if Path doesn't exist, close #2787

--- gatling-app/src/main/scala/io/gatling/app/classloader/SimulationClassLoader.scala
+++ gatling-app/src/main/scala/io/gatling/app/classloader/SimulationClassLoader.scala
@@ -56,5 +56,4 @@ private[app] class SimulationClassLoader(classLoader: ClassLoader, binaryDir: Pa
       .deepFiles
       .collect { case file if file.hasExtension("class") => classLoader.loadClass(pathToClassName(file, binaryDir)) }
       .collect { case clazz if isSimulationClass(clazz) => clazz.asInstanceOf[Class[Simulation]] }
-      .toList
 }
