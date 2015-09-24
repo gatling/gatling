8b1765a6e9f8a6268a12f485806c3548c14888e9
Stephane Landelle <slandelle@gatling.io>
2015-09-24 14:06:14 +0200

    fix sumBy

--- gatling-commons/src/main/scala/io/gatling/commons/util/Collections.scala
+++ gatling-commons/src/main/scala/io/gatling/commons/util/Collections.scala
@@ -21,7 +21,7 @@ object Collections {
 
     def sumBy[B](f: A => B)(implicit num: Numeric[B]): B = {
       var sum = num.zero
-      for(x <- this) sum = num.plus(sum, f(x))
+      for (x <- t) sum = num.plus(sum, f(x))
       sum
     }
   }
