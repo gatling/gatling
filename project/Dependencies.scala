757b9edccee672cfa6c6fcddb7c451159e24d64a
Stephane Landelle <slandelle@gatling.io>
2016-01-20 16:40:22 +0100

    Upgrade Saxon 9.7.0-2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   private val nettyNativeTransport          = "io.netty"                               % "netty-transport-native-epoll" % netty.revision classifier "linux-x86_64"
   private val akkaActor                     = "com.typesafe.akka"                     %% "akka-actor"                   % "2.4.1"
   private val config                        = "com.typesafe"                           % "config"                       % "1.3.0"
-  private val saxon                         = "net.sf.saxon"                           % "Saxon-HE"                     % "9.7.0-1"
+  private val saxon                         = "net.sf.saxon"                           % "Saxon-HE"                     % "9.7.0-2"
   private val slf4jApi                      = "org.slf4j"                              % "slf4j-api"                    % "1.7.13"
   private val fastring                      = "com.dongxiguo"                         %% "fastring"                     % "0.2.4"
   private val scopt                         = "com.github.scopt"                      %% "scopt"                        % "3.3.0"
