c652b659a424a0076eb9b1aa19607c5c94a37e40
Stephane Landelle <slandelle@gatling.io>
2016-02-01 20:35:35 +0100

    Initialize HttpRequestExpressionBuilderBenchmark

---
+++ gatling-benchmarks/src/main/scala/io/gatling/http/request/builder/HttpRequestExpressionBuilderBenchmark.scala
@@ -0,0 +1,74 @@
+/**
+ * Copyright 2011-2016 GatlingCorp (http://gatling.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gatling.http.request.builder
+
+import io.gatling.commons.validation.Validation
+import io.gatling.core.config.GatlingConfiguration
+import io.gatling.core.session._
+import io.gatling.http.ahc.HttpEngine
+import io.gatling.http.cache.HttpCaches
+import io.gatling.http.protocol._
+
+import org.asynchttpclient.Request
+import org.openjdk.jmh.annotations.Benchmark
+import com.softwaremill.quicklens._
+import org.mockito.Mockito._
+
+object HttpRequestExpressionBuilderBenchmark {
+
+  val TheBuilder: HttpRequestExpressionBuilder = {
+
+    implicit val config = GatlingConfiguration.loadForTest()
+
+    val httpProtocol = HttpProtocolBuilder(config)
+      .baseURL("http://localhost:8000")
+      .build
+
+    val httpEngine = mock(classOf[HttpEngine])
+    when(httpEngine.configuration).thenReturn(config)
+
+    val httpCaches = new HttpCaches
+
+    new HttpRequestExpressionBuilder(
+      commonAttributes = CommonAttributes(
+        requestName = "Ping".expressionSuccess,
+        method = "GET",
+        urlOrURI = Left("/ping".expressionSuccess),
+        queryParams = Nil,
+        headers = Map.empty
+      ),
+      httpAttributes = HttpAttributes(
+        checks = Nil
+      ),
+      httpComponents = HttpComponents(
+        httpProtocol = httpProtocol,
+        httpEngine = httpEngine,
+        httpCaches = httpCaches
+      )
+    )
+  }
+
+  val TheSession: Session = Session("scenario", 0)
+}
+
+class HttpRequestExpressionBuilderBenchmark {
+
+  import HttpRequestExpressionBuilderBenchmark._
+
+  @Benchmark
+  def test(): Validation[Request] =
+    TheBuilder.build(TheSession)
+}
