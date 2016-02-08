05c489b1e0439f613454ff34705fc627fdf93ec0
Stephane Landelle <slandelle@gatling.io>
2016-02-08 13:19:46 +0100

    Upgrade JsonPath 0.6.5, close gatling/gatling#2905

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
   private val jackson                       = "com.fasterxml.jackson.core"             % "jackson-databind"             % "2.7.1-1"
   private val jacksonCsv                    = "com.fasterxml.jackson.dataformat"       % "jackson-dataformat-csv"       % "2.7.1"
   private val boon                          = "io.gatling.advantageous.boon"           % "boon-json"                    % "0.5.7-RC1"
-  private val jsonpath                      = "io.gatling"                            %% "jsonpath"                     % "0.6.4"
+  private val jsonpath                      = "io.gatling"                            %% "jsonpath"                     % "0.6.5"
   private val joddLagarto                   = "org.jodd"                               % "jodd-lagarto"                 % "3.6.7"
   private val boopickle                     = "me.chrons"                             %% "boopickle"                    % "1.1.2"
   private val redisClient                   = "net.debasishg"                         %% "redisclient"                  % "3.0"
