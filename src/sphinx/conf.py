e548559aa0fcd70101d25f7ffb8ca5004fbb32f5
notdryft <gui.corre@gmail.com>
2016-04-14 16:59:35 +0200

    Update doc copyright and version

--- src/sphinx/conf.py
+++ src/sphinx/conf.py
@@ -21,8 +21,8 @@ extlinks = {
 ################
 
 project = u'Gatling'
-copyright = u'2015 Gatling Corp 2000-2015'
-version = '2.0.0-SNAPSHOT'
+copyright = u'2016 Gatling Corp 2000-2016'
+version = '2.2.1-SNAPSHOT'
 release = version
 
 ####################
