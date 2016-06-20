bf6f4bafc9154c927fe8a8b4f1cf00eb28630656
Stephane Landelle <slandelle@gatling.io>
2016-06-20 13:31:55 +0200

    Upgrade Boopickle 1.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
   private val boon                          = "io.advantageous.boon"                   % "boon-json"                    % "0.6.2"
   private val jsonpath                      = "io.gatling"                            %% "jsonpath"                     % "0.6.7"
   private val joddLagarto                   = "org.jodd"                               % "jodd-lagarto"                 % "3.7.1"
-  private val boopickle                     = "me.chrons"                             %% "boopickle"                    % "1.2.0"
+  private val boopickle                     = "me.chrons"                             %% "boopickle"                    % "1.2.2"
   private val redisClient                   = "net.debasishg"                         %% "redisclient"                  % "3.1"
   private val zinc                          = "com.typesafe.zinc"                      % "zinc"                         % "0.3.9" exclude("org.scala-lang", "scala-compiler")
   private val jmsApi                        = "org.apache.geronimo.specs"              % "geronimo-jms_1.1_spec"        % "1.1.1"
