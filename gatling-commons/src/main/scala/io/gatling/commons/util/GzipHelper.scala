1a0c5fdeda53eea66985e59a7121e6a6c86814dd
Stephane Landelle <slandelle@gatling.io>
2016-08-19 11:58:02 +0200

    Recycle Deflaters, close gatling/gatling#3111

--- gatling-commons/src/main/scala/io/gatling/commons/util/GzipHelper.scala
+++ gatling-commons/src/main/scala/io/gatling/commons/util/GzipHelper.scala
@@ -15,8 +15,8 @@
  */
 package io.gatling.commons.util
 
-import java.io.{ ByteArrayOutputStream, InputStream }
-import java.util.zip.GZIPOutputStream
+import java.io.{ ByteArrayOutputStream, InputStream, OutputStream }
+import java.util.zip.{ CRC32, Deflater, DeflaterOutputStream }
 
 import io.gatling.commons.util.Io._
 
@@ -32,11 +32,117 @@ object GzipHelper {
   def gzip(in: InputStream, bufferSize: Int = GzipDefaultBufferSize): Array[Byte] =
     withCloseable(in) { is =>
       val out = new ByteArrayOutputStream(bufferSize)
-
-      withCloseable(new GZIPOutputStream(out)) {
-        in.copyTo(_, bufferSize)
+      val gzip = ReusableGzipOutputStream.forStream(out)
+      try {
+        gzip.writeHeader()
+        in.copyTo(gzip, bufferSize)
+        gzip.finish()
+      } finally {
+        gzip.reset()
       }
 
       out.toByteArray
     }
+}
+
+class SettableOutputStream(var target: OutputStream) extends OutputStream {
+
+  override def write(b: Array[Byte], off: Int, len: Int): Unit =
+    target.write(b, off, len)
+
+  override def write(b: Array[Byte]): Unit =
+    target.write(b)
+
+  override def write(b: Int): Unit =
+    target.write(b.toByte)
+}
+
+object ReusableGzipOutputStream {
+
+  private val GzipMagic = 0x8b1f
+
+  private val TrailerSize = 8
+
+  private val Streams = new ThreadLocal[ReusableGzipOutputStream] {
+    override protected def initialValue() = new ReusableGzipOutputStream(new SettableOutputStream(null))
+  }
+
+  def forStream(target: OutputStream): ReusableGzipOutputStream = {
+    val gzip = Streams.get()
+    gzip.os.target = target
+    gzip
+  }
+}
+
+class ReusableGzipOutputStream(val os: SettableOutputStream)
+    extends DeflaterOutputStream(os, new Deflater(Deflater.DEFAULT_COMPRESSION, true)) {
+
+  import ReusableGzipOutputStream._
+
+  private val crc = new CRC32
+
+  override def write(buf: Array[Byte], off: Int, len: Int): Unit = {
+    super.write(buf, off, len)
+    crc.update(buf, off, len)
+  }
+
+  override def finish(): Unit =
+    if (!`def`.finished) {
+      `def`.finish()
+      while (!`def`.finished) {
+        var len = `def`.deflate(buf, 0, buf.length)
+        if (`def`.finished && len <= buf.length - TrailerSize) {
+          writeTrailer(buf, len)
+          len = len + TrailerSize
+          os.write(buf, 0, len)
+          return
+        }
+        if (len > 0)
+          out.write(buf, 0, len)
+      }
+      val trailer = Array.ofDim[Byte](TrailerSize)
+      writeTrailer(trailer, 0)
+      out.write(trailer)
+    }
+
+  def writeHeader(): Unit =
+    os.write(Array(
+      GzipMagic.toByte, // Magic number (short)
+      (GzipMagic >> 8).toByte, // Magic number (short)
+      Deflater.DEFLATED.toByte, // Compression method (CM)
+      0.toByte, // Flags (FLG)
+      0.toByte, // Modification time MTIME (int)
+      0.toByte, // Modification time MTIME (int)
+      0.toByte, // Modification time MTIME (int)
+      0.toByte, // Modification time MTIME (int)
+      0.toByte, // Extra flags (XFLG)
+      0.toByte // Operating system (OS)
+    ))
+
+  private def writeTrailer(buf: Array[Byte], offset: Int): Unit = {
+    writeInt(crc.getValue.toInt, buf, offset)
+    writeInt(`def`.getTotalIn, buf, offset + 4)
+  }
+
+  /*
+   * Writes integer in Intel byte order to a byte array, starting at a given offset.
+   */
+  private def writeInt(i: Int, buf: Array[Byte], offset: Int): Unit = {
+    writeShort(i & 0xffff, buf, offset)
+    writeShort((i >> 16) & 0xffff, buf, offset + 2)
+  }
+
+  /*
+   * Writes short integer in Intel byte order to a byte array, starting at a given offset
+   */
+  private def writeShort(s: Int, buf: Array[Byte], offset: Int): Unit = {
+    buf(offset) = (s & 0xff).toByte
+    buf(offset + 1) = ((s >> 8) & 0xff).toByte
+  }
+
+  def reset(): Unit = {
+    crc.reset()
+    `def`.reset()
+    os.target = null
+  }
 }
