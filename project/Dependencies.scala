f93105298b5a7cb8f9174d7765efaacb7b5cacb8
Stephane Landelle <slandelle@gatling.io>
2016-09-25 16:10:39 +0200

    Upgrade config 1.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   private val nettyNativeTransport          = "io.netty"                               % "netty-transport-native-epoll"  % netty.revision classifier "linux-x86_64"
   private val akkaActor                     = "com.typesafe.akka"                     %% "akka-actor"                    % "2.4.9"
   private val akkaSlf4j                     = "com.typesafe.akka"                     %% "akka-slf4j"                    % akkaActor.revision
-  private val config                        = "com.typesafe"                           % "config"                        % "1.3.0"
+  private val config                        = "com.typesafe"                           % "config"                        % "1.3.1"
   private val saxon                         = "net.sf.saxon"                           % "Saxon-HE"                      % "9.7.0-8"
   private val slf4jApi                      = "org.slf4j"                              % "slf4j-api"                     % "1.7.21"
   private val fastring                      = "com.dongxiguo"                         %% "fastring"                      % "0.2.4"
