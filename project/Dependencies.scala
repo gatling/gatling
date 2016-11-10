9f001a5dc35ad732ad611fa2077e5e11d5b9d41d
Stephane Landelle <slandelle@gatling.io>
2016-11-10 22:46:39 +0100

    Upgrade scalatest 3.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -44,7 +44,7 @@ object Dependencies {
   /** Test dependencies **/
   /***********************/
 
-  private val scalaTest                      = "org.scalatest"                         %% "scalatest"                    % "3.0.0"             % "test"
+  private val scalaTest                      = "org.scalatest"                         %% "scalatest"                    % "3.0.1"             % "test"
   private val scalaCheck                     = "org.scalacheck"                        %% "scalacheck"                   % "1.13.4"            % "test"
   private val akkaTestKit                    = "com.typesafe.akka"                     %% "akka-testkit"                 % akkaActor.revision  % "test"
   private val mockitoCore                    = "org.mockito"                            % "mockito-core"                 % "1.10.19"           % "test"
