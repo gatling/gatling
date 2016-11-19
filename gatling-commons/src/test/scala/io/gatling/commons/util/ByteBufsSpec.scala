a681d2c43724c69e238a60f5bccfc9619447d635
bedrin <Dmitry.Bedrin@gmail.com>
2016-11-19 22:04:41 +0300

    Fix java.lang.IndexOutOfBoundsException in io.gatling.commons.util.ByteBufs

---
+++ gatling-commons/src/test/scala/io/gatling/commons/util/ByteBufsSpec.scala
@@ -0,0 +1,39 @@
+/**
+  * Copyright 2011-2016 GatlingCorp (http://gatling.io)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package io.gatling.commons.util
+
+import io.gatling.BaseSpec
+import io.gatling.commons.util.ByteBufs.byteBufsToByteArray
+
+import io.netty.buffer.{ ByteBuf, Unpooled }
+
+class ByteBufsSpec extends BaseSpec {
+
+  it should "be able to extract data from multiple ByteBuf instances" in {
+    val buffer1: ByteBuf = Unpooled.buffer(3)
+    val buffer2: ByteBuf = Unpooled.buffer(5)
+
+    try {
+      buffer1.writeBytes(Array[Byte](0, 1, 2))
+      buffer2.writeBytes(Array[Byte](0, 1, 2, 3, 4))
+
+      byteBufsToByteArray(Seq(buffer1, buffer2)) shouldBe Array[Byte](0, 1, 2, 0, 1, 2, 3, 4)
+    } finally {
+      buffer1.release()
+      buffer2.release()
+    }
+  }
+}
