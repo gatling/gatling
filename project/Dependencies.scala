d09d506e1b4cc4b671de589da1cd8727cacf8cb2
Stephane Landelle <slandelle@gatling.io>
2016-12-20 21:04:00 +0100

    Upgrade slf4j 1.7.22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   private val akkaSlf4j                     = "com.typesafe.akka"                     %% "akka-slf4j"                    % akkaActor.revision
   private val config                        = "com.typesafe"                           % "config"                        % "1.3.1"
   private val saxon                         = "net.sf.saxon"                           % "Saxon-HE"                      % "9.7.0-14"
-  private val slf4jApi                      = "org.slf4j"                              % "slf4j-api"                     % "1.7.21"
+  private val slf4jApi                      = "org.slf4j"                              % "slf4j-api"                     % "1.7.22"
   private val fastring                      = "com.dongxiguo"                         %% "fastring"                      % "0.2.5"
   private val scopt                         = "com.github.scopt"                      %% "scopt"                         % "3.5.0"
   private val scalalogging                  = "com.typesafe.scala-logging"            %% "scala-logging"                 % "3.5.0"
