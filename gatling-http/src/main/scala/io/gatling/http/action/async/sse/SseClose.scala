a3d2f4b44ac292931599a1b6c4cb72515d7a4e96
Stephane Landelle <slandelle@gatling.io>
2017-01-03 13:09:46 +0100

    Update copyright year

--- gatling-http/src/main/scala/io/gatling/http/action/async/sse/SseClose.scala
+++ gatling-http/src/main/scala/io/gatling/http/action/async/sse/SseClose.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011-2016 GatlingCorp (http://gatling.io)
+ * Copyright 2011-2017 GatlingCorp (http://gatling.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,4 +29,3 @@ class SseClose(
 ) extends CloseAction(requestName, sseName, statsEngine, next) with SseAction with NameGen {
   override val name = genName("sseClose")
 }
-
