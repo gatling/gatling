082d0e8b125586968dd80e172fc892f00309fe48
Stephane Landelle <slandelle@gatling.io>
2017-01-12 15:23:37 +0100

    Upgrade AHC 2.0.25 and Netty 4.0.43.Final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,9 +11,9 @@ object Dependencies {
   private val scalaXml                      = "org.scala-lang.modules"                %% "scala-xml"                     % "1.0.6"
   private val scalaParserCombinators        = "org.scala-lang.modules"                %% "scala-parser-combinators"      % "1.0.5"
   private val java8Compat                   = "org.scala-lang.modules"                %% "scala-java8-compat"            % "0.8.0"
-  private val ahc                           = "org.asynchttpclient"                    % "async-http-client"             % "2.0.24"
+  private val ahc                           = "org.asynchttpclient"                    % "async-http-client"             % "2.0.25"
   private val ahcNettyUtils                 = "org.asynchttpclient"                    % "async-http-client-netty-utils" % ahc.revision
-  private val netty                         = "io.netty"                               % "netty-codec-http"              % "4.0.42.Final"
+  private val netty                         = "io.netty"                               % "netty-codec-http"              % "4.0.43.Final"
   private val nettyNativeTransport          = "io.netty"                               % "netty-transport-native-epoll"  % netty.revision classifier "linux-x86_64"
   private val akkaActor                     = "com.typesafe.akka"                     %% "akka-actor"                    % "2.4.16"
   private val akkaSlf4j                     = "com.typesafe.akka"                     %% "akka-slf4j"                    % akkaActor.revision
