5f4c7a9eb0a28d347afb8f41e2cc3ebe51f0c086
Stephane Landelle <slandelle@gatling.io>
2017-01-21 23:09:27 +0100

    Upgrade jmh 1.17.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -53,7 +53,7 @@ object Dependencies {
   private val activemqCore                   = "org.apache.activemq"                    % "activemq-broker"              % "5.14.3"            % "test"
   private val h2                             = "com.h2database"                         % "h2"                           % "1.4.193"           % "test"
   private val ffmq                           = "net.timewalker.ffmq"                    % "ffmq3-core"                   % "3.0.7"             % "test" exclude("log4j", "log4j") exclude("javax.jms", "jms")
-  private val jmh                            = "org.openjdk.jmh"                        % "jmh-core"                     % "1.17.3"
+  private val jmh                            = "org.openjdk.jmh"                        % "jmh-core"                     % "1.17.4"
 
   private val loggingDeps = Seq(slf4jApi, scalalogging, logbackClassic)
   private val testDeps = Seq(scalaTest, scalaCheck, akkaTestKit, mockitoCore)
