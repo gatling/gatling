9dc5fac62fef1866ac9baefa3c65ef8e4252751c
Stephane Landelle <slandelle@gatling.io>
2017-02-23 18:02:25 +0100

    Clean up HttpCompileTest

--- gatling-http/src/test/scala/io/gatling/http/HttpCompileTest.scala
+++ gatling-http/src/test/scala/io/gatling/http/HttpCompileTest.scala
@@ -24,18 +24,6 @@ import io.gatling.http.check.HttpCheck
 
 class HttpCompileTest extends Simulation {
 
-  val iterations = 10
-  val pause1 = 1
-  val pause2 = 2
-  val pause3 = 3
-  val pause4 = Integer.getInteger("testProperty")
-
-  val pause5 = pause4 milliseconds
-  val pause6 = pause4 seconds
-  val pause7 = pause4 nanoseconds
-
-  val baseUrl = "http://localhost:3000"
-
   val httpProtocol = http
     .baseURL("http://172.30.5.143:8080")
     .proxy(Proxy("172.31.76.106", 8080).httpsPort(8081))
@@ -45,10 +33,10 @@ class HttpCompileTest extends Simulation {
     .acceptLanguageHeader("fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4")
     .acceptEncodingHeader("gzip,deflate,sdch")
     .userAgentHeader("Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.19 (KHTML, like Gecko) Ubuntu/12.04 Chromium/18.0.1025.151 Chrome/18.0.1025.151 Safari/535.19")
-    .check(bodyString.transform(string => string.size).lessThan(100000))
-    .check(bodyString.transform((string, session) => string.size).lessThan(100000))
-    .check(bodyString.transformOption(stringO => stringO.map(_.size)).lessThan(100000))
-    .check(bodyString.transformOption((stringO, session) => stringO.map(_.size)).lessThan(100000))
+    .check(bodyString.transform(string => string.length).lessThan(100000))
+    .check(bodyString.transform((string, session) => string.length).lessThan(100000))
+    .check(bodyString.transformOption(stringO => stringO.map(_.length)).lessThan(100000))
+    .check(bodyString.transformOption((stringO, session) => stringO.map(_.length)).lessThan(100000))
     .check(bodyBytes.is("foo".getBytes()))
     .check(md5.is("XXXXX"))
     .check(sha1.is("XXXXX"))
@@ -65,48 +53,78 @@ class HttpCompileTest extends Simulation {
   val httpConfToVerifyUserProvidedInfoExtractors = http
     .extraInfoExtractor(extraInfo => List(extraInfo.requestName, extraInfo.response.body.string))
 
-  val usersInformation = tsv("user_information.tsv")
-
-  val loginChain = exec(http("First Request Chain").get("/")).pause(1)
-
-  val testData = tsv("test-data.tsv")
-
-  val richTestData = testData.convert { case ("keyOfAMultivaluedColumn", value) => value.split(",") }
-
   val testData3 = Array(Map("foo" -> "bar")).circular
 
-  val lambdaUser = scenario("Standard User")
-    .exec(loginChain)
-    // First request outside iteration
-    .repeat(2) {
-      feed(richTestData)
-        .exec(http("Request").get("/").queryParam("param", "${param}").queryParam("param2", "${param2}").basicAuth("", "").check(xpath("//input[@id='text1']/@value").transform(_.map(_ + "foo")).saveAs("aaaa_value"), jsonPath("//foo/bar[2]/baz")))
-    }
-    .repeat(2, "counterName") {
-      feed(testData.circular)
-        .exec(http("Request").get("/").queryParam("param", "${param}").queryParam("param2", "${param2}").basicAuth("", "").check(xpath("//input[@id='text1']/@value").findAll.saveAs("aaaa_value"), jsonPath("//foo/bar[2]/baz")))
-    }
-    .during(10 seconds) {
-      feed(testData)
-        .exec(http("Request").get("/").queryParam("param", "${param}").queryParam("param2", "${param2}").basicAuth("", "").check(xpath("//input[@id='text1']/@value").saveAs("aaaa_value"), jsonPath("//foo/bar[2]/baz")))
-    }
-    .forever {
-      feed(testData)
-        .exec(http("Request").get("/").queryParam("param", "${param}").queryParam("param2", "${param2}").basicAuth("", "").check(xpath("//input[@id='text1']/@value").saveAs("aaaa_value"), jsonPath("//foo/bar[2]/baz")))
-    }
-    .group("Group") {
-      exec(http("Request").post("/")
-        .form("${theForm}")
-        .formParam("baz", "${qix}")
-        .multivaluedFormParam("foo", Seq("bar")))
-        .exec(http("Request").post("/").multivaluedFormParam("foo", "${bar}"))
-        .exec(http("Request").get("/").queryParam("param", "foo"))
-        .exec(http("Request").get("/").queryParam("param", "${foo}"))
-        .exec(http("Request").get("/").queryParam("param", session => "foo"))
-        .exec(http("Request").get("/").multivaluedQueryParam("param", List("foo")))
-        .exec(http("Request").get("/").multivaluedQueryParam("param", "${foo}"))
-        .exec(http("Request").get("/").multivaluedQueryParam("param", List("foo")))
-    }
+  val scn = scenario("Scn")
+    // method
+    .exec(http("Request").get("/"))
+    .exec(http("Request").put("/"))
+    .exec(http("Request").post("/"))
+    .exec(http("Request").patch("/"))
+    .exec(http("Request").head("/"))
+    .exec(http("Request").delete("/"))
+    .exec(http("Request").options("/"))
+    .exec(http("Request").httpRequest("JSON", "/support/get-plot-data?chartID=66"))
+
+    // url function
+    .exec(http("Request").get(session => "/"))
+    // queryParam
+    .exec(http("Request").get("/").queryParam("param", "one"))
+    .exec(http("Request").get("/").queryParam("param1", "one").queryParam("param2", "two"))
+    .exec(http("Request").get("/").queryParam("param", "${param}"))
+    .exec(http("Request").get("/").queryParam("param", session => "foo"))
+    .exec(http("Request").get("/").queryParam("foo", (session: Session) => io.gatling.commons.validation.Success(1)))
+    // multivaluedQueryParam
+    .exec(http("Request").get("/").multivaluedQueryParam("param", List("foo")))
+    .exec(http("Request").get("/").multivaluedQueryParam("param", "${foo}"))
+    .exec(http("Request").get("/").multivaluedQueryParam("param", List("foo")))
+    // basic auth
+    .exec(http("Request").get("/").basicAuth("usr", "pwd"))
+    // check
+    .exec(http("Request").get("/").check(xpath("//input[@id='text1']/@value")))
+    .exec(http("Request").get("/").check(xpath("//input[@id='text1']/@value"), jsonPath("//foo/bar[2]/baz")))
+    .exec(http("Request").get("/").check(xpath("//input[@id='text1']/@value").find))
+    .exec(http("Request").get("/").check(xpath("//input[@id='text1']/@value").find.exists))
+    .exec(http("Request").get("/").check(xpath("//input[@id='text1']/@value").find.is("expected")))
+    .exec(http("Request").get("/").check(xpath("//input[@id='text1']/@value").find.exists.saveAs("key")))
+    .exec(http("Request").get("/").check(xpath("//input[@id='text1']/@value").saveAs("key")))
+    .exec(http("Request").get("/").check(xpath("//input[@id='text1']/@value").findAll))
+    .exec(http("Request").get("/").check(xpath("//input[@id='text1']/@value").count))
+    .exec(
+      http("Request").get("h/")
+        .check(
+          xpath("//input[@value='${aaaa_value}']/@id").saveAs("sessionParam"),
+          xpath("//input[@id='${aaaa_value}']/@value").notExists,
+          css(".foo"),
+          css("#foo", "href"),
+          css(".foo").ofType[Node].count.is(1),
+          css(".foo").notExists,
+          css("#foo").ofType[Node].transform { node: Node => node.getNodeName },
+          css(".foo").findRandom.is("some text"),
+          css(".foo").findRandom(5).is(Seq("some text")),
+          regex("""<input id="text1" type="text" value="aaaa" />""").optional.saveAs("var1"),
+          regex("""<input id="text1" type="text" value="aaaa" />""").count.is(1),
+          regex("""<input id="text1" type="test" value="aaaa" />""").notExists,
+          status.in(200 to 210).saveAs("blablaParam"),
+          status.in(200, 210).saveAs("blablaParam"),
+          status.in(Seq(200, 304)).saveAs("blablaParam"),
+          bodyBytes.is(RawFileBody("foobar.txt")),
+          bodyString.is(ElFileBody("foobar.txt")),
+          xpath("//input[@value='aaaa']/@id").not("param"),
+          xpath("//input[@id='text1']/@value").is("aaaa").saveAs("test2"),
+          md5.is("0xA59E79AB53EEF2883D72B8F8398C9AC3"),
+          substring("Foo"),
+          responseTimeInMillis.lessThan(1000)
+        )
+    )
+    .exec(http("Request").get("/tests").check(header(HttpHeaderNames.ContentType).is("text/html; charset=utf-8")))
+    // form
+    .exec(http("Request").post("/")
+      .form("${theForm}")
+      .formParam("baz", "${qix}")
+      .multivaluedFormParam("foo", Seq("bar")))
+    .exec(http("Request").post("/").multivaluedFormParam("foo", "${bar}"))
+    // resources
     .exec(http("Request").get("/")
       .resources(
         http("Request").post("/").multivaluedFormParam("foo", "${bar}"),
@@ -114,205 +132,42 @@ class HttpCompileTest extends Simulation {
         http("Request").get("/").queryParam("param", "${foo}"),
         http("Request").get("/").queryParam("param", session => "foo")
       ))
-    .uniformRandomSwitch(exec(http("Request").get("/")), exec(http("Request2").get("/")))
-    .randomSwitch(
-      40d -> exec(http("Request").get("/")),
-      50d -> exec(http("Request2").get("/"))
-    )
-    .randomSwitch(40d -> exec(http("Request").get("/")))
-    .pause(pause2)
-    // Loop
-    .repeat(iterations, "counter") {
-      // What will be repeated ?
-      // First request to be repeated
-      exec { session =>
-        println("iterate: " + session("counter"))
-        session
-      }
-        .exec(http("").httpRequest("JSON", "/support/get-plot-data?chartID=66"))
-        .exec(
-          http("Home Page").get("http://localhost:3000")
-            .check(
-              xpath("//input[@value='${aaaa_value}']/@id").saveAs("sessionParam"),
-              xpath("//input[@id='${aaaa_value}']/@value").notExists,
-              css(".foo"),
-              css("#foo", "href"),
-              css(".foo").ofType[Node].count.is(1),
-              css(".foo").notExists,
-              css("#foo").ofType[Node].transform { node: Node => node.getNodeName },
-              css(".foo").findRandom.is("some text"),
-              css(".foo").findRandom(5).is(Seq("some text")),
-              regex("""<input id="text1" type="text" value="aaaa" />""").optional.saveAs("var1"),
-              regex("""<input id="text1" type="text" value="aaaa" />""").count.is(1),
-              regex("""<input id="text1" type="test" value="aaaa" />""").notExists,
-              status.in(200 to 210).saveAs("blablaParam"),
-              status.in(200, 210).saveAs("blablaParam"),
-              status.in(Seq(200, 304)).saveAs("blablaParam"),
-              bodyBytes.is(RawFileBody("foobar.txt")),
-              bodyString.is(ElFileBody("foobar.txt")),
-              xpath("//input[@value='aaaa']/@id").not("param"),
-              xpath("//input[@id='text1']/@value").is("aaaa").saveAs("test2"),
-              md5.is("0xA59E79AB53EEF2883D72B8F8398C9AC3"),
-              substring("Foo"),
-              responseTimeInMillis.lessThan(1000)
-            )
-        )
-        .during(12000 milliseconds, "foo") {
-          exec(http("In During 1").get("http://localhost:3000/aaaa"))
-            .pause(2, constantPauses)
-            .repeat(2, "tutu") {
-              exec { session =>
-                println("--nested loop: " + session("tutu"))
-                session
-              }
-            }
-            .exec { session =>
-              println("-loopDuring: " + session("foo"))
-              session
-            }
-            .exec(http("In During 2").get("/"))
-            .pause(2)
-        }
-        .pause(pause2)
-        .during(12000 milliseconds, "duringCounter") {
-          exec(http("In During 1").get("/"))
-            .pause(2)
-            .exec { session =>
-              println("-iterate1: " + session("counter") + ", doFor: " + session("duringCounter"))
-              session
-            }
-            .repeat(2, "count") {
-              exec { session =>
-                println("--iterate1: " + session("counter") + ", doFor: " + session("duringCounter") + ", iterate2: " + session("count"))
-                session
-              }
-            }
-            .exec(http("In During 2").get("/"))
-            .pause(2)
-        }
-        .exec { session => session.set("test2", "bbbb") }
-        .doIfEqualsOrElse("test2", "aaaa") {
-          exec(http("IF=TRUE Request").get("/"))
-        } {
-          exec(http("IF=FALSE Request").get("/"))
-        }.pause(pause2)
-        .exec(http("Url from session").get("/aaaa"))
-        .pause(1000 milliseconds)
-        // Second request to be repeated
-        .exec(http("Create2")
-          .post("/things")
-          .queryParam("login", "${login}")
-          .queryParam("password", "${password}")
-          .queryParam("foo", 1) // make sure raw non String objects are converted
-          .queryParam("foo", _ => 1) // make raw non String objects returned by functions are converted
-          .queryParam("foo", (session: Session) => io.gatling.commons.validation.Success(1)) // make sure functions returning expressions of other types are properly converted
-          .body(ElFileBody("create_thing.txt")).asJSON)
-        .pause(pause1)
-        // Third request to be repeated
-        .exec(http("Liste Articles").get("/things").queryParam("firstname", "${firstname}").queryParam("lastname", "${lastname}"))
-        .pause(pause1)
-        .exec(http("Test Page").get("/tests").check(header(HttpHeaderNames.ContentType).is("text/html; charset=utf-8").saveAs("sessionParam")))
-        // Fourth request to be repeated
-        .pause(100 milliseconds)
-        // switch
-        .randomSwitch(
-          40d -> exec(http("Possibility 1").get("/p1")),
-          55d -> exec(http("Possibility 2").get("/p2")) // last 5% bypass
-        )
-        .exec(http("Create")
-          .post("/things").queryParam("postTest", "${sessionParam}").body(RawFileBody("create_thing.txt")).asJSON
-          .check(status.is(201).saveAs("status")))
-        .exec(http("bodyParts")
-          .post("url")
-          .formUpload("name", "path")
-          .bodyPart(RawFileBodyPart("name", "path"))
-          .bodyPart(ElFileBodyPart("name", "path")))
-    }
-    // Head request
-    .exec(http("head on root").head("/").proxy(Proxy("172.31.76.106", 8080).httpsPort(8081)))
-    // Second request outside iteration
-    .exec(http("Add to Cart").get("/").check(regex("""<input id="text1" type="text" value="(.*)" />""").saveAs("input")))
-    .exec(http("Add to Cart").get("/").check(regex(session => """<input id="text1" type="text" value="smth" />""").count.lessThan(10).saveAs("input")))
-    .exec(http("Add to Cart").get("/").check(regex(session => """<input id="(.*)" type="text" value="(.*)" />""").ofType[(String, String)] saveAs ("input")))
-    .pause(pause1)
+    // body
+    .exec(http("Request").post("/things").body(StringBody("FOO${BAR}BAZ")).asJSON)
+    .exec(http("Request").post("/things").body(ElFileBody("create_thing.txt")))
+    .exec(http("Request").post("/things").body(RawFileBody("create_thing.txt")))
+    .exec(http("Request").post("/things").body(PebbleStringBody("create_thing.txt")))
+    .exec(http("Request").post("/things").body(PebbleFileBody("create_thing.txt")))
+    // bodyParts
+    .exec(http("Request").post("url")
+      .formUpload("name", "path")
+      .bodyPart(RawFileBodyPart("name", "path"))
+      .bodyPart(ElFileBodyPart("name", "path")))
+    // proxy
+    .exec(http("Request").head("/").proxy(Proxy("172.31.76.106", 8080).httpsPort(8081)))
+    // polling
     .exec(polling.every(10.seconds).exec(http("poll").get("/foo")))
     .exec(polling.pollerName("poll").every(10.seconds).exec(http("poll").get("/foo")))
     .exec(polling.pollerName("poll").stop)
     .exec(polling.stop)
+    // rendezVous
     .rendezVous(100)
+    // flushSessionCookies
     .exec(flushSessionCookies)
-    .pause(pause4)
+    // addCookie
     .exec(addCookie(Cookie("foo", "bar").withDomain("/")))
-    .pace(5)
-    .pace(5 seconds)
-    .pace("${foo}")
-    .pace(5, 10)
-    .pace("${foo}", "${bar}")
-    .doSwitch("${foo}")(
-      "a" -> exec(http("a").get("/")),
-      "b" -> exec(http("b").get("/"))
-    )
-    .doSwitchOrElse("${foo}")(
-      "a" -> exec(http("a").get("/")),
-      "b" -> exec(http("b").get("/")) //
-    )(exec(http("else").get("/")))
-    .exec(http("transformResponse")
-      .get("/")
-      .transformResponse {
-        case response if response.isReceived =>
-          import io.gatling.http.response._
-          new ResponseWrapper(response) {
-            override val body = new StringResponseBody(response.body.string.replace(")]}',", ""), response.charset)
-          }
-      })
-    .exec { session => session.set("tryMax", 3) }
-    .tryMax("${tryMax}") {
-      exec(http("tryMaxWithExpression")
-        .get("/"))
-    }
+    // transformResponse
+    .exec(http("Request").get("/").transformResponse {
+      case response if response.isReceived =>
 
-  val inject1 = nothingFor(10 milliseconds)
-  val inject2 = rampUsers(10).over(10 minutes)
-  val inject3 = constantUsersPerSec(10).during(1 minute)
-  val inject4 = atOnceUsers(100)
-  val inject5 = rampUsersPerSec(10) to 20 during (10 minutes)
-  val inject6 = splitUsers(1000).into(rampUsers(10) over (10 seconds)).separatedBy(10 seconds)
-  val inject7 = splitUsers(1000).into(rampUsers(10) over (10 seconds)).separatedBy(atOnceUsers(30))
-  val inject8 = heavisideUsers(1000) over (20 seconds)
+        import io.gatling.http.response._
 
-  val injectionSeq = Vector(1, 2, 4, 8).map(x => rampUsers(x * 100) over (5 seconds))
-  setUp(
-    lambdaUser.inject(inject1),
-    lambdaUser.inject(injectionSeq: _*),
-    lambdaUser.inject(inject1, inject2).throttle(jumpToRps(20), reachRps(40) in (10 seconds), holdFor(30 seconds))
-  )
-    .protocols(httpProtocol)
-    .pauses(uniformPausesPlusOrMinusPercentage(1))
-    .disablePauses
-    .constantPauses
-    .exponentialPauses
-    .uniformPauses(1.5)
-    .uniformPauses(1337 seconds)
-    .assertions(
-      global.responseTime.mean.lte(50),
-      global.responseTime.max.between(50, 500),
-      global.successfulRequests.count.gte(1500),
-      global.responseTime.percentile1.lt(100),
-      global.responseTime.percentile(99.999).lt(100),
-      global.allRequests.percent.is(100),
-      forAll.failedRequests.percent.is(0),
-      forAll.responseTime.max.is(100),
-      details("Users" / "Search" / "Index page").responseTime.mean.gt(0),
-      details("Admins" / "Create").failedRequests.percent.lt(90),
-      details("request_9").requestsPerSec.gte(10)
-    )
-    .throttle(jumpToRps(20), reachRps(40) in (10 seconds), holdFor(30 seconds))
-    // Applies on the setup
-    .constantPauses
-    .disablePauses
-    .exponentialPauses
-    .uniformPauses(1.5)
-    .uniformPauses(1337 seconds)
+        new ResponseWrapper(response) {
+          override val body = new StringResponseBody(response.body.string.replace(")]}',", ""), response.charset)
+        }
+    })
+
+  setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 
   // Conditional check compile test
   val requestWithUntypedCheckIf =
@@ -323,7 +178,7 @@ class HttpCompileTest extends Simulation {
         }
       )
 
-  def isJsonResponse(response: Response): Boolean = response.header(HttpHeaderNames.ContentType).exists { x => x.contains(HttpHeaderValues.ApplicationJson) }
+  def isJsonResponse(response: Response): Boolean = response.header(HttpHeaderNames.ContentType).exists(_.contains(HttpHeaderValues.ApplicationJson))
 
   val requestWithTypedCheckIf =
     http("typedCheckIf").get("/")
