eb3e2c516401de2374e38a4169bc1028fea40faa
Reik Schatz <rschatz@ea.com>
2017-03-21 17:13:22 +0100

    added BatchedFeederBuilder

--- gatling-core/src/main/scala/io/gatling/core/feeder/FeederBuilder.scala
+++ gatling-core/src/main/scala/io/gatling/core/feeder/FeederBuilder.scala
@@ -15,6 +15,8 @@
  */
 package io.gatling.core.feeder
 
+import java.util.concurrent.locks.ReentrantLock
+
 import io.gatling.core.structure.ScenarioContext
 
 trait FeederBuilder[T] {
@@ -23,6 +25,44 @@ trait FeederBuilder[T] {
 
 case class FeederWrapper[T](feeder: Feeder[T]) extends FeederBuilder[T] {
   def build(ctx: ScenarioContext) = feeder
+}
+
+case class BatchedFeederBuilder[T](
+    records:   Iterator[Record[T]],
+    batchSize: Long,
+    strategy:  FeederStrategy      = Queue
+) extends FeederBuilder[T] {
+
+  override def build(ctx: ScenarioContext): Feeder[T] = {
+      def fetchNextBatch(): Feeder[T] = {
+        val batchRecords = (1L to batchSize).toIterator.zip(records).map {
+          case (_, record) => record
+        }.toIndexedSeq
+        strategy.feeder(batchRecords, ctx)
+      }
+
+    var currentBatch: Feeder[T] = fetchNextBatch()
+
+    var invocations = 1
+    val lock = new ReentrantLock()
+
+    new Feeder[T] {
+      def hasNext: Boolean = {
+        lock.lock()
+        try {
+          if (invocations % batchSize == 0) {
+            currentBatch = fetchNextBatch()
+          }
+          invocations += 1
+        } finally {
+          lock.unlock()
+        }
+
+        currentBatch.hasNext
+      }
+      def next: Record[T] = currentBatch.next()
+    }
+  }
 }
 
 case class RecordSeqFeederBuilder[T](
