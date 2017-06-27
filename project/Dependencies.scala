288e3ebae5f86f73e83c244a26a34f2e399014b0
Stephane Landelle <slandelle@gatling.io>
2017-06-27 23:11:04 +0200

    Upgrade pebble 2.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,7 +39,7 @@ object Dependencies {
   private val bouncycastle                  = "org.bouncycastle"                       % "bcpkix-jdk15on"                % "1.57"
   private val quicklens                     = "com.softwaremill.quicklens"            %% "quicklens"                     % "1.4.8"
   private val testInterface                 = "org.scala-sbt"                          % "test-interface"                % "1.0"
-  private val pebble                        = "com.mitchellbosecke"                    % "pebble"                        % "2.3.0"
+  private val pebble                        = "com.mitchellbosecke"                    % "pebble"                        % "2.4.0"
   private val guava                         = "com.google.guava"                       % "guava"                         % "22.0"
   private val findbugs                      = "com.google.code.findbugs"               % "jsr305"                        % "3.0.1"
 
