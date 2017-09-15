bedec9d2cb5dbd5a3424abeff839335f954b1627
Stephane Landelle <slandelle@gatling.io>
2017-09-15 09:55:24 +0200

    Upgrade hdrhistogram 2.1.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -46,7 +46,7 @@ object Dependencies {
   private val jmsApi                        = "org.apache.geronimo.specs"              % "geronimo-jms_1.1_spec"         % "1.1.1"
   private val logback                       = "ch.qos.logback"                         % "logback-classic"               % "1.2.3"
   private val tdigest                       = "com.tdunning"                           % "t-digest"                      % "3.1"
-  private val hdrHistogram                  = "org.hdrhistogram"                       % "HdrHistogram"                  % "2.1.9"
+  private val hdrHistogram                  = "org.hdrhistogram"                       % "HdrHistogram"                  % "2.1.10"
   private val caffeine                      = "com.github.ben-manes.caffeine"          % "caffeine"                      % "2.5.5"
   private val bouncycastle                  = "org.bouncycastle"                       % "bcpkix-jdk15on"                % "1.58"
   private val quicklens                     = "com.softwaremill.quicklens"            %% "quicklens"                     % "1.4.8"
