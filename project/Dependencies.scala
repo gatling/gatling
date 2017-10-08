dd15271765c5e98ca53c6ad6fd69db764b757820
Stephane Landelle <slandelle@gatling.io>
2017-10-08 21:05:54 +0200

    Upgrade saxon 9.8.0-5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   private val akkaActor                     = "com.typesafe.akka"                     %% "akka-actor"                    % "2.5.6"
   private val akkaSlf4j                     = "com.typesafe.akka"                     %% "akka-slf4j"                    % akkaActor.revision
   private val config                        = "com.typesafe"                           % "config"                        % "1.3.2"
-  private val saxon                         = "net.sf.saxon"                           % "Saxon-HE"                      % "9.8.0-4"
+  private val saxon                         = "net.sf.saxon"                           % "Saxon-HE"                      % "9.8.0-5"
   private val slf4jApi                      = "org.slf4j"                              % "slf4j-api"                     % "1.7.25"
   private val fastring                      = "com.dongxiguo"                         %% "fastring"                      % "0.3.1"
   private val spire                         = "io.gatling"                            %% "spire-cfor"                    % "0.14.1"
