9d8b4fce44020b69cf9e3866a028a3f792722f8f
Stephane Landelle <slandelle@gatling.io>
2018-01-24 11:15:14 +0100

    Don't log unexpected incoming messages, close #3404
    Motivation:
    
    Gatling OSS reports crash on unmatched incoming messages.
    
    Modification:
    
    Ignore those
    
    Result:
    
    No more ArrayIndexOutOfBoundsException when generating report

--- gatling-core/src/main/scala/io/gatling/core/stats/StatsEngine.scala
+++ gatling-core/src/main/scala/io/gatling/core/stats/StatsEngine.scala
@@ -142,17 +142,19 @@ class DataWritersStatsEngine(system: ActorSystem, dataWriters: Seq[ActorRef]) ex
     message:      Option[String],
     extraInfo:    List[Any]       = Nil
   ): Unit =
-    dispatch(ResponseMessage(
-      session.scenario,
-      session.userId,
-      session.groupHierarchy,
-      requestName,
-      timings,
-      status,
-      responseCode,
-      message,
-      extraInfo
-    ))
+    if (timings.endTimestamp >= 0) {
+      dispatch(ResponseMessage(
+        session.scenario,
+        session.userId,
+        session.groupHierarchy,
+        requestName,
+        timings,
+        status,
+        responseCode,
+        message,
+        extraInfo
+      ))
+    }
 
   override def logGroupEnd(
     session:       Session,
