b007895b3d9cfe8d3201c41fc746ca194c196e72
Stephane Landelle <slandelle@gatling.io>
2018-02-22 09:42:44 +0100

    Upgrade mockito 2.15.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -66,7 +66,7 @@ object Dependencies {
   private val scalaTest                      = "org.scalatest"                         %% "scalatest"                    % "3.0.5"             % "test"
   private val scalaCheck                     = "org.scalacheck"                        %% "scalacheck"                   % "1.13.5"            % "test"
   private val akkaTestKit                    = "com.typesafe.akka"                     %% "akka-testkit"                 % akkaActor.revision  % "test"
-  private val mockitoCore                    = "org.mockito"                            % "mockito-core"                 % "2.13.0"            % "test"
+  private val mockitoCore                    = "org.mockito"                            % "mockito-core"                 % "2.15.0"            % "test"
   private val activemqBroker                 = "org.apache.activemq"                    % "activemq-broker"              % "5.15.3"            % "test"
   private val h2                             = "com.h2database"                         % "h2"                           % "1.4.196"           % "test"
   private val jmh                            = "org.openjdk.jmh"                        % "jmh-core"                     % "1.20"
