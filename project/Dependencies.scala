d1a9f152f436ee589678805df89513440da6f90d
Stephane Landelle <slandelle@gatling.io>
2018-04-24 15:12:15 +0200

    Upgrade scalacheck 1.14.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -70,7 +70,7 @@ object Dependencies {
   // Test dependencies
 
   private val scalaTest                      = "org.scalatest"                       %% "scalatest"                       % "3.0.5"             % "test"
-  private val scalaCheck                     = "org.scalacheck"                      %% "scalacheck"                      % "1.13.5"            % "test"
+  private val scalaCheck                     = "org.scalacheck"                      %% "scalacheck"                      % "1.14.0"            % "test"
   private val akkaTestKit                    = akka.organization                     %% "akka-testkit"                    % akka.revision  % "test"
   private val mockitoCore                    = "org.mockito"                          % "mockito-core"                    % "2.18.0"            % "test"
   private val activemqBroker                 = "org.apache.activemq"                  % "activemq-broker"                 % "5.15.3"            % "test"
