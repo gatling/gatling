1cfa997f0bee0c4bcb10e755f401814d67b36236
Stephane Landelle <slandelle@gatling.io>
2018-06-15 17:17:13 +0200

    Add missing httpRequest overload that takes a String method

--- gatling-http/src/main/scala/io/gatling/http/request/builder/Http.scala
+++ gatling-http/src/main/scala/io/gatling/http/request/builder/Http.scala
@@ -34,6 +34,7 @@ case class Http(requestName: Expression[String]) {
   def head(url: Expression[String]): HttpRequestBuilder = httpRequest(HttpMethod.HEAD, url)
   def delete(url: Expression[String]): HttpRequestBuilder = httpRequest(HttpMethod.DELETE, url)
   def options(url: Expression[String]): HttpRequestBuilder = httpRequest(HttpMethod.OPTIONS, url)
+  def httpRequest(method: String, url: Expression[String]): HttpRequestBuilder = httpRequest(new HttpMethod(method), Left(url))
   def httpRequest(method: HttpMethod, url: Expression[String]): HttpRequestBuilder = httpRequest(method, Left(url))
   def httpRequest(method: HttpMethod, urlOrURI: Either[Expression[String], Uri]): HttpRequestBuilder = new HttpRequestBuilder(CommonAttributes(requestName, method, urlOrURI), HttpAttributes())
 }
