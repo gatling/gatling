0df222fdfbe7740639954b7d73fb2111577960af
Stephane Landelle <slandelle@gatling.io>
2018-06-26 21:57:29 +0200

    Add remote peer address and timeout value to request TimeoutException
    Motivation:
    
    It's very helpful for debunking timeout issues to get the peer actual address and the timeout value. This information was available in AHC but was lost in the new client.
    
    Modifications:
    
    Restore peer address and timeout value in request Timeout exception message.
    
    Result:
    
    More helpful request TimeoutException message.

--- gatling-http-client/src/main/java/io/gatling/http/client/impl/RequestTimeout.java
+++ gatling-http-client/src/main/java/io/gatling/http/client/impl/RequestTimeout.java
@@ -17,23 +17,42 @@
 package io.gatling.http.client.impl;
 
 import io.gatling.http.client.HttpListener;
+import io.gatling.netty.util.ahc.StringBuilderPool;
 import io.netty.channel.Channel;
 import io.netty.channel.EventLoop;
 
+import java.net.InetSocketAddress;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 public interface RequestTimeout {
 
-  TimeoutException REQUEST_TIMEOUT = new TimeoutException("Request timeout") {
+  class RequestTimeoutException extends TimeoutException {
+
+    private static String message(long timeout, InetSocketAddress remoteAddress) {
+      StringBuilder message = StringBuilderPool.DEFAULT.get().append("Request timeout");
+      if (remoteAddress != null) {
+        message.append(" to ").append(remoteAddress.getHostName());
+        if (!remoteAddress.isUnresolved()) {
+          message.append('/').append(remoteAddress.getAddress().getHostAddress());
+        }
+        message.append(':').append(remoteAddress.getPort());
+      }
+      return message.append(" after ").append(timeout).append(" ms").toString();
+    }
+
+    RequestTimeoutException(long timeout, InetSocketAddress remoteAddress) {
+      super(message(timeout, remoteAddress));
+    }
+
     @Override
-    public synchronized Throwable fillInStackTrace() {
+    public Throwable fillInStackTrace() {
       return this;
     }
-  };
+  }
 
-  static RequestTimeout newRequestTimeout(long timeout, HttpListener listener, EventLoop eventLoop) {
+  static RequestTimeout scheduleRequestTimeout(long timeout, HttpListener listener, EventLoop eventLoop) {
     return timeout > 0 ? new DefaultRequestTimeout(timeout, listener, eventLoop) : NoopRequestTimeout.INSTANCE;
   }
 
@@ -44,17 +63,20 @@ public interface RequestTimeout {
   void setChannel(Channel channel);
 
   class DefaultRequestTimeout implements RequestTimeout {
+    private final long timeout;
     private final ScheduledFuture<?> f;
     private final HttpListener listener;
     private Channel channel;
+    private InetSocketAddress remoteAddress;
 
     private DefaultRequestTimeout(long timeout, HttpListener listener, EventLoop eventLoop) {
+      this.timeout = timeout;
       this.listener = listener;
       f = eventLoop.schedule(this::execute, timeout, TimeUnit.MILLISECONDS);
     }
 
     private void execute() {
-      listener.onThrowable(REQUEST_TIMEOUT);
+      listener.onThrowable(new RequestTimeoutException(timeout, remoteAddress));
       if (channel != null) {
         channel.close();
       }
@@ -70,6 +92,7 @@ public interface RequestTimeout {
 
     public void setChannel(Channel channel) {
       this.channel = channel;
+      remoteAddress = (InetSocketAddress) channel.remoteAddress();
     }
   }
 
