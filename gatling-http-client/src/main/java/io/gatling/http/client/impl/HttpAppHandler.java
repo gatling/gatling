gatling/gatling

commit dc3905a03f5601c31883650860677d05ff555793

Harden HTTP/1 exception handling

gatling-http-client/src/main/java/io/gatling/http/client/impl/HttpAppHandler.java
@@ -70,16 +70,21 @@ class HttpAppHandler extends ChannelDuplexHandler {
   }
 
   private void crash(ChannelHandlerContext ctx, Throwable cause, boolean close) {
-    if (isInactive()) {
-      return;
-    }
+    try {
+      if (isInactive()) {
+        return;
+      }
 
-    tx.listener.onThrowable(cause);
-    tx.requestTimeout.cancel();
-    setInactive();
+      tx.requestTimeout.cancel();
+      tx.listener.onThrowable(cause);
+      setInactive();
 
-    if (close) {
-      ctx.close();
+    } catch (Exception e) {
+      LOGGER.error("Exception while handling HTTP/1.1 crash, please report to Gatling maintainers", e);
+    } finally {
+      if (close) {
+        ctx.close();
+      }
     }
   }
 
@@ -140,17 +145,20 @@ class HttpAppHandler extends ChannelDuplexHandler {
         }
         boolean last = chunk instanceof LastHttpContent;
 
-        HttpListener listener = tx.listener; // might be null out by setInactive
         if (last) {
-          tx.requestTimeout.cancel();
           if (tx.closeConnection) {
             ctx.channel().close();
           } else {
             channelPool.offer(ctx.channel());
           }
+        }
+
+        tx.listener.onHttpResponseBodyChunk(chunk.content(), last);
+
+        if (last) {
+          tx.requestTimeout.cancel();
           setInactive();
         }
-        listener.onHttpResponseBodyChunk(chunk.content(), last);
       }
     } finally {
       ReferenceCountUtil.release(msg);
@@ -166,7 +174,6 @@ class HttpAppHandler extends ChannelDuplexHandler {
     if (!httpResponseReceived && client.retry(tx, ctx.channel().eventLoop())) {
       // only retry when we haven't started receiving response
       setInactive();
-
     } else {
       crash(ctx, PREMATURE_CLOSE, false);
     }

commit 947765ff5469d9b04e7eef5651759222794a4d2d

Fix HTTP/1 follow redirect, close #3537
Motivation:

Regression introduced in dc3905a: request following a redirect on HTTP/1 times out.

Modifications:

setInactive must be called before onHttpResponseBodyChunk because it might be performing a redirect, so calling it after would lose the current HttpTx and reads will be ignored.

Result:

No more timeout on redirect.

gatling-http-client/src/main/java/io/gatling/http/client/impl/HttpAppHandler.java
@@ -70,11 +70,14 @@ class HttpAppHandler extends ChannelDuplexHandler {
   }
 
   private void crash(ChannelHandlerContext ctx, Throwable cause, boolean close) {
-    try {
-      if (isInactive()) {
-        return;
-      }
+    if (isInactive()) {
+      return;
+    }
+    crash0(ctx, cause, close, tx);
+  }
 
+  private void crash0(ChannelHandlerContext ctx, Throwable cause, boolean close, HttpTx tx) {
+    try {
       tx.requestTimeout.cancel();
       tx.listener.onThrowable(cause);
       setInactive();
@@ -145,7 +148,12 @@ class HttpAppHandler extends ChannelDuplexHandler {
         }
         boolean last = chunk instanceof LastHttpContent;
 
+        // making a local copy because setInactive might be called (on last)
+        HttpTx tx = this.tx;
+
         if (last) {
+          tx.requestTimeout.cancel();
+          setInactive();
           if (tx.closeConnection) {
             ctx.channel().close();
           } else {
@@ -153,11 +161,12 @@ class HttpAppHandler extends ChannelDuplexHandler {
           }
         }
 
-        tx.listener.onHttpResponseBodyChunk(chunk.content(), last);
-
-        if (last) {
-          tx.requestTimeout.cancel();
-          setInactive();
+        try {
+          tx.listener.onHttpResponseBodyChunk(chunk.content(), last);
+        } catch (Exception e) {
+          // can't let exceptionCaught handle this because setInactive might have been called (on last)
+          crash0(ctx, e, true, tx);
+          throw e;
         }
       }
     } finally {