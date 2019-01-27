a044534eeedcac63b327ef8b59006eb39e830be5
Stephane Landelle <slandelle@gatling.io>
2019-01-27 16:41:25 +0100

    Upgrade json4s 3.6.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
   private val sfm                            = ("org.simpleflatmapper"                % "lightning-csv"                   % "6.2.1")
     .exclude("org.simpleflatmapper", "ow2-asm")
   private val sfmUtil                        = sfm.organization                       % "sfm-util"                        % sfm.revision
-  private val json4sJackson                  = "org.json4s"                          %% "json4s-jackson"                  % "3.6.3"
+  private val json4sJackson                  = "org.json4s"                          %% "json4s-jackson"                  % "3.6.4"
   private val jsonpath                       = "io.gatling"                          %% "jsonpath"                        % "0.6.14"
   private val joddJson                       = "org.jodd"                             % "jodd-json"                       % "5.0.6"
   private val joddLagarto                    = joddJson.organization                  % "jodd-lagarto"                    % joddJson.revision
