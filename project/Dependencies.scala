2b22a4cc76359633265422fb8a63613e9c9015a0
Stephane Landelle <slandelle@gatling.io>
2019-02-14 22:27:51 +0100

    Upgrade sfm 6.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies {
   private val scopt                          = "com.github.scopt"                    %% "scopt"                           % "3.7.1"
   private val scalaLogging                   = "com.typesafe.scala-logging"          %% "scala-logging"                   % "3.9.2"
   private val jackson                        = "com.fasterxml.jackson.core"           % "jackson-databind"                % "2.9.8"
-  private val sfm                            = ("org.simpleflatmapper"                % "lightning-csv"                   % "6.3.0")
+  private val sfm                            = ("org.simpleflatmapper"                % "lightning-csv"                   % "6.4.0")
     .exclude("org.simpleflatmapper", "ow2-asm")
   private val sfmUtil                        = sfm.organization                       % "sfm-util"                        % sfm.revision
   private val json4sJackson                  = "org.json4s"                          %% "json4s-jackson"                  % "3.6.4"
