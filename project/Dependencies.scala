3fa1b2764762e5eafa25608a443b615a3737bb98
Stephane Landelle <slandelle@gatling.io>
2019-02-28 15:03:36 +0100

    Upgrade slf4j 1.7.26

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
   private val akkaSlf4j                      = akka.organization                     %% "akka-slf4j"                      % akka.revision
   private val config                         = "com.typesafe"                         % "config"                          % "1.3.3"
   private val saxon                          = "net.sf.saxon"                         % "Saxon-HE"                        % "9.9.1-1"
-  private val slf4jApi                       = "org.slf4j"                            % "slf4j-api"                       % "1.7.25"
+  private val slf4jApi                       = "org.slf4j"                            % "slf4j-api"                       % "1.7.26"
   private val fastring                       = "com.dongxiguo"                       %% "fastring"                        % "1.0.0"
   private val spire                          = ("org.typelevel"                      %% "spire-macros"                    % "0.16.0")
     .exclude("org.typelevel", "machinist_2.12")
