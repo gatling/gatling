gatling/gatling

commit 05778900db54d7b85d2f5099219fb878310aceb9

Donc create non base url for baseUrl sub paths

gatling-recorder/src/main/scala/io/gatling/recorder/scenario/template/SimulationTemplate.scala
@@ -16,11 +16,12 @@
 
 package io.gatling.recorder.scenario.template
 
-import com.dongxiguo.fastring.Fastring.Implicits._
-
+import io.gatling.http.client.ahc.uri.Uri
 import io.gatling.recorder.scenario.{ ProtocolDefinition, ScenarioElement, TagElement }
 import io.gatling.recorder.scenario.{ PauseElement, RequestElement }
 import io.gatling.recorder.config.RecorderConfiguration
+
+import com.dongxiguo.fastring.Fastring.Implicits._
 
 private[scenario] object SimulationTemplate {
 
@@ -80,7 +81,9 @@ $mapContent)"""
               val chainContent = chain.map { element =>
                 val prefix = element match {
                   case TagElement(_) => ""
-                  case _             => if (firstNonTagElement) { firstNonTagElement = false; "" } else "."
+                  case _ => if (firstNonTagElement) {
+                    firstNonTagElement = false; ""
+                  } else "."
                 }
                 fast"$prefix${renderScenarioElement(element, extractedUris)}"
               }.mkFastring("\n\t\t")
@@ -90,7 +93,7 @@ $mapContent)"""
           val chainsList = chains.indices.map(i => fast"chain_$i").mkFastring(", ")
 
           fast"""$chainElements
-					
+
 	val scn = scenario("$scenarioName").exec(
 		$chainsList)"""
       }
@@ -104,7 +107,7 @@ $mapContent)"""
       }
 
     val extractedUris = new ExtractedUris(flatScenarioElements(scenarioElements))
-    val nonBaseUrls = extractedUris.vals.filter(_.value != protocol.baseUrl)
+    val nonBaseUrls = extractedUris.vals.filter(value => Uri.create(value.value).getBaseUrl != protocol.baseUrl)
 
     fast"""$renderPackage
 import scala.concurrent.duration._

gatling-http-client/src/main/java/io/gatling/http/client/ahc/oauth/OAuthSignatureCalculatorInstance.java
@@ -143,7 +143,7 @@ public class OAuthSignatureCalculatorInstance {
                                     String percentEncodedNonce) {
 
     // beware: must generate first as we're using pooled StringBuilder
-    String baseUrl = uri.toBaseUrl();
+    String baseUrl = uri.toUrlWithoutQuery();
     String encodedParams = encodedParams(consumerAuth, userAuth, oauthTimestamp, percentEncodedNonce, uri.getEncodedQueryParams(), formParams);
 
     StringBuilder sb = StringBuilderPool.DEFAULT.get();

gatling-http-client/src/main/java/io/gatling/http/client/ahc/uri/Uri.java
@@ -179,16 +179,28 @@ public class Uri {
   /**
    * @return [scheme]://[hostname](:[port])/path. Port is omitted if it matches the scheme's default one.
    */
-  public String toBaseUrl() {
+  public String toUrlWithoutQuery() {
+    StringBuilder sb = toBaseUrl0();
+    if (isNonEmpty(path)) {
+      sb.append(path);
+    }
+    return sb.toString();
+  }
+
+  /**
+   * @return [scheme]://[hostname](:[port]). Port is omitted if it matches the scheme's default one.
+   */
+  public String getBaseUrl() {
+    return toBaseUrl0().toString();
+  }
+
+  private StringBuilder toBaseUrl0() {
     StringBuilder sb = StringBuilderPool.DEFAULT.get();
     sb.append(scheme).append("://").append(host);
     if (port != -1 && port != getSchemeDefaultPort()) {
       sb.append(':').append(port);
     }
-    if (isNonEmpty(path)) {
-      sb.append(path);
-    }
-    return sb.toString();
+    return sb;
   }
 
   public String toRelativeUrl() {
@@ -201,10 +213,6 @@ public class Uri {
       sb.append('?').append(query);
 
     return sb.toString();
-  }
-
-  public String getBaseUrl() {
-    return scheme + "://" + host + ":" + getExplicitPort();
   }
 
   public String getAuthority() {

commit eed5cb7741c66a05ce337e393f62c1c287dd48e9

Fix IllegalArgumentException "missing scheme" when recorded simulation mixes http and https on same host, close #3686
Motivation:

05778900db54d7b85d2f5099219fb878310aceb9 introduced a regression: extracted uris might not have a scheme if http and https requests are mixed for same host.

Modification:

Protect against missing scheme in extracted uris.

Result:

No more IllegalArgumentException

gatling-recorder/src/main/scala/io/gatling/recorder/scenario/template/SimulationTemplate.scala
@@ -107,7 +107,16 @@ $mapContent)"""
       }
 
     val extractedUris = new ExtractedUris(flatScenarioElements(scenarioElements))
-    val nonBaseUrls = extractedUris.vals.filter(value => Uri.create(value.value).getBaseUrl != protocol.baseUrl)
+
+    val nonBaseUrls = extractedUris.vals.filter { extractedUri =>
+      val uriWithScheme =
+        if (extractedUri.value.startsWith("http")) {
+          extractedUri.value
+        } else {
+          "http://" + extractedUri.value
+        }
+      Uri.create(uriWithScheme).getBaseUrl != protocol.baseUrl
+    }
 
     fast"""$renderPackage
 import scala.concurrent.duration._