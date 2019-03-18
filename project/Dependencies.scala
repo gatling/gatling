511c606de0b076ac91e7d292a531f3696c2b4048
Stephane Landelle <slandelle@gatling.io>
2019-03-18 10:53:26 +0100

    Upgrade junit 5.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -75,7 +75,7 @@ object Dependencies {
   private val h2                             = "com.h2database"                       % "h2"                              % "1.4.198"           % "test"
   private val jmh                            = "org.openjdk.jmh"                      % "jmh-core"                        % "1.21"
 
-  private val junit                          = "org.junit.jupiter"                    % "junit-jupiter-api"               % "5.4.0"             % "test"
+  private val junit                          = "org.junit.jupiter"                    % "junit-jupiter-api"               % "5.4.1"             % "test"
   private val jetty                          = "org.eclipse.jetty"                    % "jetty-server"                    % "9.4.15.v20190215"  % "test"
   private val jettyProxy                     = jetty.organization                     % "jetty-proxy"                     % jetty.revision      % "test"
 
