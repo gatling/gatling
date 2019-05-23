04272b6b13124ca666bcebe6774efad94d6ee749
Stephane Landelle <slandelle@gatling.io>
2019-05-23 10:06:57 +0200

    nit: compile test proving Proxy default protocol

--- gatling-http/src/test/scala/io/gatling/http/compile/HttpCompileTest.scala
+++ gatling-http/src/test/scala/io/gatling/http/compile/HttpCompileTest.scala
@@ -30,6 +30,7 @@ class HttpCompileTest extends Simulation {
     .baseUrl("http://172.30.5.143:8080")
     .baseUrls("http://172.30.5.143:8080", "http://172.30.5.143:8081")
     .virtualHost("172.30.5.143:8080")
+    .proxy(Proxy("172.31.76.106", 8080))
     .proxy(Proxy("172.31.76.106", 8080).httpsPort(8081))
     .proxy(Proxy("172.31.76.106", 8080).http)
     .proxy(Proxy("172.31.76.106", 8080).socks4)
