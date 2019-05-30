bc7f7c4efe04703e37053a400ae930d8cebcbc0f
Stephane Landelle <slandelle@gatling.io>
2019-05-30 21:45:34 +0200

    Upgrade pebble 3.0.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -64,7 +64,7 @@ object Dependencies {
   private val bouncyCastle                   = "org.bouncycastle"                     % "bcpkix-jdk15on"                  % "1.61"
   private val quicklens                      = "com.softwaremill.quicklens"          %% "quicklens"                       % "1.4.12"
   private val fastUuid                       = "com.eatthepath"                       % "fast-uuid"                       % "0.1"
-  private val pebble                         = "io.pebbletemplates"                   % "pebble"                          % "3.0.9"
+  private val pebble                         = "io.pebbletemplates"                   % "pebble"                          % "3.0.10"
 
   // Test dependencies
   private val scalaTest                      = "org.scalatest"                       %% "scalatest"                       % "3.0.7"             % "test"
