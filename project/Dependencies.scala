6ae9d309e053bdb0e6a0d75ae784d611d4d3167d
Stephane Landelle <slandelle@gatling.io>
2019-09-11 14:56:56 +0200

    Upgrade sfm 8.0.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies {
   private val scopt                          = "com.github.scopt"                    %% "scopt"                           % "3.7.1"
   private val scalaLogging                   = "com.typesafe.scala-logging"          %% "scala-logging"                   % "3.9.2"
   private val jackson                        = "com.fasterxml.jackson.core"           % "jackson-databind"                % "2.9.9.3"
-  private val sfm                            = ("org.simpleflatmapper"                % "lightning-csv"                   % "8.0.1")
+  private val sfm                            = ("org.simpleflatmapper"                % "lightning-csv"                   % "8.0.2")
     .exclude("org.simpleflatmapper", "ow2-asm")
   private val json4sJackson                  = "org.json4s"                          %% "json4s-jackson"                  % "3.6.7"
   private val jsonpath                       = "io.gatling"                          %% "jsonpath"                        % "0.7.0"
