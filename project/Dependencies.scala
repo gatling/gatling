0d09010a362ede5fc7286ef7053cf0b8b67412a9
Stephane Landelle <slandelle@gatling.io>
2020-02-20 01:31:22 +0100

    Upgrade scala-java8-compat 0.9.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   private val scalaSwing                     = "org.scala-lang.modules"              %% "scala-swing"                     % "2.1.1"
   private val scalaXml                       = "org.scala-lang.modules"              %% "scala-xml"                       % "1.2.0"
   private val scalaParserCombinators         = "org.scala-lang.modules"              %% "scala-parser-combinators"        % "1.1.2"
-  private val java8Compat                    = "org.scala-lang.modules"              %% "scala-java8-compat"              % "0.9.0"
+  private val java8Compat                    = "org.scala-lang.modules"              %% "scala-java8-compat"              % "0.9.1"
   private val netty                          = "io.netty"                             % "netty-codec-http"                % "4.1.45.Final"
   private val nettyBuffer                    = netty.organization                     % "netty-buffer"                    % netty.revision
   private val nettyHandler                   = netty.organization                     % "netty-handler"                   % netty.revision
