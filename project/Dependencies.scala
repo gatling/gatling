47ff35f96cda59a409c65813829cadec0330ce15
Stephane Landelle <slandelle@gatling.io>
2020-04-15 14:07:28 +0200

    nit

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,8 +5,8 @@ object Dependencies {
   // Compile dependencies
 
   // format: OFF
-  private def scalaReflect(version: String)  = "org.scala-lang"             % "scala-reflect"                   % version
-  private def scalaCompiler(version: String) = "org.scala-lang"             % "scala-compiler"                  % version
+  private def scalaReflect(version: String)  = "org.scala-lang"                       % "scala-reflect"                   % version
+  private def scalaCompiler(version: String) = "org.scala-lang"                       % "scala-compiler"                  % version
   private val scalaSwing                     = "org.scala-lang.modules"              %% "scala-swing"                     % "2.1.1"
   private val scalaParserCombinators         = "org.scala-lang.modules"              %% "scala-parser-combinators"        % "1.1.2"
   private val java8Compat                    = "org.scala-lang.modules"              %% "scala-java8-compat"              % "0.9.1"
