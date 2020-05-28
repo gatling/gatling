7487d8643cdb4d646167e5a6fc804e041c566fa4
Stephane Landelle <slandelle@gatling.io>
2020-05-28 15:16:43 +0200

    Upgrade quicklens 1.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -57,7 +57,7 @@ object Dependencies {
   private val hdrHistogram                   = "org.hdrhistogram"                     % "HdrHistogram"                    % "2.1.12"
   private val caffeine                       = "com.github.ben-manes.caffeine"        % "caffeine"                        % "2.8.4"
   private val bouncyCastle                   = "org.bouncycastle"                     % "bcpkix-jdk15on"                  % "1.65"
-  private val quicklens                      = "com.softwaremill.quicklens"          %% "quicklens"                       % "1.5.0"
+  private val quicklens                      = "com.softwaremill.quicklens"          %% "quicklens"                       % "1.6.0"
   private val fastUuid                       = "com.eatthepath"                       % "fast-uuid"                       % "0.1"
   private val pebble                         = "io.pebbletemplates"                   % "pebble"                          % "3.1.3"
 
