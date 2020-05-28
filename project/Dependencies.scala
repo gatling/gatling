7f6e45f60385f1fb5b8e14bed003cfdb5f4d809f
Stephane Landelle <slandelle@gatling.io>
2020-05-28 15:17:38 +0200

    Upgrade jetty 9.4.29.v20200521

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -74,7 +74,7 @@ object Dependencies {
   private val jmh                            = "org.openjdk.jmh"                      % "jmh-core"                        % "1.22"
 
   private val junit                          = "org.junit.jupiter"                    % "junit-jupiter-api"               % "5.5.2"             % "test"
-  private val jetty                          = "org.eclipse.jetty"                    % "jetty-server"                    % "9.4.27.v20200227"  % "test"
+  private val jetty                          = "org.eclipse.jetty"                    % "jetty-server"                    % "9.4.29.v20200521"  % "test"
   private val jettyProxy                     = jetty.organization                     % "jetty-proxy"                     % jetty.revision      % "test"
 
   // format: ON
