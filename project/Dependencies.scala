39572d055bca0216e1e2d9a4c3cfa74766c4aed6
Scala Steward <ops@gatling.io>
2020-06-05 09:56:15 +0000

    Update h2 to 1.4.200

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -70,7 +70,7 @@ object Dependencies {
   private val mockitoCore                    = "org.mockito"                          % "mockito-core"                    % "3.2.0"             % "test"
   private val activemqBroker                 = ("org.apache.activemq"                 % "activemq-broker"                 % "5.15.13"           % "test")
     .exclude("org.apache.geronimo.specs", "geronimo-jms_1.1_spec")
-  private val h2                             = "com.h2database"                       % "h2"                              % "1.4.199"           % "test"
+  private val h2                             = "com.h2database"                       % "h2"                              % "1.4.200"           % "test"
   private val jmh                            = "org.openjdk.jmh"                      % "jmh-core"                        % "1.22"
 
   private val junit                          = "org.junit.jupiter"                    % "junit-jupiter-api"               % "5.5.2"             % "test"
