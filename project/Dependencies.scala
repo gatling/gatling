1242c8982a2a7218f584e8d3f89d0a1dc1d8bd30
Scala Steward <ops@gatling.io>
2020-06-08 14:05:44 +0000

    Update akka-actor, akka-slf4j, akka-testkit to 2.6.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   private val nettyEpoll                     = netty.organization                     % "netty-transport-native-epoll"    % netty.revision classifier "linux-x86_64"
   private val nettyHttp2                     = netty.organization                     % "netty-codec-http2"               % netty.revision
   private val nettyBoringSsl                 = netty.organization                     % "netty-tcnative-boringssl-static" % "2.0.30.Final"
-  private val akka                           = "com.typesafe.akka"                   %% "akka-actor"                      % "2.6.5"
+  private val akka                           = "com.typesafe.akka"                   %% "akka-actor"                      % "2.6.6"
   private val akkaSlf4j                      = akka.organization                     %% "akka-slf4j"                      % akka.revision
   private val config                         = "com.typesafe"                         % "config"                          % "1.4.0"
   private val saxon                          = ("net.sf.saxon"                        % "Saxon-HE"                        % "10.1")
