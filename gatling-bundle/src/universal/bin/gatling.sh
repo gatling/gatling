1e30148dac41fb11399f3b2f1f85a2184f35f47c
Stephane Landelle <slandelle@gatling.io>
2020-07-10 23:49:05 +0200

    Disable UseBiasedLocking, close #3918
    Motivation:
    
    In JDK 15, biased locking will be deprecated and disabled. According to Hazelcast JET, any low-latency Java application should have this disabled.
    
    https://openjdk.java.net/jeps/374
    https://jet-start.sh/blog/2020/06/23/jdk-gc-benchmarks-rematch#the-jvm-options
    
    Modifications:
    
    Add "-XX:-UseBiasedLocking" to JVM options.
    
    Result:
    
    Better performance.

--- gatling-bundle/src/universal/bin/gatling.sh
+++ gatling-bundle/src/universal/bin/gatling.sh
@@ -34,7 +34,7 @@ echo "GATLING_HOME is set to ${GATLING_HOME}"
 DEFAULT_JAVA_OPTS="-server"
 DEFAULT_JAVA_OPTS="${DEFAULT_JAVA_OPTS} -Xmx1G -XX:+HeapDumpOnOutOfMemoryError"
 DEFAULT_JAVA_OPTS="${DEFAULT_JAVA_OPTS} -XX:+UseG1GC -XX:+ParallelRefProcEnabled"
-DEFAULT_JAVA_OPTS="${DEFAULT_JAVA_OPTS} -XX:MaxInlineLevel=20 -XX:MaxTrivialSize=12"
+DEFAULT_JAVA_OPTS="${DEFAULT_JAVA_OPTS} -XX:MaxInlineLevel=20 -XX:MaxTrivialSize=12 -XX:-UseBiasedLocking"
 DEFAULT_JAVA_OPTS="${DEFAULT_JAVA_OPTS} -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false"
 COMPILER_OPTS="-Xss100M $DEFAULT_JAVA_OPTS $JAVA_OPTS"
 
