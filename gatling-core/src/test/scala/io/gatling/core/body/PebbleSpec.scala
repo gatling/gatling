c837146e29bdb52e76cf7eb31776cc1b8dd31c7a
Stephane Landelle <slandelle@gatling.io>
2020-10-02 09:26:58 +0200

    nit: Pebble shouldn't expose internal Gatling Session attributes

--- gatling-core/src/test/scala/io/gatling/core/body/PebbleSpec.scala
+++ gatling-core/src/test/scala/io/gatling/core/body/PebbleSpec.scala
@@ -22,63 +22,63 @@ import scala.collection.immutable
 import scala.collection.mutable
 
 import io.gatling.BaseSpec
+import io.gatling.core.session.SessionPrivateAttributes
 
 class PebbleSpec extends BaseSpec {
 
-  "toJava" should "convert attributes of type immutable.Map" in {
+  "sessionAttributesToJava" should "convert attributes of type immutable.Map" in {
     val input = Map("foo" -> immutable.Map("bar" -> "baz"))
-    val output = Pebble.toJava(input)
-    output shouldBe a[ju.Map[_, _]]
+    val output = Pebble.sessionAttributesToJava(input)
     output.size shouldBe 1
     output.get("foo").asInstanceOf[ju.Map[_, _]].get("bar") shouldBe "baz"
   }
 
   it should "convert attributes of type mutable.Map" in {
     val input = Map("foo" -> mutable.Map("bar" -> "baz"))
-    val output = Pebble.toJava(input)
-    output shouldBe a[ju.Map[_, _]]
+    val output = Pebble.sessionAttributesToJava(input)
     output.size shouldBe 1
     output.get("foo").asInstanceOf[ju.Map[_, _]].get("bar") shouldBe "baz"
   }
 
   it should "convert attributes of type immutable.Seq" in {
     val input = Map("foo" -> immutable.Seq("bar"))
-    val output = Pebble.toJava(input)
-    output shouldBe a[ju.Map[_, _]]
+    val output = Pebble.sessionAttributesToJava(input)
     output.size shouldBe 1
     output.get("foo").asInstanceOf[ju.List[_]].get(0) shouldBe "bar"
   }
 
   it should "convert attributes of type mutable.Seq" in {
     val input = Map("foo" -> mutable.Seq("bar"))
-    val output = Pebble.toJava(input)
-    output shouldBe a[ju.Map[_, _]]
+    val output = Pebble.sessionAttributesToJava(input)
     output.size shouldBe 1
     output.get("foo").asInstanceOf[ju.List[_]].get(0) shouldBe "bar"
   }
 
   it should "convert attributes of type immutable.Set" in {
     val input = Map("foo" -> immutable.Set("bar"))
-    val output = Pebble.toJava(input)
-    output shouldBe a[ju.Map[_, _]]
+    val output = Pebble.sessionAttributesToJava(input)
     output.size shouldBe 1
     output.get("foo").asInstanceOf[ju.Set[_]].iterator.next() shouldBe "bar"
   }
 
   it should "convert attributes of type mutable.Set" in {
     val input = Map("foo" -> mutable.Set("bar"))
-    val output = Pebble.toJava(input)
-    output shouldBe a[ju.Map[_, _]]
+    val output = Pebble.sessionAttributesToJava(input)
     output.size shouldBe 1
     output.get("foo").asInstanceOf[ju.Set[_]].iterator.next() shouldBe "bar"
   }
 
   it should "not convert other types" in {
     val input = Map("foo" -> 1, "bar" -> "baz")
-    val output = Pebble.toJava(input)
-    output shouldBe a[ju.Map[_, _]]
+    val output = Pebble.sessionAttributesToJava(input)
     output.size shouldBe 2
     output.get("foo") shouldBe 1
     output.get("bar") shouldBe "baz"
+  }
+
+  it should "remove any Gatling private attribute" in {
+    val input = Map("foo" -> "bar", SessionPrivateAttributes.PrivateAttributePrefix + "hello" -> "world")
+    val output = Pebble.sessionAttributesToJava(input)
+    output.size shouldBe 1
   }
 }
