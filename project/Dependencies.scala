70c74999ba8cad0f0ce301df8e61e22cc7b39634
Scala Steward <ops@gatling.io>
2020-10-22 16:06:47 +0000

    Update jetty-proxy, jetty-server to 9.4.33.v20201020

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -74,7 +74,7 @@ object Dependencies {
   private val jmh                            = "org.openjdk.jmh"                      % "jmh-core"                        % "1.25.2"
 
   private val junit                          = "org.junit.jupiter"                    % "junit-jupiter-api"               % "5.5.2"             % "test"
-  private val jetty                          = "org.eclipse.jetty"                    % "jetty-server"                    % "9.4.31.v20200723"  % "test"
+  private val jetty                          = "org.eclipse.jetty"                    % "jetty-server"                    % "9.4.33.v20201020"  % "test"
   private val jettyProxy                     = jetty.organization                     % "jetty-proxy"                     % jetty.revision      % "test"
 
   // format: ON
