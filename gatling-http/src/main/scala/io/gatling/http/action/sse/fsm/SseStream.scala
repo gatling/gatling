1ab469e5dd58e8661d6d481143bf6b39238c84ea
Stephane Landelle <slandelle@gatling.io>
2020-11-08 08:46:58 +0100

    nit

--- gatling-http/src/main/scala/io/gatling/http/action/sse/fsm/SseStream.scala
+++ gatling-http/src/main/scala/io/gatling/http/action/sse/fsm/SseStream.scala
@@ -99,9 +99,7 @@ class SseStream(
         logger.debug("Server closed the stream while in state Open. Reconnecting.")
         // reconnect
         originalSession.eventLoop.schedule(
-          new Runnable {
-            override def run(): Unit = connect()
-          },
+          (() => connect()): Runnable,
           retryDelayInSeconds,
           TimeUnit.SECONDS
         )
