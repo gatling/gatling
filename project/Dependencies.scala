e20919e67228a5c9bc5407dd12234bf485534974
Scala Steward <ops@gatling.io>
2021-02-19 14:54:22 +0000

    Update scalatest to 3.2.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -72,7 +72,7 @@ object Dependencies {
   private val pebble                         = "io.pebbletemplates"                   % "pebble"                          % "3.1.5"
 
   // Test dependencies
-  private val scalaTest                      = "org.scalatest"                       %% "scalatest"                       % "3.2.4"             % Test
+  private val scalaTest                      = "org.scalatest"                       %% "scalatest"                       % "3.2.5"             % Test
   private val scalaTestScalacheck            = "org.scalatestplus"                   %% "scalacheck-1-15"                 % "3.2.4.0"           % Test
   private val scalaTestMockito               = scalaTestScalacheck.organization      %% "mockito-3-4"                     % "3.2.3.0"           % Test
   private val scalaCheck                     = "org.scalacheck"                      %% "scalacheck"                      % "1.15.3"            % Test
