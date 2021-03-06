0c8105f7917e2cf3a4ff6123adf5885f90ed8949
Stephane Landelle <slandelle@gatling.io>
2021-03-06 12:59:42 +0100

    Upgrade jackson 2.12.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
     .exclude("org.scala-lang.modules", "scala-collection-compat_2.13")
   private val scopt                          = "com.github.scopt"                    %% "scopt"                           % "3.7.1"
   private val scalaLogging                   = "com.typesafe.scala-logging"          %% "scala-logging"                   % "3.9.2"
-  private val jackson                        = "com.fasterxml.jackson.core"           % "jackson-databind"                % "2.12.1"
+  private val jackson                        = "com.fasterxml.jackson.core"           % "jackson-databind"                % "2.12.2"
   private val sfm                            = ("org.simpleflatmapper"                % "lightning-csv"                   % "8.2.3")
     .exclude("org.simpleflatmapper", "ow2-asm")
   private val lagarto                        = "org.jodd"                             % "jodd-lagarto"                    % "6.0.3"
