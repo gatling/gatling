401a3a8e27f75fbf124432d5ec741060d6268b90
Stephane Landelle <slandelle@gatling.io>
2021-04-01 08:56:21 +0200

    Upgrade scalacheck-1-15 3.2.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -68,7 +68,7 @@ object Dependencies {
 
   // Test dependencies
   private val scalaTest                      = "org.scalatest"                       %% "scalatest"                       % "3.2.7"             % Test
-  private val scalaTestScalacheck            = "org.scalatestplus"                   %% "scalacheck-1-15"                 % "3.2.6.0"           % Test
+  private val scalaTestScalacheck            = "org.scalatestplus"                   %% "scalacheck-1-15"                 % "3.2.7.0"           % Test
   private val scalaTestMockito               = scalaTestScalacheck.organization      %% "mockito-3-4"                     % "3.2.7.0"           % Test
   private val scalaCheck                     = "org.scalacheck"                      %% "scalacheck"                      % "1.15.3"            % Test
   private val akkaTestKit                    = akka.organization                     %% "akka-testkit"                    % akka.revision       % Test
