51f04bd5fb77b60668b1397e8f726f920ffb283b
ops+github@gatling.io <Gatling CI>
2021-06-14 02:41:06 +0000

    Update bcpkix-jdk15on to 1.69

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -66,7 +66,7 @@ object Dependencies {
   private val tdigest                        = "com.tdunning"                         % "t-digest"                        % "3.1"
   private val hdrHistogram                   = "org.hdrhistogram"                     % "HdrHistogram"                    % "2.1.12"
   private val caffeine                       = "com.github.ben-manes.caffeine"        % "caffeine"                        % "2.9.1"
-  private val bouncyCastle                   = "org.bouncycastle"                     % "bcpkix-jdk15on"                  % "1.68"
+  private val bouncyCastle                   = "org.bouncycastle"                     % "bcpkix-jdk15on"                  % "1.69"
   private val quicklens                      = "com.softwaremill.quicklens"          %% "quicklens"                       % "1.7.4"
   private val fastUuid                       = "com.eatthepath"                       % "fast-uuid"                       % "0.1"
   private val pebble                         = "io.pebbletemplates"                   % "pebble"                          % "3.1.5"
