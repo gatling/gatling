2952de972eba9f84f186459d020c0ddc307727fd
ops+github@gatling.io <Gatling CI>
2021-06-21 02:41:15 +0000

    Update scala-logging to 3.9.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -34,7 +34,7 @@ object Dependencies {
     .exclude("org.typelevel", "algebra_2.13")
     .exclude("org.scala-lang.modules", "scala-collection-compat_2.13")
   private val scopt                          = "com.github.scopt"                    %% "scopt"                           % "3.7.1"
-  private val scalaLogging                   = "com.typesafe.scala-logging"          %% "scala-logging"                   % "3.9.3"
+  private val scalaLogging                   = "com.typesafe.scala-logging"          %% "scala-logging"                   % "3.9.4"
   private val jackson                        = "com.fasterxml.jackson.core"           % "jackson-databind"                % "2.12.3"
   private val sfm                            = ("org.simpleflatmapper"                % "lightning-csv"                   % "8.2.3")
     .exclude("org.simpleflatmapper", "ow2-asm")
