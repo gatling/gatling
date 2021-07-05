e1a41dee6afba56c98b29430c02b955ca576d890
ops+github@gatling.io <Gatling CI>
2021-07-05 02:30:33 +0000

    Update caffeine to 2.9.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -65,7 +65,7 @@ object Dependencies {
   private val logback                        = "ch.qos.logback"                       % "logback-classic"                 % "1.2.3"
   private val tdigest                        = "com.tdunning"                         % "t-digest"                        % "3.1"
   private val hdrHistogram                   = "org.hdrhistogram"                     % "HdrHistogram"                    % "2.1.12"
-  private val caffeine                       = "com.github.ben-manes.caffeine"        % "caffeine"                        % "2.9.1"
+  private val caffeine                       = "com.github.ben-manes.caffeine"        % "caffeine"                        % "2.9.2"
   private val bouncyCastle                   = "org.bouncycastle"                     % "bcpkix-jdk15on"                  % "1.69"
   private val quicklens                      = "com.softwaremill.quicklens"          %% "quicklens"                       % "1.7.4"
   private val fastUuid                       = "com.eatthepath"                       % "fast-uuid"                       % "0.1"
