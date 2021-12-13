09a1293024dd40ada0f5b814fbffcc5cf1976fa5
ops+github@gatling.io <Gatling CI>
2021-12-13 02:39:20 +0000

    Update netty-buffer, netty-codec-http, ... to 4.1.71.Final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     .exclude("org.jline", "jline")
   private val scalaSwing                     = "org.scala-lang.modules"              %% "scala-swing"                     % "3.0.0"
   private val scalaParserCombinators         = "org.scala-lang.modules"              %% "scala-parser-combinators"        % "2.1.0"
-  private val netty                          = "io.netty"                             % "netty-codec-http"                % "4.1.69.Final"
+  private val netty                          = "io.netty"                             % "netty-codec-http"                % "4.1.71.Final"
   private val nettyBuffer                    = netty.organization                     % "netty-buffer"                    % netty.revision
   private val nettyHandler                   = netty.organization                     % "netty-handler"                   % netty.revision
   private val nettyMqtt                      = netty.organization                     % "netty-codec-mqtt"                % netty.revision
