dc214a3d93de94870ef249906d85ebebd69d6996
ops+github@gatling.io <Gatling CI>
2021-12-13 02:39:36 +0000

    Update compiler-bridge, zinc to 1.5.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
   private val jmespath                       = "io.burt"                              % "jmespath-jackson"                % "0.5.1"
   private val boopickle                      = "io.suzaku"                           %% "boopickle"                       % "1.3.3"
   private val redisClient                    = "net.debasishg"                       %% "redisclient"                     % "3.42"
-  private val zinc                           = ("org.scala-sbt"                      %% "zinc"                            % "1.5.7")
+  private val zinc                           = ("org.scala-sbt"                      %% "zinc"                            % "1.5.8")
     .exclude("org.jline", "jline")
     .exclude("org.scala-sbt.jline3", "jline-terminal")
     .exclude("org.jline", "jline-terminal-jna")
