gatling/gatling

commit 199f3511f9889d09d8532dbaa62aa4c6ae43bd99

Make multipart work over HTTP/2, close #4199
Motivation:

Multipart payloads are not properly sent when using HTTP/2.

Modification:

* fix `ChunkedWriteHandler` location in the pipeline when using HTTP/2
* turn `MultipartChunkedInput` into a `ChunkedInput<HttpContent>` so `HttpToHttp2ConnectionHandler` properly converts chunks into DataFrames.

Result:

Multipart works over HTTP/2

gatling-http-client/src/main/java/io/gatling/http/client/impl/DefaultHttpClient.java
@@ -161,10 +161,7 @@ public class DefaultHttpClient implements HttpClient {
                   new ChannelInitializer<Channel>() {
                     @Override
                     protected void initChannel(Channel channel) {
-                      channel
-                          .pipeline()
-                          .addLast(PINNED_HANDLER, NoopHandler.INSTANCE)
-                          .addLast(CHUNKED_WRITER_HANDLER, new ChunkedWriteHandler());
+                      channel.pipeline().addLast(PINNED_HANDLER, NoopHandler.INSTANCE);
                     }
                   });
 
@@ -951,6 +948,7 @@ public class DefaultHttpClient implements HttpClient {
 
                     ctx.pipeline()
                         .addLast(HTTP2_HANDLER, http2Handler)
+                        .addLast(CHUNKED_WRITER_HANDLER, new ChunkedWriteHandler())
                         .addLast(
                             APP_HTTP2_HANDLER,
                             new Http2AppHandler(

gatling-http-client/src/main/java/io/gatling/http/client/body/multipart/MultipartFormDataRequestBody.java
@@ -76,7 +76,7 @@ public final class MultipartFormDataRequestBody extends RequestBody.Base<List<Pa
     try {
       ByteBuf chunk;
       do {
-        chunk = content.readChunk(ByteBufAllocator.DEFAULT);
+        chunk = content.readChunk(ByteBufAllocator.DEFAULT).content();
         composite.addComponent(true, chunk);
       } while (chunk != null);
       return ByteBufUtils.byteBuf2Bytes(composite);

gatling-http-client/src/main/java/io/gatling/http/client/body/multipart/impl/MultipartChunkedInput.java
@@ -21,6 +21,8 @@ import static io.gatling.http.client.util.MiscUtils.*;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultHttpContent;
+import io.netty.handler.codec.http.HttpContent;
 import io.netty.handler.stream.ChunkedInput;
 import java.io.IOException;
 import java.util.List;
@@ -28,7 +30,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MultipartChunkedInput implements ChunkedInput<ByteBuf> {
+public class MultipartChunkedInput implements ChunkedInput<HttpContent> {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MultipartChunkedInput.class);
 
@@ -81,13 +83,12 @@ public class MultipartChunkedInput implements ChunkedInput<ByteBuf> {
 
   @Override
   @Deprecated
-  public ByteBuf readChunk(ChannelHandlerContext ctx) throws Exception {
+  public HttpContent readChunk(ChannelHandlerContext ctx) throws Exception {
     return readChunk(ctx.alloc());
   }
 
   @Override
-  public ByteBuf readChunk(ByteBufAllocator alloc) throws Exception {
-
+  public HttpContent readChunk(ByteBufAllocator alloc) throws Exception {
     if (endOfInput) {
       return null;
     }
@@ -98,13 +99,13 @@ public class MultipartChunkedInput implements ChunkedInput<ByteBuf> {
     switch (state) {
       case STOP:
         endOfInput = true;
-        return buffer;
+        return new DefaultHttpContent(buffer);
       case SUSPEND:
         // this will suspend the stream in ChunkedWriteHandler
         buffer.release();
         return null;
       case CONTINUE:
-        return buffer;
+        return new DefaultHttpContent(buffer);
       default:
         throw new IllegalStateException("Unknown state: " + state);
     }

commit 27ee5f94345282aab2626df31a418131848ce666

Fix regression crashing HTTP/1 requests when HTTP/2 is enabled, close #4213
Motivation:

Regression introduced in https://github.com/gatling/gatling/commit/199f3511f9889d09d8532dbaa62aa4c6ae43bd99

Getting a `NoSuchElementException: chunked-writer` as it's now no longer set yet in this path of the code.

Modification:

Align pipeline configuration logic on ALPN success branch.

Result:

No more crash

gatling-http-client/src/main/java/io/gatling/http/client/impl/DefaultHttpClient.java
@@ -993,13 +993,10 @@ public class DefaultHttpClient implements HttpClient {
                     }
                     tx.listener.onProtocolAwareness(false);
                     ctx.pipeline()
-                        .addBefore(CHUNKED_WRITER_HANDLER, HTTP_CLIENT_CODEC, newHttpClientCodec())
-                        .addBefore(
-                            CHUNKED_WRITER_HANDLER,
-                            INFLATER_HANDLER,
-                            new CustomHttpContentDecompressor())
-                        .addAfter(
-                            CHUNKED_WRITER_HANDLER,
+                        .addLast(HTTP_CLIENT_CODEC, newHttpClientCodec())
+                        .addLast(INFLATER_HANDLER, new CustomHttpContentDecompressor())
+                        .addLast(CHUNKED_WRITER_HANDLER, new ChunkedWriteHandler())
+                        .addLast(
                             APP_HTTP_HANDLER,
                             new HttpAppHandler(DefaultHttpClient.this, channelPool, config));
                     whenAlpn.setSuccess(null);