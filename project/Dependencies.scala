8442b19297c211ab04ef7799ddf43dfda68500ad
Gatling CI <64018838+gatling-ci@users.noreply.github.com>
2022-02-21 18:13:33 +0100

    Update activemq-broker to 5.16.4 (#376)
    Co-authored-by: ops+github@gatling.io <Gatling CI>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -82,7 +82,7 @@ object Dependencies {
   private val scalaCheck                     = "org.scalacheck"                      %% "scalacheck"                      % "1.15.4"            % Test
   private val akkaTestKit                    = akka.organization                     %% "akka-testkit"                    % akka.revision       % Test
   private val mockitoCore                    = "org.mockito"                          % "mockito-core"                    % "4.3.1"            % Test
-  private val activemqBroker                 = ("org.apache.activemq"                 % "activemq-broker"                 % "5.16.3"            % Test)
+  private val activemqBroker                 = ("org.apache.activemq"                 % "activemq-broker"                 % "5.16.4"            % Test)
     .exclude("org.apache.geronimo.specs", "geronimo-jms_1.1_spec")
   private val h2                             = "com.h2database"                       % "h2"                              % "2.1.210"           % Test
   private val jmh                            = "org.openjdk.jmh"                      % "jmh-core"                        % "1.27"
