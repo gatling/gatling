293ea1425f4dc630b92f20f46484b3f18164c165
ops+github@gatling.io <Gatling CI>
2022-04-11 02:38:28 +0000

    Update scalacheck to 1.16.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -80,7 +80,7 @@ object Dependencies {
   private val scalaTest                      = "org.scalatest"                       %% "scalatest"                         % "3.2.11"            % Test
   private val scalaTestScalacheck            = "org.scalatestplus"                   %% "scalacheck-1-15"                   % "3.2.11.0"          % Test
   private val scalaTestMockito               = scalaTestScalacheck.organization      %% "mockito-3-4"                       % "3.2.10.0"          % Test
-  private val scalaCheck                     = "org.scalacheck"                      %% "scalacheck"                        % "1.15.4"            % Test
+  private val scalaCheck                     = "org.scalacheck"                      %% "scalacheck"                        % "1.16.0"            % Test
   private val akkaTestKit                    = akka.organization                     %% "akka-testkit"                      % akka.revision       % Test
   private val mockitoCore                    = "org.mockito"                          % "mockito-core"                      % "4.4.0"            % Test
   private val activemqBroker                 = ("org.apache.activemq"                 % "activemq-broker"                   % "5.16.4"            % Test)
