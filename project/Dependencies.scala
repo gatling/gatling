e79bd093410e04b893a02daf3a61e650884d888c
ops+github@gatling.io <Gatling CI>
2022-05-09 02:45:13 +0000

    Update activemq-broker to 5.16.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -83,7 +83,7 @@ object Dependencies {
   private val scalaCheck                     = "org.scalacheck"                      %% "scalacheck"                        % "1.16.0"            % Test
   private val akkaTestKit                    = akka.organization                     %% "akka-testkit"                      % akka.revision       % Test
   private val mockitoCore                    = "org.mockito"                          % "mockito-core"                      % "4.5.1"            % Test
-  private val activemqBroker                 = ("org.apache.activemq"                 % "activemq-broker"                   % "5.16.4"            % Test)
+  private val activemqBroker                 = ("org.apache.activemq"                 % "activemq-broker"                   % "5.16.5"            % Test)
     .exclude("org.apache.geronimo.specs", "geronimo-jms_1.1_spec")
   private val h2                             = "com.h2database"                       % "h2"                                % "2.1.212"           % Test
   private val jmh                            = "org.openjdk.jmh"                      % "jmh-core"                          % "1.27"
