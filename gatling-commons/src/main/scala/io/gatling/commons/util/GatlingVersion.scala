96643d9cd015064f46ad815a5ac3a1049170542d
Stephane Landelle <slandelle@gatling.io>
2022-07-04 10:47:59 +0200

    nit

--- gatling-commons/src/main/scala/io/gatling/commons/util/GatlingVersion.scala
+++ gatling-commons/src/main/scala/io/gatling/commons/util/GatlingVersion.scala
@@ -44,7 +44,7 @@ object GatlingVersion {
 }
 
 final case class GatlingVersion(fullVersion: String, releaseDate: ZonedDateTime) {
-  val minorVersion: String = fullVersion.split("\\.").take(2).mkString(".")
+  val minorVersion: String = fullVersion.split('.').take(2).mkString(".")
   val majorVersion: String = fullVersion.substring(fullVersion.indexOf('.'))
   def isSnapshot: Boolean = fullVersion.endsWith("-SNAPSHOT")
   def isEnterprise: Boolean = fullVersion.contains(".FL")
--- gatling-commons/src/main/scala/io/gatling/commons/util/Java.scala
+++ gatling-commons/src/main/scala/io/gatling/commons/util/Java.scala
@@ -20,7 +20,7 @@ import scala.util.Properties
 
 object Java {
   val MajorVersion: Int = {
-    (Properties.javaSpecVersion.split("\\.").toList match {
+    (Properties.javaSpecVersion.split('.').toList match {
       case "1" :: major :: _ => major
       case major :: _        => major
       case _                 => throw new IllegalArgumentException(s"Malformed java.specification.version System property value: ${Properties.javaSpecVersion}")
