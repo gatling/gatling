9a888036ec5b4d537224808af0a48b46f66ae6ab
ops+github@gatling.io <Gatling CI>
2022-07-25 02:50:06 +0000

    Update netty-tcnative-boringssl-static, ... to 2.0.54.Final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   private val nettyEpollLinuxX86             = netty.withName("netty-transport-native-epoll")                               classifier "linux-x86_64"
   private val nettyEpollLinuxArm             = netty.withName("netty-transport-native-epoll")                               classifier "linux-aarch_64"
   private val nettyHttp2                     = netty.withName("netty-codec-http2")
-  private val nettyTcNative                  = netty.organization                     % "netty-tcnative-classes"            % "2.0.53.Final"
+  private val nettyTcNative                  = netty.organization                     % "netty-tcnative-classes"            % "2.0.54.Final"
   private val nettyTcNativeBoringSsl         = nettyTcNative.withName("netty-tcnative-boringssl-static")
   private val nettyTcNativeBoringSslLinuxX86 = nettyTcNativeBoringSsl  classifier "linux-x86_64"
   private val nettyTcNativeBoringSslLinuxArm = nettyTcNativeBoringSsl  classifier "linux-aarch_64"
