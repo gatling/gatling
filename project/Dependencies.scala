ac68307b6b4aa414e09d9c6436189396f43f45d7
Stephane Landelle <slandelle@gatling.io>
2022-08-06 15:29:23 +0200

    nit

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -88,7 +88,7 @@ object Dependencies {
   private val scalaTestScalacheck            = "org.scalatestplus"                   %% "scalacheck-1-15"                   % "3.2.11.0"          % Test
   private val scalaTestMockito               = scalaTestScalacheck.organization      %% "mockito-3-4"                       % "3.2.10.0"          % Test
   private val scalaCheck                     = "org.scalacheck"                      %% "scalacheck"                        % "1.16.0"            % Test
-  private val akkaTestKit                    = akka.withName("akka-testkit")                                                                      % Test
+  private val akkaTestKit                    = akka.withName("akka-testkit")                                                              % Test
   private val mockitoCore                    = "org.mockito"                          % "mockito-core"                      % "4.6.1"             % Test
   private val activemqBroker                 = ("org.apache.activemq"                 % "activemq-broker"                   % "5.16.5"            % Test)
     .exclude("org.apache.geronimo.specs", "geronimo-jms_1.1_spec")
