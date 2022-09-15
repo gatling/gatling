a670433ab5ee6c8adb74384966c52a14d3223708
bairov <amerousful@gmail.com>
2022-09-15 16:54:30 +0300

    fix: ZonedDate for Gatling EL 'currentDate', close #4332
    Motivation:
    
    DateTimeFormatter.ofPattern(pattern).format(LocalDateTime.now()) doesn't support time zone
    
    Pattern EEE MMM dd YYYY HH:mm:ss 'GMT'Z will throw java.time.temporal.UnsupportedTemporalTypeException: Unsupported field: OffsetSeconds
    
    Modification:
    
    Use ZonedDateTime instead of LocalDateTime.

--- gatling-core/src/main/scala/io/gatling/core/session/el/ElCompiler.scala
+++ gatling-core/src/main/scala/io/gatling/core/session/el/ElCompiler.scala
@@ -17,7 +17,7 @@
 package io.gatling.core.session.el
 
 import java.{ util => ju }
-import java.time.LocalDateTime
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.util.UUID
 import java.util.concurrent.ThreadLocalRandom
@@ -235,7 +235,7 @@ case object CurrentTimeMillisPart extends ElPart[Long] {
 }
 
 final case class CurrentDateTimePart(format: DateTimeFormatter) extends ElPart[String] {
-  def apply(session: Session): Validation[String] = format.format(LocalDateTime.now()).success
+  def apply(session: Session): Validation[String] = format.format(ZonedDateTime.now()).success
 }
 
 case object RandomSecureUUID extends ElPart[UUID] {
