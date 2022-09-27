7147d687aed5f3ac109f4a4fa6f5604932eba9d4
Stephane Landelle <slandelle@gatling.io>
2022-09-27 21:49:34 +0200

    chore: improve feeder crash message

--- gatling-core/src/main/scala/io/gatling/core/action/Feed.scala
+++ gatling-core/src/main/scala/io/gatling/core/action/Feed.scala
@@ -17,6 +17,7 @@
 package io.gatling.core.action
 
 import io.gatling.commons.util.Clock
+import io.gatling.commons.validation._
 import io.gatling.core.session.{ Expression, Session }
 import io.gatling.core.stats.StatsEngine
 
--- gatling-core/src/main/scala/io/gatling/core/action/FeedActor.scala
+++ gatling-core/src/main/scala/io/gatling/core/action/FeedActor.scala
@@ -28,57 +28,65 @@ import io.gatling.core.session.Session
 import akka.actor.{ ActorRef, Props }
 
 private[core] object FeedActor {
-  def props[T](feeder: Feeder[T], controller: ActorRef): Props = Props(new FeedActor(feeder, controller))
+  def props[T](feeder: Feeder[T], feederName: Option[String], controller: ActorRef): Props = Props(new FeedActor(feeder, feederName, controller))
 }
 
-private final class FeedActor[T](val feeder: Feeder[T], controller: ActorRef) extends BaseActor {
+private final class FeedActor[T](val feeder: Feeder[T], feederName: Option[String], controller: ActorRef) extends BaseActor {
 
-  private def withErrorHandling(f: => Validation[Record[Any]]): Validation[Record[Any]] =
-    try {
-      f
-    } catch {
-      case _: NoSuchElementException | _: ArrayIndexOutOfBoundsException => "Feeder is now empty, stopping engine".failure
-      case NonFatal(e) =>
-        logger.error("Feeder crashed", e)
-        s"Feeder crashed: ${e.detailedMessage}".failure
-    }
+  private def emptyFeederFailure = s"Feeder ${feederName.getOrElse("unknown")} is now empty, stopping engine".failure
 
   private def pollSingleRecord(): Validation[Record[Any]] =
-    withErrorHandling(feeder.next().success)
+    if (feeder.hasNext) {
+      feeder.next().success
+    } else {
+      emptyFeederFailure
+    }
 
   private def pollMultipleRecords(n: Int): Validation[Record[Any]] =
-    withErrorHandling {
-      if (n <= 0) {
-        s"$n is not a valid number of records".failure
-      } else {
-        val map = feeder
-          .next()
-          .view
-          .mapValues { value =>
-            val array = new Array[Any](n)
-            array(0) = value
-            array
-          }
-          .toMap
+    if (feeder.hasNext) {
+      val map: Map[String, Array[Any]] = feeder
+        .next()
+        .view
+        .mapValues { value =>
+          val array = new Array[Any](n)
+          array(0) = value
+          array
+        }
+        .toMap
 
-        for {
-          n <- 2 to n
-          (key, value) <- feeder.next()
-        } map.get(key).foreach(array => array(n - 1) = value)
-
-        map.success
+      var i = 1
+      while (feeder.hasNext && i < n) {
+        for ((key, value) <- feeder.next()) {
+          map.get(key).foreach(array => array(i) = value)
+        }
+        i += 1
       }
+
+      if (i == n) {
+        map.success
+      } else {
+        emptyFeederFailure
+      }
+    } else {
+      emptyFeederFailure
     }
 
   def receive: Receive = { case FeedMessage(session, number, next) =>
-    val newAttributes = number match {
-      case Some(n) => pollMultipleRecords(n)
-      case _       => pollSingleRecord()
-    }
+    try {
+      val newAttributes = number match {
+        case Some(n) if n > 1  => pollMultipleRecords(n)
+        case Some(n) if n <= 0 => s"$n is not a valid number of records".failure
+        case _                 => pollSingleRecord()
+      }
 
-    newAttributes match {
-      case Success(attr)    => next ! session.setAll(attr)
-      case Failure(message) => controller ! ControllerCommand.Crash(new IllegalStateException(message))
+      newAttributes match {
+        case Success(attr)    => next ! session.setAll(attr)
+        case Failure(message) => controller ! ControllerCommand.Crash(new IllegalStateException(message))
+      }
+    } catch {
+      case NonFatal(e) =>
+        logger.error(s"Feeder ${feederName.getOrElse("unknown")} crashed", e)
+        s"Feeder ${feederName.getOrElse("unknown")} crashed: ${e.detailedMessage}, stopping engine".failure
     }
   }
 
