176dea89b495b2e840b68d58931207f23af54700
ops+github@gatling.io <Gatling CI>
2022-10-10 03:00:22 +0000

    Update jupiter-interface, ... to 0.11.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -99,7 +99,7 @@ object Dependencies {
 
   private val junit                          = "org.junit.jupiter"                    % "junit-jupiter-api"                 % "5.9.1"             % Test
   private val junitEngine                    = junit.withName("junit-jupiter-engine")
-  private val jupiterInterface               = "net.aichler"                          % "jupiter-interface"                 % "0.11.0"            % Test
+  private val jupiterInterface               = "net.aichler"                          % "jupiter-interface"                 % "0.11.1"            % Test
 
   private val jetty                          = "org.eclipse.jetty"                    % "jetty-server"                      % "9.4.49.v20220914"  % Test
   private val jettyProxy                     = jetty.organization                     % "jetty-proxy"                       % jetty.revision      % Test
