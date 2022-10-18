95a8183dabbf656c74fb14e16516040706ec48f4
Erwan <Erwan>
2022-10-18 10:28:36 +0200

    add dsl implicit builder.build

--- gatling-udp/src/main/scala/io/github/gatling/udp/UdpDsl.scala
+++ gatling-udp/src/main/scala/io/github/gatling/udp/UdpDsl.scala
@@ -3,12 +3,20 @@ package io.github.gatling.udp
 import io.gatling.core.session._
 import io.github.gatling.udp.check.UdpCheckSupport
 import io.github.gatling.udp.protocol.{UdpProtocol, UdpProtocolBuilder}
-import io.github.gatling.udp.request.UdpDslBuilder
+import io.github.gatling.udp.request.{FireAndForgetDslBuilder, SendConnectedDslBuilder, UdpDslBuilder}
+
+import scala.language.implicitConversions
 
 trait UdpDsl extends UdpCheckSupport {
 
   def udp(host: String, port: Int): UdpProtocol = new UdpProtocolBuilder(host, port).build
 
   def udp(requestName: Expression[String]): UdpDslBuilder = UdpDslBuilder(requestName)
+
+  implicit def udpProtocolBuilder2udpProtocol(builder: UdpProtocolBuilder): UdpProtocol = builder.build
+
+  implicit def udpDslBuilder2ActionBuilder(builder: FireAndForgetDslBuilder): ActionBuilder = builder.build
+
+  implicit def udpDslBuilder2ActionBuilder(builder: SendConnectedDslBuilder): ActionBuilder = builder.build
 
 }
