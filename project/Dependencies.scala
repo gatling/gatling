7d20b8818a9ceaf669ac0fc53a64da076720b8e8
ops+github@gatling.io <Gatling CI>
2022-11-21 02:39:59 +0000

    Update mockito-core to 4.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -91,7 +91,7 @@ object Dependencies {
   private val scalaTestMockito               = scalaTestScalacheck.organization      %% "mockito-3-4"                       % "3.2.10.0"          % Test
   private val scalaCheck                     = "org.scalacheck"                      %% "scalacheck"                        % "1.17.0"            % Test
   private val akkaTestKit                    = akka.withName("akka-testkit")                                                              % Test
-  private val mockitoCore                    = "org.mockito"                          % "mockito-core"                      % "4.8.1"             % Test
+  private val mockitoCore                    = "org.mockito"                          % "mockito-core"                      % "4.9.0"             % Test
   private val activemqBroker                 = ("org.apache.activemq"                 % "activemq-broker"                   % "5.16.5"            % Test)
     .exclude("org.apache.geronimo.specs", "geronimo-jms_1.1_spec")
   private val h2                             = "com.h2database"                       % "h2"                                % "2.1.214"           % Test
