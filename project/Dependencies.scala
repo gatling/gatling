0da852cd9323bd327ad75c10f31bc5829a144137
Stephane Landelle <slandelle@gatling.io>
2023-04-17 10:28:52 +0200

    Upgrade gatling-recorder-bc-shaded 1.73

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -77,7 +77,7 @@ object Dependencies {
   private val tdigest                        = "com.tdunning"                         % "t-digest"                          % "3.1"
   private val hdrHistogram                   = "org.hdrhistogram"                     % "HdrHistogram"                      % "2.1.12"
   private val caffeine                       = "com.github.ben-manes.caffeine"        % "caffeine"                          % "2.9.3"
-  private val bouncyCastle                   = "io.gatling"                           % "gatling-recorder-bc-shaded"        % "1.72"
+  private val bouncyCastle                   = "io.gatling"                           % "gatling-recorder-bc-shaded"        % "1.73"
   private val quicklens                      = "com.softwaremill.quicklens"          %% "quicklens"                         % "1.9.1"
   private val fastUuid                       = "com.eatthepath"                       % "fast-uuid"                         % "0.2.0"
   private val pebble                         = "io.pebbletemplates"                   % "pebble"                            % "3.2.0"
