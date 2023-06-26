72835b2e41d4d619274cf5d3216b64d780ab5dd1
ops+github@gatling.io <Gatling CI>
2023-06-26 02:35:07 +0000

    Update commons-codec to 1.16.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -107,7 +107,7 @@ object Dependencies {
   // Docs dependencies
   private val commonsIo                      = "commons-io"                           % "commons-io"                        % "2.13.0"
   private val commonsLang                    = "org.apache.commons"                   % "commons-lang3"                     % "3.12.0"
-  private val commonsCodec                   = "commons-codec"                        % "commons-codec"                     % "1.15"
+  private val commonsCodec                   = "commons-codec"                        % "commons-codec"                     % "1.16.0"
 
   // format: ON
   private val loggingDeps = Seq(slf4jApi, scalaLogging, logback)
