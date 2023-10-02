03496693120ba5b3852c4971eb0840233ee3ceb3
Stephane Landelle <slandelle@gatling.io>
2023-10-02 23:23:45 +0200

    chore: upgrade gatling-enterprise-plugin-commons 1.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -84,7 +84,7 @@ object Dependencies {
   private val pebble                         = "io.pebbletemplates"                   % "pebble"                            % "3.2.1"
   private val spotbugs                       = "com.github.spotbugs"                  % "spotbugs-annotations"              % "4.7.3"
   private val typetools                      = "net.jodah"                            % "typetools"                         % "0.6.3"
-  private val gatlingEnterprisePluginCommons = "io.gatling"                           % "gatling-enterprise-plugin-commons" % "1.6.1"
+  private val gatlingEnterprisePluginCommons = "io.gatling"                           % "gatling-enterprise-plugin-commons" % "1.7.0"
 
   // Test dependencies
   private val scalaTest                      = "org.scalatest"                       %% "scalatest"                         % "3.2.17"            % Test
