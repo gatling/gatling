bf62d323091741cdb7ccf0b6e8a8a21409beea98
slandelle <slandelle@gatling.io>
2023-11-13 10:17:58 +0100

    nit: clean up

--- gatling-http-client/src/main/java/io/gatling/http/client/impl/request/WritableRequestBuilder.java
+++ gatling-http-client/src/main/java/io/gatling/http/client/impl/request/WritableRequestBuilder.java
@@ -23,19 +23,25 @@ import io.gatling.http.client.Request;
 import io.gatling.http.client.body.RequestBody;
 import io.gatling.http.client.body.WritableContent;
 import io.gatling.http.client.proxy.HttpProxyServer;
+import io.gatling.http.client.proxy.ProxyServer;
 import io.gatling.http.client.uri.Uri;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.*;
-import java.io.IOException;
 import java.util.function.Consumer;
 
 public class WritableRequestBuilder {
 
-  private static WritableRequest buildRequestWithoutBody(
-      String url, HttpMethod method, HttpHeaders headers) {
+  private static String requestUrl(Uri uri, ProxyServer proxyServer, boolean http2) {
+    return http2 || (proxyServer instanceof HttpProxyServer && !uri.isSecured())
+        ? uri.toUrl()
+        : // HTTP proxy with clear HTTP uses absolute url
+        uri.toRelativeUrl();
+  }
 
+  private static WritableRequest buildRequestWithoutBody(
+      HttpMethod method, String url, HttpHeaders headers) {
     // force content-length to 0 when method usually takes a body, some servers might break
     // otherwise
     if (!headers.contains(CONTENT_LENGTH)
@@ -43,79 +49,72 @@ public class WritableRequestBuilder {
       headers.set(CONTENT_LENGTH, 0);
     }
 
-    FullHttpRequest nettyRequest =
+    return new WritableRequest(
         new DefaultFullHttpRequest(
             HttpVersion.HTTP_1_1,
             method,
             url,
             Unpooled.EMPTY_BUFFER,
             headers,
-            EmptyHttpHeaders.INSTANCE);
-
-    return new WritableRequest(nettyRequest, null);
+            EmptyHttpHeaders.INSTANCE),
+        null);
   }
 
-  private static WritableRequest buildRequestWithBody(
-      String url,
-      HttpMethod method,
-      HttpHeaders headers,
-      RequestBody requestBody,
-      ByteBufAllocator alloc)
-      throws IOException {
-
-    WritableContent writableContent = requestBody.build(alloc);
-
-    Object content = writableContent.getContent();
-
-    if (content instanceof ByteBuf && !headers.contains(EXPECT, HttpHeaderValues.CONTINUE, true)) {
-      ByteBuf bb = (ByteBuf) content;
-      if (!headers.contains(CONTENT_LENGTH)) {
-        headers.set(CONTENT_LENGTH, bb.readableBytes());
-      }
-      FullHttpRequest nettyRequest =
-          new DefaultFullHttpRequest(
-              HttpVersion.HTTP_1_1, method, url, bb, headers, EmptyHttpHeaders.INSTANCE);
-
-      return new WritableRequest(nettyRequest, null);
-
-    } else {
-      if (!headers.contains(CONTENT_LENGTH) && !headers.contains(TRANSFER_ENCODING)) {
-        if (writableContent.getContentLength() >= 0) {
-          headers.set(CONTENT_LENGTH, writableContent.getContentLength());
-        } else {
-          headers.set(TRANSFER_ENCODING, HttpHeaderValues.CHUNKED);
-        }
-      }
-
-      HttpRequest nettyRequest = new DefaultHttpRequest(HttpVersion.HTTP_1_1, method, url, headers);
-
-      return new WritableRequest(nettyRequest, content);
+  private static WritableRequest buildFullRequest(
+      HttpMethod method, String url, HttpHeaders headers, ByteBuf body, long contentLength) {
+    if (!headers.contains(CONTENT_LENGTH)) {
+      headers.set(CONTENT_LENGTH, contentLength);
     }
+
+    return new WritableRequest(
+        new DefaultFullHttpRequest(
+            HttpVersion.HTTP_1_1, method, url, body, headers, EmptyHttpHeaders.INSTANCE),
+        null);
+  }
+
+  private static WritableRequest buildRequestWithChunkedBody(
+      HttpMethod method, String url, HttpHeaders headers, Object body, long contentLength) {
+    if (!headers.contains(CONTENT_LENGTH) && !headers.contains(TRANSFER_ENCODING)) {
+      if (contentLength >= 0) {
+        headers.set(CONTENT_LENGTH, contentLength);
+      } else {
+        headers.set(TRANSFER_ENCODING, HttpHeaderValues.CHUNKED);
+      }
+    }
+
+    return new WritableRequest(
+        new DefaultHttpRequest(HttpVersion.HTTP_1_1, method, url, headers), body);
   }
 
   public static WritableRequest buildRequest(Request request, ByteBufAllocator alloc, boolean http2)
       throws Exception {
-    Uri uri = request.getUri();
-    HttpHeaders headers = request.getHeaders();
-    RequestBody requestBody = request.getBody();
-
-    String url =
-        http2 || (request.getProxyServer() instanceof HttpProxyServer && !uri.isSecured())
-            ? uri.toUrl()
-            : // HTTP proxy with clear HTTP uses absolute url
-            uri.toRelativeUrl();
-
-    WritableRequest writableRequest =
-        requestBody == null
-            ? buildRequestWithoutBody(url, request.getMethod(), headers)
-            : buildRequestWithBody(url, request.getMethod(), headers, requestBody, alloc);
 
     Consumer<Request> signatureCalculator = request.getSignatureCalculator();
     if (signatureCalculator != null) {
-      // only apply now that WritableRequest has patched Content-Length and Transfer-Encoding
-      // headers
       signatureCalculator.accept(request);
     }
-    return writableRequest;
+
+    HttpMethod method = request.getMethod();
+    String url = requestUrl(request.getUri(), request.getProxyServer(), http2);
+    HttpHeaders headers = request.getHeaders();
+    RequestBody requestBody = request.getBody();
+
+    WritableContent writableContent = requestBody != null ? requestBody.build(alloc) : null;
+
+    if (writableContent == null) {
+      return buildRequestWithoutBody(method, url, headers);
+
+    } else {
+      long contentLength = writableContent.getContentLength();
+      Object content = writableContent.getContent();
+      if (content instanceof ByteBuf
+          && !headers.contains(EXPECT, HttpHeaderValues.CONTINUE, true)) {
+        // FIXME don't handle Expect-Continue here
+        return buildFullRequest(method, url, headers, (ByteBuf) content, contentLength);
+
+      } else {
+        return buildRequestWithChunkedBody(method, url, headers, content, contentLength);
+      }
+    }
   }
 }
