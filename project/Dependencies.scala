3ff1bb4758917b760b4105e0a14cd2e194993343
slandelle <slandelle@gatling.io>
2023-12-04 14:41:12 +0100

    chore: upgrade pebble 3.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -81,7 +81,7 @@ object Dependencies {
   private val bouncyCastle                   = "io.gatling"                           % "gatling-recorder-bc-shaded"        % "1.77.0"
   private val quicklens                      = "com.softwaremill.quicklens"          %% "quicklens"                         % "1.9.6"
   private val fastUuid                       = "com.eatthepath"                       % "fast-uuid"                         % "0.2.0"
-  private val pebble                         = "io.pebbletemplates"                   % "pebble"                            % "3.2.1"
+  private val pebble                         = "io.pebbletemplates"                   % "pebble"                            % "3.2.2"
   private val spotbugs                       = "com.github.spotbugs"                  % "spotbugs-annotations"              % "4.8.2"
   private val typetools                      = "net.jodah"                            % "typetools"                         % "0.6.3"
   private val gatlingEnterprisePluginCommons = "io.gatling"                           % "gatling-enterprise-plugin-commons" % "1.7.0"
