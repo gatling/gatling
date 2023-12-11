658319aa1a7644ec0b6a1955c35c7194b654b48d
slandelle <slandelle@gatling.io>
2023-12-11 17:31:26 +0100

    nit: group gatling deps together

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,6 +10,7 @@ object Dependencies {
   private def scalaCompilerBridge(version: String) = "org.scala-lang"                 % "scala2-sbt-bridge"                 % version
   private val gatlingSharedUtil              = "io.gatling"                          %% "gatling-shared-util"               % "0.0.5"
   private val gatlingSharedModel             = "io.gatling"                          %% "gatling-shared-model"              % "0.0.4"
+  private val gatlingEnterprisePluginCommons = "io.gatling"                           % "gatling-enterprise-plugin-commons" % "1.7.0"
   private val scalaSwing                     = "org.scala-lang.modules"              %% "scala-swing"                       % "3.0.0"
   private val scalaParserCombinators         = "org.scala-lang.modules"              %% "scala-parser-combinators"          % "2.3.0"
   private val netty                          = "io.netty"                             % "netty-codec-http"                  % "4.1.101.Final"
@@ -84,7 +85,6 @@ object Dependencies {
   private val pebble                         = "io.pebbletemplates"                   % "pebble"                            % "3.2.2"
   private val spotbugs                       = "com.github.spotbugs"                  % "spotbugs-annotations"              % "4.8.2"
   private val typetools                      = "net.jodah"                            % "typetools"                         % "0.6.3"
-  private val gatlingEnterprisePluginCommons = "io.gatling"                           % "gatling-enterprise-plugin-commons" % "1.7.0"
 
   // Test dependencies
   private val scalaTest                      = "org.scalatest"                       %% "scalatest"                         % "3.2.17"            % Test
