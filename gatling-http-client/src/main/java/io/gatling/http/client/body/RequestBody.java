gatling/gatling

commit 0d68559afeed5ceedfb4bca390d009c202134a7f

chore: limit the length of printed HTTP body Strings
Motivation:

Try to not blow heap usage with humongous bodies

gatling-http/src/main/scala/io/gatling/http/util/package.scala
@@ -36,6 +36,8 @@ package object util extends LazyLogging {
 
   private val bufferFormat = DateTimeFormatter.ofPattern("HH:mm:ss.SSS").withZone(ZoneId.systemDefault())
 
+  private val BodyMaxPrintableLength = 500_000
+
   implicit class HttpStringBuilder(val buff: jl.StringBuilder) extends AnyVal {
     def appendHttpHeaders(headers: HttpHeaders): jl.StringBuilder = {
       headers.asScala.foreach { entry =>
@@ -60,7 +62,7 @@ package object util extends LazyLogging {
       }
 
       Option(request.getBody).foreach { requestBody =>
-        buff.append("body:").append(requestBody).append(Eol)
+        buff.append("body:").append(requestBody.toString.substring(0, BodyMaxPrintableLength)).append(Eol)
       }
 
       if (request.getProxyServer != null) {
@@ -96,7 +98,7 @@ package object util extends LazyLogging {
             buff.append("body:").append(Eol)
             if (isText(response.headers)) {
               try {
-                buff.append(response.body.string)
+                buff.append(response.body.string.substring(0, BodyMaxPrintableLength))
               } catch {
                 case NonFatal(t) =>
                   val message = "Could not decode response body"

commit b43554edd228a4d2b38c908f9dc99f97cb3a21ff

fix: Properly truncate HTTP request and response bodies when logging, close #4496
Motivation:

Regression introduced in https://github.com/gatling/gatling/commit/0d68559afeed5ceedfb4bca390d009c202134a7f

gatling-http-client/src/main/java/io/gatling/http/client/body/RequestBody.java
@@ -31,6 +31,8 @@ public interface RequestBody {
     return null;
   }
 
+  String print(int maxLength);
+
   abstract class Base<T> implements RequestBody {
 
     protected final T content;
@@ -41,6 +43,15 @@ public interface RequestBody {
 
     public T getContent() {
       return content;
+    }
+
+    protected final String truncate(String string, int maxLength) {
+      return string.length() > maxLength ? string.substring(0, maxLength) + "..." : string;
+    }
+
+    @Override
+    public String toString() {
+      return print(Integer.MAX_VALUE);
     }
   }
 }

gatling-http-client/src/main/java/io/gatling/http/client/body/bytearray/ByteArrayRequestBody.java
@@ -59,10 +59,10 @@ public final class ByteArrayRequestBody extends RequestBody.Base<byte[]> {
   }
 
   @Override
-  public String toString() {
+  public String print(int maxLength) {
     return "ByteArrayRequestBody{"
         + "content (Base64)="
-        + Base64.getEncoder().encodeToString(content)
+        + truncate(Base64.getEncoder().encodeToString(content), maxLength)
         + '}';
   }
 }

gatling-http-client/src/main/java/io/gatling/http/client/body/file/FileRequestBody.java
@@ -66,7 +66,7 @@ public final class FileRequestBody extends RequestBody.Base<File> {
   }
 
   @Override
-  public String toString() {
+  public String print(int maxLength) {
     return "FileRequestBody{" + "charset=" + charset + ", content=" + content + '}';
   }
 }

gatling-http-client/src/main/java/io/gatling/http/client/body/form/FormUrlEncodedRequestBody.java
@@ -104,14 +104,14 @@ public final class FormUrlEncodedRequestBody extends RequestBody.Base<List<Param
   }
 
   @Override
-  public String toString() {
+  public String print(int maxLength) {
     return "FormUrlEncodedRequestBody{"
         + "patchedContentType='"
         + patchedContentType
         + "', charset="
         + charset
         + ", content="
-        + encode()
+        + truncate(encode().toString(), maxLength)
         + '}';
   }
 }

gatling-http-client/src/main/java/io/gatling/http/client/body/is/InputStreamRequestBody.java
@@ -71,7 +71,7 @@ public final class InputStreamRequestBody extends RequestBody.Base<InputStream> 
   }
 
   @Override
-  public String toString() {
+  public String print(int maxLength) {
     return "InputStreamRequestBody{content=???}";
   }
 }

gatling-http-client/src/main/java/io/gatling/http/client/body/multipart/MultipartFormDataRequestBody.java
@@ -98,12 +98,12 @@ public final class MultipartFormDataRequestBody extends RequestBody.Base<List<Pa
   }
 
   @Override
-  public String toString() {
+  public String print(int maxLength) {
     final StringBuilder sb = new StringBuilder("MultipartFormDataRequestBody{");
     sb.append("boundary=").append(Arrays.toString(boundary));
     sb.append(", patchedContentType='").append(patchedContentType).append('\'');
     sb.append(", content=");
-    content.forEach(part -> sb.append(part).append("\n"));
+    content.forEach(part -> sb.append(truncate(part.toString(), maxLength)).append("\n"));
     sb.append('}');
     return sb.toString();
   }

gatling-http-client/src/main/java/io/gatling/http/client/body/string/StringRequestBody.java
@@ -56,7 +56,12 @@ public final class StringRequestBody extends RequestBody.Base<String> {
   }
 
   @Override
-  public String toString() {
-    return "StringRequestBody{" + "charset=" + charset + ", content=" + content + '}';
+  public String print(int maxLength) {
+    return "StringRequestBody{"
+        + "charset="
+        + charset
+        + ", content="
+        + truncate(content, maxLength)
+        + '}';
   }
 }

gatling-http-client/src/main/java/io/gatling/http/client/body/stringchunks/StringChunksRequestBody.java
@@ -61,12 +61,12 @@ public final class StringChunksRequestBody extends RequestBody.Base<List<StringW
   }
 
   @Override
-  public String toString() {
+  public String print(int maxLength) {
     return "StringChunksRequestBody{"
         + "charset="
         + charset
         + ", content="
-        + StringWithCachedBytes.toString(content)
+        + truncate(StringWithCachedBytes.toString(content), maxLength)
         + '}';
   }
 }

gatling-http/src/main/scala/io/gatling/http/util/package.scala
@@ -62,7 +62,7 @@ package object util extends LazyLogging {
       }
 
       Option(request.getBody).foreach { requestBody =>
-        buff.append("body:").append(requestBody.toString.substring(0, BodyMaxPrintableLength)).append(Eol)
+        buff.append("body:").append(requestBody.print(BodyMaxPrintableLength)).append(Eol)
       }
 
       if (request.getProxyServer != null) {
@@ -98,7 +98,13 @@ package object util extends LazyLogging {
             buff.append("body:").append(Eol)
             if (isText(response.headers)) {
               try {
-                buff.append(response.body.string.substring(0, BodyMaxPrintableLength))
+                val bodyString = response.body.string
+                if (bodyString.length > BodyMaxPrintableLength) {
+                  buff.append(response.body.string, 0, BodyMaxPrintableLength).append("...")
+                } else {
+                  buff.append(response.body.string)
+                }
+
               } catch {
                 case NonFatal(t) =>
                   val message = "Could not decode response body"