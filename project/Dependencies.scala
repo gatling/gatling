b4171f96aebe133de6dffb7ee4c533b7c68ea518
Guillaume Corré <gcorre@gatling.io>
2024-02-06 10:43:31 +0100

    doc: replace callOptions with dedicated deadlineAfter methods

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -124,7 +124,7 @@ object Dependencies {
 
   // Dependencies by module
 
-  private val gatlingGrpcVersion = "3.10.3"
+  private val gatlingGrpcVersion = "3.10.4"
   val docSamplesDependencies =
     Seq(
       commonsIo,
