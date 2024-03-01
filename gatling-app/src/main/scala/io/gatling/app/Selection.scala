9562c1d9ed2797e7cd8ac636ea79f35f44b9820d
slandelle <slandelle@gatling.io>
2024-03-01 10:16:06 +0100

    chore:drop interactive mode from Gatling
    Motivation:
    
    Should be handled in the build plugins, not Gatling itself, in particular now that we've dropped the original bundle.

--- gatling-app/src/main/scala/io/gatling/app/Selection.scala
+++ gatling-app/src/main/scala/io/gatling/app/Selection.scala
@@ -16,92 +16,22 @@
 
 package io.gatling.app
 
-import scala.Console._
-import scala.annotation.tailrec
-import scala.io.StdIn
-import scala.util.{ Success, Try }
-
-import io.gatling.app.classloader.SimulationClassLoader
 import io.gatling.commons.util.StringHelper._
 import io.gatling.core.cli.GatlingArgs
 
 final class Selection(val simulationClass: SimulationClass, val simulationId: String, val description: String)
 
 object Selection {
-  private val MaxReadSimulationNumberAttempts = 10
 
   def apply(gatlingArgs: GatlingArgs): Selection =
-    new Selector().selection(gatlingArgs)
+    gatlingArgs.simulationClass match {
+      case Some(simulationClassName) =>
+        val simulationClass = SimulationClass
+          .fromClass(getClass.getClassLoader.loadClass(simulationClassName))
+          .getOrElse(throw new IllegalArgumentException(s"Simulation class $simulationClassName is not a Simulation"))
+        new Selection(simulationClass, simulationClass.simpleName.clean, gatlingArgs.runDescription.getOrElse(""))
 
-  private class Selector {
-    def selection(gatlingArgs: GatlingArgs): Selection =
-      gatlingArgs.simulationClass match {
-        case Some(configDefinedSimulationClassName) =>
-          val simulation = SimulationClass
-            .fromClass(getClass.getClassLoader.loadClass(configDefinedSimulationClassName))
-            .getOrElse(throw new IllegalArgumentException(s"User defined simulation class $configDefinedSimulationClassName is not a Simulation"))
-          new Selection(simulation, simulation.simpleName.clean, gatlingArgs.runDescription.getOrElse(""))
-
-        case _ =>
-          val simulationClasses: List[SimulationClass] =
-            new SimulationClassLoader(getClass.getClassLoader).simulationClasses.sortBy(_.canonicalName)
-          val simulation = singleSimulationFromList(simulationClasses)
-            .getOrElse(interactiveSelect(simulationClasses))
-          val simulationId = simulation.simpleName.clean
-          val runDescription = gatlingArgs.runDescription.getOrElse(askRunDescription())
-          new Selection(simulation, simulationId, runDescription)
-      }
-
-    private def singleSimulationFromList(simulationClasses: List[SimulationClass]) =
-      simulationClasses match {
-        case List(simulationClass) =>
-          println(s"${simulationClass.canonicalName} is the only simulation, executing it.")
-          Some(simulationClass)
-
-        case _ => None
-      }
-
-    @SuppressWarnings(Array("org.wartremover.warts.SeqApply"))
-    private def interactiveSelect(simulationClasses: List[SimulationClass]): SimulationClass = {
-      val validRange = simulationClasses.indices
-
-      @tailrec
-      def readSimulationNumber(attempts: Int): Int =
-        if (attempts > MaxReadSimulationNumberAttempts) {
-          println(s"Max attempts of reading simulation number ($MaxReadSimulationNumberAttempts) reached. Aborting.")
-          System.out.flush()
-          sys.exit(1)
-        } else {
-          println("Choose a simulation number:")
-          for ((simulation, index) <- simulationClasses.zipWithIndex) {
-            println(s"     [$index] ${simulation.canonicalName}")
-          }
-
-          Try(StdIn.readInt()) match {
-            case Success(number) =>
-              if (validRange.contains(number)) {
-                number
-              } else {
-                println(s"Invalid selection, must be in [0, ${simulationClasses.length - 1}].")
-                readSimulationNumber(attempts + 1)
-              }
-            case _ =>
-              println("Invalid characters, please provide a correct simulation number:")
-              readSimulationNumber(attempts + 1)
-          }
-        }
-
-      if (simulationClasses.isEmpty) {
-        println("Couldn't find any Simulation class. Please check that your Simulations are in the correct location.")
-        System.out.flush()
-        sys.exit(1)
-      }
-      simulationClasses(readSimulationNumber(0))
+      case _ =>
+        throw new IllegalArgumentException("Missing simulation class")
     }
-
-    private def askRunDescription(): String = {
-      println("Select run description (optional)")
-      StdIn.readLine().trim
-    }
-  }
 }
