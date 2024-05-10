2b0f3160e370436b72d30908ff30f45fa748a3ce
Ravi Kumar Gajul <51070896+ravigajul@users.noreply.github.com>
2024-05-09 17:35:29 -0700

    Update InjectionSampleJava.java
    Updated randomized() to randomized on lines 38 and 40

--- src/docs/content/reference/script/core/injection/code/InjectionSampleJava.java
+++ src/docs/content/reference/script/core/injection/code/InjectionSampleJava.java
@@ -35,9 +35,9 @@ setUp(
     atOnceUsers(10), // 2
     rampUsers(10).during(5), // 3
     constantUsersPerSec(20).during(15), // 4
-    constantUsersPerSec(20).during(15).randomized(), // 5
+    constantUsersPerSec(20).during(15).randomized, // 5
     rampUsersPerSec(10).to(20).during(10), // 6
-    rampUsersPerSec(10).to(20).during(10).randomized(), // 7
+    rampUsersPerSec(10).to(20).during(10).randomized, // 7
     stressPeakUsers(1000).during(20) // 8
   ).protocols(httpProtocol)
 );
