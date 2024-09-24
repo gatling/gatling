1a495d5cbc5f1f9adfb5efa49f1461b54cb20edd
slandelle <slandelle@gatling.io>
2024-09-24 17:25:28 +0200

    chore: upgrade gatling-grpc 3.12.0.3 and gatling-mqtt 3.12.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -96,8 +96,8 @@ object Dependencies {
   private val parserDeps = Seq(jackson, saxon, lagarto, joddUtil, jmespath)
 
   // Dependencies by module
-  private val gatlingGrpcVersion = "3.12.0.1"
-  private val gatlingMqttVersion = "3.12.0"
+  private val gatlingGrpcVersion = "3.12.0.3"
+  private val gatlingMqttVersion = "3.12.0.1"
 
   val nettyUtilDependencies =
     Seq(
