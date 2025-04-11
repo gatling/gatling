4b5ad6bd3c8cedcd3ad0d0fd10b8ab92c47342d6
slandelle <slandelle@gatling.io>
2025-04-11 10:05:36 +0200

    fix: only enable window size when jzib is in the classpath, close #4619

--- gatling-http-client/src/main/java/io/gatling/http/client/impl/AllowClientNoContextWebSocketClientCompressionHandler.java
+++ gatling-http-client/src/main/java/io/gatling/http/client/impl/AllowClientNoContextWebSocketClientCompressionHandler.java
@@ -22,10 +22,27 @@ import io.netty.handler.codec.compression.ZlibCodecFactory;
 import io.netty.handler.codec.http.websocketx.extensions.WebSocketClientExtensionHandler;
 import io.netty.handler.codec.http.websocketx.extensions.compression.DeflateFrameClientExtensionHandshaker;
 import io.netty.handler.codec.http.websocketx.extensions.compression.PerMessageDeflateClientExtensionHandshaker;
+import io.netty.util.internal.PlatformDependent;
 
 // fork of WebSocketClientCompressionHandler that enables allowClientNoContext
 public final class AllowClientNoContextWebSocketClientCompressionHandler
     extends WebSocketClientExtensionHandler {
+
+  private static final boolean JZLIB_AVAILABLE;
+
+  static {
+    boolean jzlibAvailable;
+    try {
+      Class.forName(
+          "com.jcraft.jzlib.JZlib",
+          false,
+          PlatformDependent.getClassLoader(ZlibCodecFactory.class));
+      jzlibAvailable = true;
+    } catch (ClassNotFoundException t) {
+      jzlibAvailable = false;
+    }
+    JZLIB_AVAILABLE = jzlibAvailable;
+  }
 
   public static final AllowClientNoContextWebSocketClientCompressionHandler INSTANCE =
       new AllowClientNoContextWebSocketClientCompressionHandler();
@@ -38,7 +55,7 @@ public final class AllowClientNoContextWebSocketClientCompressionHandler
   private AllowClientNoContextWebSocketClientCompressionHandler() {
     super(
         new PerMessageDeflateClientExtensionHandshaker(
-            6, ZlibCodecFactory.isSupportingWindowSizeAndMemLevel(), MAX_WINDOW_SIZE, true, false),
+            6, JZLIB_AVAILABLE, MAX_WINDOW_SIZE, true, false),
         new DeflateFrameClientExtensionHandshaker(false),
         new DeflateFrameClientExtensionHandshaker(true));
   }
