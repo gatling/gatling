24c140d5092325c5a2c98f92e8503eea7d296b01
slandelle <slandelle@gatling.io>
2025-11-17 22:50:17 +0100

    chore: upgrade gatling-shared-util 0.0.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
 
   // scalafmt: { align.tokens = ["=", "%", "%%", "classifier"], maxColumn = 250 }
   private def scalaReflect(version: String)  = "org.scala-lang"                                               % "scala-reflect"            % version % Provided
-  private val gatlingSharedUtil              = "io.gatling"                                                  %% "gatling-shared-util"      % "0.0.13"
+  private val gatlingSharedUtil              = "io.gatling"                                                  %% "gatling-shared-util"      % "0.0.14"
   private val gatlingSharedModel             = "io.gatling"                                                  %% "gatling-shared-model"     % "0.1.2"
   private val gatlingSharedCli               = "io.gatling"                                                   % "gatling-shared-cli"       % "0.0.7"
   private val scalaSwing                     = "org.scala-lang.modules"                                      %% "scala-swing"              % "3.0.0"
